13dec550c01b15ebc9af488624f9e126f02aba25
lzwjava <lzwjava@gmail.com>
2021-07-10 01:06:00 +0800

    a

--- 202/src/Main.java
+++ 202/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.FileInputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
@@ -12,7 +13,7 @@ public class Main {
             }
             int numerator = sc.nextInt();
             int denominator = sc.nextInt();
-            int size = 1000;
+            int size = 3000;
             int nums[] = new int[size];
             int positive = (int) (numerator / denominator);
             int res = numerator % denominator;
@@ -24,10 +25,10 @@ public class Main {
                 }
                 nums[i] = (int) (res * 10 / denominator);
                 res = (res * 10) % denominator;
-                // System.out.println("nums[i] " + i + "=" + nums[i]);
-            }
+            }         
+
             if (i < size) {
-                System.out.print(String.format("%d/%d = %d.", numerator, denominator, positive));
+                System.out.print(String.format("%d/%d = %d.", numerator, denominator, positive));                
                 for (int j = 0; j < i; j++) {
                     System.out.print(nums[j]);
                 }
@@ -35,18 +36,22 @@ public class Main {
                 System.out.println("   1 = number of digits in repeating cycle");
             } else {
                 boolean found = false;
-                int start =0, len = 0;                
-                for (len = 1; len * 2 < size; len++) {
-                    for (start = 0; start + len * 2 < size; start++) {
+                int start = 0, end = 0;
+
+                for (end = 1; end < size; end++) {
+                    for (start = 0; start < end; start++) {
+                        // System.out.println("start " + start +" end " + end);                        
                         boolean ok = true;
-                        for (int j = 0; start + j * len < size; j++) {
-                            if (nums[start + j % len] != nums[j]) {
+                        int len = end - start;
+                        for (int j = start + len; j < size; j++) {
+                            if (nums[j] != nums[start + (j - start) % len]) {
                                 ok = false;
                                 break;
                             }
                         }
                         if (ok) {
                             found = true;
+                            break;
                         }
                     }
                     if (found) {
@@ -54,39 +59,50 @@ public class Main {
                     }
                 }
                 if (found) {
+                    int len = end - start;
                     System.out.print(String.format("%d/%d = %d.", numerator, denominator, positive));
-                    for (int j = 0; j < start; j++) {
-                        System.out.print(nums[j]);
+                    int j;
+                    for (j = 0; j < 50; j++) {
+                        if (j < start) {
+                            System.out.print(nums[j]);
+                        } else if (j == start) {
+                            System.out.print("(");
+                            System.out.print(nums[j]);
+                        } else if (j < start + len) {
+                            System.out.print(nums[j]);
+                        } else if (j == start + len) {
+                            System.out.print(")");
+                            break;
+                        }
                     }
-                    System.out.print("(");
-                    int printLen = Math.min(50, len);
-                    for (int j = 0; j < printLen; j++) {
-                        System.out.print(nums[start + j]);
+                    if (j == 50) {
+                        System.out.print("...)");
                     }
-                    if (len > 50) {
-                        System.out.println("...)");
-                    } else {
-                        System.out.println(")");
-                    }
+                    System.out.println();
                     System.out.println(String.format("   %d = number of digits in repeating cycle", len));
                 }
             }
+            System.out.println();
         }
         sc.close();
     }
 
     public static void main(String[] args) throws Exception {
         FileInputStream inStream = null;
-        boolean isLocal = System.getProperty("os.name").equals("Mac OS X");
+        PrintStream outStream = null;
+        boolean isLocal = System.getProperty("os.name").equals("Mac OS X");        
         if (isLocal) {
             inStream = new FileInputStream("1.in");
+            outStream = new PrintStream("1.out");
             System.setIn(inStream);
+            // System.setOut(outStream);
         }
 
         work();
 
         if (isLocal) {
             inStream.close();
+            outStream.close();
         }
     }
 }
