390cb5ac2aebd8fec552d3cd3e89ee12eae0bc5b
lzwjava <lzwjava@gmail.com>
2021-08-08 01:24:23 +0800

    a

---
+++ 10038/src/Main.java
@@ -0,0 +1,58 @@
+import java.io.FileInputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+   
+    void work() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int nums[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums[i] = sc.nextInt();
+            }
+            boolean ok = true;
+            boolean[] takes = new boolean[n];
+            for (int i = 0; i < n - 1; i++) {
+                int v = Math.abs(nums[i] - nums[i + 1]);
+                if (v >= 1 && v <= n - 1) {
+                    if (!takes[v]) {
+                        takes[v] = true;
+                    } else {
+                        ok = false;
+                        break;
+                    }
+                } else {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                System.out.println("Jolly");
+            } else {
+                System.out.println("Not jolly");
+            }
+        }
+        sc.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inStream = null;
+        PrintStream outStream = null;
+        boolean isLocal = System.getProperty("os.name").equals("Mac OS X");        
+        if (isLocal) {
+            inStream = new FileInputStream("1.in");
+            // outStream = new PrintStream("1.out");
+            System.setIn(inStream);
+            // System.setOut(outStream);
+        }
+
+        new Main().work();
+
+        if (isLocal) {
+            inStream.close();
+            // outStream.close();
+        }
+    }
+}
