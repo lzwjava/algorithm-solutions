ba758511f9005a00a15623411b9c116627367032
lzwjava <lzwjava@gmail.com>
2021-08-07 20:42:54 +0800

    a

--- 12333/src/Main.java
+++ 12333/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         int index;
 
         FiBigInteger() {
-            
+
         }
 
         FiBigInteger(BigInteger bi, int index) {
@@ -22,26 +22,63 @@ public class Main {
             this.str = bi.toString(10);
         }
     }
+    
+    String add(String a, String b) {
+        if (a.length() > b.length()) {
+            String tmp = a;
+            a = b;
+            b = tmp;
+        }
+        int alen = a.length();
+        int blen = b.length();
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        int i;
+        for (i = 0; i < blen; i++) {
+            int sum = carry;
+            if (alen - 1 - i >= 0) {
+                char cha = a.charAt(alen - 1 - i);
+                sum += (cha - '0');
+            }
+            char chb = b.charAt(blen - 1 - i);
+            sum += (chb - '0');
+            if (sum > 10) {
+                carry = 1;
+                sum = sum % 10;
+            } else {
+                carry = 0;
+            }
+            sb.append((char)('0' + sum));
+        }   
+        return sb.reverse().toString();                
+    }
    
     void work() {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println(add("10", "11"));
+
         int t = sc.nextInt();
         ArrayList<FiBigInteger> fs = new ArrayList<>();
-        fs.add(new FiBigInteger(new BigInteger("1"), 0));
-        fs.add(new FiBigInteger(new BigInteger("1"), 1));
+        FiBigInteger fb2 = new FiBigInteger(new BigInteger("1"), 0);
+        FiBigInteger fb1 = new FiBigInteger(new BigInteger("1"), 1);
+        fs.add(fb2);
+        fs.add(fb1);
         int n = 2;
-        for (; n < 10000; n++) {
-            FiBigInteger fb1 = fs.get(n - 1);
-            FiBigInteger fb2 = fs.get(n - 2);
+        for (; n < 1000; n++) {
             BigInteger bn = fb1.bi.add(fb2.bi);
             FiBigInteger fb = new FiBigInteger(bn, n);
             fs.add(fb);
+        
+            fb2 = fb1;
+            fb1 = fb;            
         }
+        int caseNum = 1;
 
         while (t > 0) {
             String str = sc.next();
 
-            System.out.print(String.format("Case #%d: ", t));
+            System.out.print(String.format("Case #%d: ", caseNum));
             boolean found = false;
             int i = 0;
             for (i = 0; i < fs.size(); i++) {
@@ -56,7 +93,9 @@ public class Main {
             } else {
                 System.out.println("-1");
             }
+
             t--;
+            caseNum++;
         }
         sc.close();
     }
