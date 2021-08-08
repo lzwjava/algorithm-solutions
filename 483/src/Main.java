e4aee8b8fe0adbef9c997a3be1df2786f2523f80
lzwjava <lzwjava@gmail.com>
2021-08-08 23:07:44 +0800

    a

---
+++ 483/src/Main.java
@@ -0,0 +1,43 @@
+import java.io.FileInputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+   
+    void solve() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String words[] = line.split(" ");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < words.length; i++) {
+                if (i != 0) {
+                    sb.append(" ");
+                }
+                StringBuilder word = new StringBuilder(words[i]);
+                sb.append(word.reverse());
+            }
+            System.out.println(sb.toString());
+        }
+        sc.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inStream = null;
+        PrintStream outStream = null;
+        boolean isLocal = System.getProperty("os.name").equals("Mac OS X");        
+        if (isLocal) {
+            inStream = new FileInputStream("1.in");
+            // outStream = new PrintStream("1.out");
+            System.setIn(inStream);
+            // System.setOut(outStream);
+        }
+
+        new Main().solve();
+
+        if (isLocal) {
+            inStream.close();
+            // outStream.close();
+        }
+    }
+}
