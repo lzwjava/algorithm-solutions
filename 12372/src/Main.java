b42e7b2133d2d4d1a17e42090963df32e07bbab5
lzwjava <lzwjava@gmail.com>
2021-08-09 01:28:55 +0800

    a

--- 12372/src/Main.java
+++ 12372/src/Main.java
@@ -5,7 +5,22 @@ import java.util.Scanner;
 public class Main {
    
     void solve() {
-        Scanner sc = new Scanner(System.in);    
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int caseNum =1;
+        while (t > 0) {
+            int l = sc.nextInt();
+            int w = sc.nextInt();
+            int h = sc.nextInt();
+            System.out.print(String.format("Case %d: ", caseNum));
+            if (l <= 20 && w <= 20 && h <= 20) {
+                System.out.println("good");
+            } else {
+                System.out.println("bad");                
+            }
+            t--;
+            caseNum++;
+        }
         sc.close();
     }
 
