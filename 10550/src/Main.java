0f3bbe54412e03964b65df35e02761b5df0358c7
lzwjava <lzwjava@gmail.com>
2021-08-09 22:21:19 +0800

    a

--- 10550/src/Main.java
+++ 10550/src/Main.java
@@ -11,20 +11,28 @@ public class Main {
             int c1 = sc.nextInt();
             int c2 = sc.nextInt();
             int c3 = sc.nextInt();
-            
-            int degree = 0;
-            degree += 360 * 2;
-            if (p > c1) {
-                degree += (40 - p + c1) * 360 / 40;
-            } else {
-                degree += (c1 - p) * 360 / 40;
+            if (p == 0 && c1 == 0 && c2 == 0 && c3 == 0) {
+                break;
+            }            
+            int degree = 360 * 2;
+            if (p < c1) {
+                degree += (c1 - p) * 9;       
+            } else {       
+                degree += (c1 + 40 - p) * 9;
             }
-            p = c1;
             
             degree += 360;
-            if (p < c2) {
-                degree += 
+            if (c1 < c2) {
+                degree += (c1 + 40 - c2)*9;
+            } else {
+                degree += (c1 - c2) * 9;
             }
+            if (c2 > c3) {
+                degree += (40 - c2 + c3) * 9;                
+            } else {
+                degree += (c3 - c2) * 9;
+            }
+            System.out.println(degree);
         }
         sc.close();
     }
