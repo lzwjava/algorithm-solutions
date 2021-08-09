b5b7e5db25894a392dc8233241c52dad71b74e6e
lzwjava <lzwjava@gmail.com>
2021-08-10 02:52:18 +0800

    a

---
+++ 541/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.FileInputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+   
+    void solve() {
+        Scanner sc = new Scanner(System.in);    
+        sc.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inStream = null;
+        PrintStream outStream = null;
+        boolean isLocal = System.getProperty("os.name").equals("Mac OS X");        
+        if (isLocal) {
+            inStream = new FileInputStream("1.in");
+            // outStream = new PrintStream("1.out");
+            System.setIn(inStream);
+            // System.setOut(outStream);
+        }
+
+        new Main().solve();
+
+        if (isLocal) {
+            inStream.close();
+            // outStream.close();
+        }
+    }
+}
