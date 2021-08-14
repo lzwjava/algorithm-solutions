2f0855080fe6b1c3860658782e883c9c373f5734
lzwjava <lzwjava@gmail.com>
2021-08-14 23:03:39 +0800

    a

--- 10130/src/Main.java
+++ 10130/src/Main.java
@@ -1,44 +1,20 @@
 import java.io.FileInputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
-    class Obj implements Comparable<Obj> {
+    class Obj {
         int p;
         int w;
-        double r;
 
         Obj(int p, int w) {
             this.p = p;
             this.w = w;
-            this.r = p * 1.0 / w;
         }
-
-        @Override
-        public int compareTo(Main.Obj o) {
-            return Double.compare(o.r, r);
-        }
-
     }
     
-    int calObjs(ArrayList<Obj> objs, ArrayList<Boolean> used, int i, int n, int mw, int value) {
-        if (i == n) {
-            return value;
-        }
-        used.set(i, false);
-        int p1 = calObjs(objs, used, i + 1, n, mw, value);
-        Obj o = objs.get(i);
-        if (mw>= o.w){
-            used.set(i, true);
-            int p2 = calObjs(objs, used, i + 1, n, mw - o.w, value + o.p);
-            return p1 > p2 ? p1 : p2;
-        } else {
-            return p1;
-        }
-    }
    
     void solve() {
         Scanner sc = new Scanner(System.in);
@@ -54,20 +30,37 @@ public class Main {
             }
             int g = sc.nextInt();
             ArrayList<Integer> mws = new ArrayList<>();
+            int max = 0;
             for (int i = 0; i < g; i++) {
-                mws.add(sc.nextInt());
-            }              
+                int mw = sc.nextInt();
+                mws.add(mw);
+                if (mw > max){
+                    max = mw;
+                }
+            }
+
+            int d[][] = new int[n+1][max+1];
+            for (int i = 0; i <= max; i++) {
+                d[0][i] = 0;
+            }
+            for (int i = 1; i <= n; i++) {
+                for (int w = 0; w <= max; w++) {
+                    int v = d[i - 1][w];
+                    Obj obj = objs.get(i-1);
+                    if (w >= obj.w) {
+                        int nv = d[i - 1][w - obj.w] + obj.p;
+                        if (v < nv) {
+                            v = nv;
+                        }
+                    }
+                    d[i][w] = v;
+                }
+            }
 
             int maxValue = 0;
-            for (int i = 0; i < mws.size(); i++) {
-                ArrayList<Boolean> used = new ArrayList<>();
-                for (int j = 0; j < n; j++) {
-                    used.add(false);
-                }
-
+            for (int i = 0; i < mws.size(); i++) {         
                 int mw = mws.get(i);
-                int p = calObjs(objs, used, 0, n, mw, 0);
-                maxValue += p;
+                maxValue += d[n][mw];
             }
 
             System.out.println(maxValue);
