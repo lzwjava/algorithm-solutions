c9988496693e673ce1d86db01cdedece31dcf246
lzwjava <lzwjava@gmail.com>
2021-08-17 00:44:01 +0800

    a

--- 11984/src/Main.java
+++ 11984/src/Main.java
@@ -3,11 +3,28 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.StringTokenizer;
 
 public class Main {
+    
+    double f2c(int df) {
+        return df * 5.0 / 9;
+    }
    
     void solve() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));           
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        int caseNum = 1;
+        while (t > 0) {
+            String line = br.readLine();
+            StringTokenizer st = new StringTokenizer(line);
+            int C = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            double nc = C + f2c(d);
+            System.out.println(String.format("Case %d: %.2f", caseNum, nc));
+            caseNum++;
+            t--;
+        }
         br.close();
     }
 
--- java_sample_sc/src/Main.java
+++ java_sample_sc/src/Main.java
@@ -1,14 +1,12 @@
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class Main {
    
-    void solve() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));           
-        br.close();
+    void solve() {
+        Scanner sc = new Scanner(System.in);    
+        sc.close();
     }
 
     public static void main(String[] args) throws Exception {
