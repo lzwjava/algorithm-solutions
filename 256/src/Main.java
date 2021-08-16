ae6e1f242d7eedbc71a0592391e75eed6af33f1a
lzwjava <lzwjava@gmail.com>
2021-08-17 00:10:11 +0800

    a

--- 256/src/Main.java
+++ 256/src/Main.java
@@ -1,12 +1,49 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.Scanner;
 
 public class Main {
+
+    String numsToString(int nums[], int i, int j) {
+        StringBuilder sb = new StringBuilder();
+        for (int k = i; k < j; k++) {
+            sb.append(nums[k]);
+        }
+        return sb.toString();
+    }
+
+    void permutation(int nums[], int i, int n) {
+        if (i == n) {            
+            String a = numsToString(nums, 0, n/2);        
+            String b = numsToString(nums, n / 2, n);           
+            int s = Integer.parseInt(a) + Integer.parseInt(b);
+
+            String c = numsToString(nums, 0, n);            
+            if (s * s == Integer.parseInt(c)) {
+                System.out.println(c);
+            }
+            return;
+        }
+        for (int j = 0; j <= 9; j++) {
+            nums[i] = j;
+            permutation(nums, i+1, n);
+        }
+    }
    
-    void solve() {
-        Scanner sc = new Scanner(System.in);    
-        sc.close();
+    void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String line = br.readLine();
+            if (line == null) {
+                break;
+            }
+            int n = Integer.parseInt(line);
+            int nums[] = new int[n];
+            permutation(nums, 0, n);
+        }
+        br.close();
     }
 
     public static void main(String[] args) throws Exception {
