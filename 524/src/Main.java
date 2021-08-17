4fa0e5cbce565269427ae121bbe3a9502dcfbcc1
lzwjava <lzwjava@gmail.com>
2021-08-17 23:37:09 +0800

    a

--- 524/src/Main.java
+++ 524/src/Main.java
@@ -3,32 +3,31 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Main {
 
-    ArrayList<Integer> primes;
+    boolean isp[];
 
-    boolean isPrime(int x) {
-        return primes.contains(x);
-    }
+    PrintWriter out;
 
     void permutation(int nums[], boolean vis[], int i, int n) {
         if (i == n) {
-            if (isPrime(nums[n - 1] + nums[0])) {
+            if (isp[nums[n - 1] + nums[0]]) {
                 for (int k = 0; k < n; k++) {
                     if (k != 0) {
-                        System.out.print(" ");
+                        out.append(" ");
                     }
-                    System.out.print(nums[k]);
+                    out.append(nums[k]+"");
                 }
-                System.out.println();
-            }            
+                out.append("\n");
+            }
             return;
         }
         for (int k = 1; k <= n; k++) {
             if (!vis[k]) {
-                if (isPrime(k + nums[i - 1])) {
+                if (isp[k + nums[i - 1]]) {
                     nums[i] = k;
                     vis[k] = true;
                     permutation(nums, vis, i + 1, n);
@@ -39,10 +38,12 @@ public class Main {
     }
    
     void solve() throws IOException {
-        primes = new ArrayList<>();
-        for (int i = 2; i <= 32; i++) {
+        int size = 35;
+        isp = new boolean[size];
+        ArrayList<Integer> primes = new ArrayList<>();
+        for (int i = 2; i < size; i++) {
             boolean ok = true;
-            int si = (int)Math.sqrt(i);
+            int si = (int) Math.sqrt(i);
             for (int j = 0; j < primes.size(); j++) {
                 int pj = primes.get(j);
                 if (pj > si) {
@@ -56,23 +57,34 @@ public class Main {
                 primes.add(i);
             }
         }
+        for (Integer p : primes) {
+            isp[p] = true;
+        }
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int caseNum = 1;
+        out = new PrintWriter(System.out);
+        boolean first = true;
         while (true) {
             String s = br.readLine();
             if (s == null) {
                 break;
             }
             int n = Integer.parseInt(s);
-            System.out.println(String.format("Case %d:", caseNum));            
-            boolean vis[] = new boolean[n+1];
+            if (first) {
+                first = false;
+            } else {
+                out.append("\n");
+            }
+            out.append(String.format("Case %d:\n", caseNum));
+            boolean vis[] = new boolean[n + 1];
             int nums[] = new int[n];
             nums[0] = 1;
             vis[1] = true;
             permutation(nums, vis, 1, n);
-            System.out.println();
             caseNum++;
         }
+        out.flush();
         br.close();
     }
 
