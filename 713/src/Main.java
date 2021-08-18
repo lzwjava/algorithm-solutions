b28c14885c01f7eea1ee4ce34f445e64125d8ae9
lzwjava <lzwjava@gmail.com>
2021-08-18 23:08:16 +0800

    a

--- 713/src/Main.java
+++ 713/src/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -12,10 +14,31 @@ public class Main {
 
     Main() {
         in = new BufferedReader(new InputStreamReader(System.in));
-        out = new PrintWriter(System.out);        
+        out = new PrintWriter(System.out);
+    }
+    
+    String reverse(String s) {
+        StringBuilder sb = new StringBuilder();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            sb.append(s.charAt(len-1-i));
+        }
+        return sb.toString();
     }
    
     void solve() throws IOException {
+        String ns = in.readLine();
+        int n = Integer.parseInt(ns);
+        for (int i = 0; i < n; i++) {
+            String s = in.readLine();
+            StringTokenizer st = new StringTokenizer(s);
+            String a = reverse(st.nextToken());
+            String b = reverse(st.nextToken());
+            BigInteger c = new BigInteger(a).add(new BigInteger(b));
+            String rs = reverse(c.toString());
+            String rss = new BigInteger(rs).toString();
+            out.append(rss).append('\n');
+        }
     }
 
     void close() throws IOException {
