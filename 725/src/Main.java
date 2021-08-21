740ae25d8af91ccb1b37711701e96d7362ffdae0
lzwjava <lzwjava@gmail.com>
2021-08-21 23:06:58 +0800

    a

--- 725/src/Main.java
+++ 725/src/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 public class Main {
 
@@ -26,13 +27,36 @@ public class Main {
     }
     
     void permutation(int nums[], boolean vis[], int i) {
-        if (i == 10) {
-            String str1 = numsToStr(nums, 0, 5);
-            String str2 = numsToStr(nums, 5, 10);
-            int a1 = Integer.parseInt(str1);
-            int a2 = Integer.parseInt(str2);
-            if (a2 != 0 && a1 == n * a2) {
-                System.out.println(String.format("%s / %s = %d", str1, str2, n));
+        if (i == 5) {
+            String str2 = numsToStr(nums, 0, 5);
+            int a2 = Integer.parseInt(str2);                        
+            int a1 = n * a2;
+            String str1 = String.format("%d", a1);
+            if (str1.length() > 5 || str1.length() < 4) {
+                return;
+            }
+            if (str1.length() == 4) {
+                str1 = "0" + str1;
+            }
+            int nnums[] = Arrays.copyOf(nums, 10);            
+            for (int j = 0; j < str1.length(); j++) {
+                char ch = str1.charAt(j);
+                int num = ch - '0';
+                nnums[5 + j] = num;
+            }
+            boolean vis1[] = new boolean[10];
+            boolean ok = true;
+            for (int j = 0; j < nnums.length; j++) {
+                int nnum = nnums[j];
+                if (vis1[nnum]) {
+                    ok = false;
+                    break;
+                } else {
+                    vis1[nnum] = true;
+                }
+            }
+            if (ok) {
+                out.append(String.format("%s / %s = %d\n", str1, str2, n));
                 found = true;
             }
             return;
@@ -54,17 +78,17 @@ public class Main {
             if (n == 0) {
                 break;
             }
-            int nums[] = new int[10];
+            int nums[] = new int[5];
             found = false;
             boolean vis[] = new boolean[10];
             if (first) {
                 first = false;
             } else {
-                System.out.println();
+                out.append('\n');
             }
             permutation(nums, vis, 0);
             if (!found) {
-                System.out.println(String.format("There are no solutions for %d.", n));
+                out.append(String.format("There are no solutions for %d.\n", n));
             }
         }
     }
