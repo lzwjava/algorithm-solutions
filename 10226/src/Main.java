b712c9a86731469d3d41e6c7123be5b832ce720c
lzwjava <lzwjava@gmail.com>
2021-08-26 11:26:50 +0800

    a

--- 10226/src/Main.java
+++ 10226/src/Main.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class Main {
 
@@ -16,7 +19,36 @@ public class Main {
     }
    
     void solve() throws IOException {
-        
+        int t = Integer.parseInt(in.readLine());
+        in.readLine();
+        while (t > 0) {
+            HashMap<String, Integer> trees = new HashMap<>();
+            int total = 0;
+            while (true) {
+                String name = in.readLine();
+                if (name == null || name.isEmpty()) {
+                    break;
+                }
+                Integer count = trees.get(name);
+                if (count == null) {
+                    count = 0;
+                }
+                count++;
+                trees.put(name, count);
+                total++;
+            }
+            ArrayList<String> names = new ArrayList<>();
+            names.addAll(trees.keySet());
+            Collections.sort(names);
+            for (String name : names) {
+                Integer count = trees.get(name);
+                out.append(String.format("%s %.4f\n", name, count * 100.0 / total));
+            }
+            t--;
+            if (t != 0) {
+                out.append('\n');
+            }
+        }       
     }
 
     void close() throws IOException {
