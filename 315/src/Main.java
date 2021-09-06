1533596b93ab2c3e8fa8010f589043d5ab918a3d
lzwjava <lzwjava@gmail.com>
2021-09-06 22:50:14 +0800

    a

--- 315/src/Main.java
+++ 315/src/Main.java
@@ -4,18 +4,65 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
     BufferedReader in;
     PrintWriter out;
+    boolean grid[][];
+    int n;
 
     Main() {
         in = new BufferedReader(new InputStreamReader(System.in));
-        out = new PrintWriter(System.out);        
+        out = new PrintWriter(System.out);
+    }
+    
+    void dfs(int skip, int start, boolean vis[]) {
+        vis[start] = true;
+        for (int i = 0; i < n; i++) {
+            if (i != skip && grid[start][i] && !vis[i]) {
+                dfs(skip, i, vis);
+            }
+        }
     }
    
     void solve() throws IOException {
+        while (true) {
+            n = Integer.parseInt(in.readLine());
+            if (n == 0) {
+                break;
+            }
+            grid = new boolean[n][n];
+            while (true) {
+                StringTokenizer st = new StringTokenizer(in.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                if (a == 0) {
+                    break;
+                }
+                while (st.hasMoreTokens()) {
+                    int b = Integer.parseInt(st.nextToken());
+                    grid[a - 1][b - 1] = grid[b - 1][a - 1] = true;
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                boolean vis[] = new boolean[n];
+                int j = (i + 1) % n;
+                dfs(i, j, vis);
+                boolean critical = false;
+                for (int k = 0; k < n; k++) {
+                    if (k != i && !vis[k]) {
+                        critical = true;
+                        break;
+                    }
+                }
+                if (critical) {
+                    count++;
+                }
+            }
+            out.append(String.format("%d\n", count));
+        }
     }
 
     void close() throws IOException {
