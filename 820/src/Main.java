e42db6b093a86a0584e0f6042cfac898c63b3488
Zhiwei <lzw@wuhuarongdeMacBook-Pro.local>
2021-10-01 18:30:47 +0800

    a

--- 820/src/Main.java
+++ 820/src/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,6 +17,52 @@ public class Main {
         in = new BufferedReader(new InputStreamReader(System.in));
         out = new PrintWriter(System.out);        
     }
+
+    class Edge implements Comparable<Edge>{
+        int x;
+        int y;
+        int bandwidth;
+
+        Edge(){            
+        }
+
+        Edge(int x, int y, int bandwidth){
+            this.x = x;
+            this.y = y;
+            this.bandwidth = bandwidth;
+        }
+
+        @Override
+        public int compareTo(Main.Edge o) {
+            return bandwidth - o.bandwidth;
+        }
+    }
+
+    int getGroup(int[] groups, int x){
+        if (groups[x] == x){
+            return x;
+        } else {
+            return getGroup(groups, groups[x]);
+        }
+    }
+
+    int maxBandwidth;
+
+    void dfs(int[][] graph, boolean vis[], int n, int s, int t, int bandwidth){
+        if (s == t){
+            if (bandwidth > maxBandwidth){
+                maxBandwidth = bandwidth;
+            }
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if (graph[s][i] > 0 && !vis[i]){
+                vis[i] = true;
+                dfs(graph, vis, n, i, t, bandwidth + graph[s][i]);
+                vis[i] = false;                
+            }
+        }
+    }
    
     void solve() throws IOException {
         int caseNum = 1;
@@ -25,11 +73,50 @@ public class Main {
             }
             String str = in.readLine();
             StringTokenizer st = new StringTokenizer(str);
-            int s = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            out.append(String.format("Network %d\n", caseNum));
-            out.append(String.format("The bandwidth is %d.", 0));
+            int s = Integer.parseInt(st.nextToken()) - 1;
+            int t = Integer.parseInt(st.nextToken())  -1;
+            int c = Integer.parseInt(st.nextToken());            
+            ArrayList<Edge> edges = new ArrayList<>();
+            for(int i=0;i<c;i++){         
+                st = new StringTokenizer(in.readLine());
+                int x,y,b;
+                x = Integer.parseInt(st.nextToken());                                                                                             
+                y = Integer.parseInt(st.nextToken());
+                b = Integer.parseInt(st.nextToken());                
+                Edge e = new Edge(x-1, y-1,b);
+                edges.add(e);
+            }
+            Collections.sort(edges);
+            int[] groups = new int[n];
+            for(int i=0;i<n;i++){
+                groups[i] = i;
+            }
+            int i;            
+            for(i=0;i<edges.size();i++){
+                Edge e = edges.get(i);
+                int gx = getGroup(groups, e.x);
+                int gy = getGroup(groups, e.y);
+                if (gx!=gy){
+                    groups[gy] = gx;
+                    int gs = getGroup(groups, s);
+                    int gt = getGroup(groups, t);
+                    if (gs == gt){
+                        break;
+                    }
+                }
+            }            
+            int[][] graph = new int[n][n];
+            for(int j=0;j<=i;j++)          {
+                Edge e = edges.get(j);
+                graph[e.x][e.y] = graph[e.y][e.x] = e.bandwidth;
+            }
+            boolean[] vis = new boolean[n];
+            vis[s] = true;
+            out.append(String.format("Network %d\n", caseNum));            
+            maxBandwidth = 0;
+            dfs(graph,vis, n, s, t, 0);                        
+            out.append(String.format("The bandwidth is %d.\n", maxBandwidth));
+            out.append('\n');
             caseNum++;
         }
     }
@@ -50,9 +137,9 @@ public class Main {
         boolean isLocal = System.getProperty("os.name").equals("Mac OS X");        
         if (isLocal) {
             inStream = new FileInputStream("1.in");
-            // outStream = new PrintStream("1.out");
+            outStream = new PrintStream("1.out");
             System.setIn(inStream);
-            // System.setOut(outStream);
+            System.setOut(outStream);
         }
 
         Main main = new Main();
--- 834/src/Main.java
+++ 834/src/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -16,6 +18,39 @@ public class Main {
     }
    
     void solve() throws IOException {
+        while(true){
+            String s = in.readLine();
+            if (s == null){
+                break;
+            }
+            StringTokenizer st = new StringTokenizer(s);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            ArrayList<Integer> nums = new ArrayList<>();
+            while(a %b != 0){
+                int c = a / b;
+                nums.add(c);
+                a -= c * b;
+                int t = a;
+                a = b;
+                b =t;    
+            }
+            int c = a /b;
+            nums.add(c);
+            
+            out.append("[");
+            for(int i =0;i<nums.size();i++) {
+                out.append(String.valueOf(nums.get(i)));
+                if (i==0){
+                    out.append(";");
+                } else if (i==nums.size()-1) {
+                    continue;
+                } else {
+                    out.append(",");
+                }
+            }
+            out.append("]\n");
+        }
     }
 
     void close() throws IOException {
