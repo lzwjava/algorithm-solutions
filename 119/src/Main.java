b7706da36f2ed586d435d22663ce876aa31b70d5
lzwjava <lzwjava@gmail.com>
2021-10-15 01:53:38 +0800

    a

--- 119/src/Main.java
+++ 119/src/Main.java
@@ -1,12 +1,82 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.Scanner;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+    
+    class Giving {
+        String from;
+        int money;
+        int toCount;
+        String[] toNames;
+        
+        Giving(String from, int money, int toCount, String[] toNames) {
+            this.from = from;
+            this.money = money;
+            this.toCount = toCount;
+            this.toNames = toNames;
+        }
+    }
    
-    void solve() {
-        Scanner sc = new Scanner(System.in);    
-        sc.close();
+    void solve() throws IOException {
+        String line = in.readLine();        
+        while (true) {        
+            int n = Integer.parseInt(line);
+            String[] names = new String[n];
+            line = in.readLine();
+            StringTokenizer st = new StringTokenizer(line);
+            for (int i = 0; i < n; i++) {
+                names[i] = st.nextToken();
+            }
+            ArrayList<Giving> givings = new ArrayList<>();
+            while (true) {
+                line = in.readLine();
+                if (line == null) {
+                    break;
+                }
+                st = new StringTokenizer(line);
+                int count = st.countTokens();
+                if (count == 1) {
+                    break;
+                }
+                String from = st.nextToken();
+                int money = Integer.parseInt(st.nextToken());
+                int toCount = Integer.parseInt(st.nextToken());
+                String[] toNames = new String[toCount];
+                for (int i = 0; i < toCount; i++) {
+                    String toName = st.nextToken();
+                    toNames[i] = toName;
+                }
+                Giving giving = new Giving(from, money, toCount, toNames);
+                givings.add(giving);
+            }                                
+            if (line == null){
+                break;
+            }
+        }
+    }
+
+    void close() throws IOException {
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.flush();
+            out.close();              
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -20,7 +90,9 @@ public class Main {
             // System.setOut(outStream);
         }
 
-        new Main().solve();
+        Main main = new Main();
+        main.solve();
+        main.close();
 
         if (isLocal) {
             inStream.close();
