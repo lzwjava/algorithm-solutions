4207f506742d6b96743b08241394b0f5ece22d30
lzwjava <lzwjava@gmail.com>
2021-10-17 02:07:26 +0800

    a

---
+++ 138/src/Main.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);        
+    }
+   
+    void solve() throws IOException {
+        int t = 0;
+        for (int n = 1; n <= 11309768; n++) {
+            long s = (long)n * (n + 1) / 2;
+            long k = (long)Math.sqrt(s);
+            if (k * k == s) {
+                out.append(String.format("%10d%10d\n", k,n));
+                t++;
+                if (t == 10) {
+                    break;                    
+                }
+            }
+        }
+    }
+
+    void close() throws IOException {
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.flush();
+            out.close();              
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inStream = null;
+        PrintStream outStream = null;
+        boolean isLocal = System.getProperty("os.name").equals("Mac OS X");        
+        if (isLocal) {
+            inStream = new FileInputStream("1.in");
+            // outStream = new PrintStream("1.out");
+            System.setIn(inStream);
+            // System.setOut(outStream);
+        }
+
+        Main main = new Main();
+        main.solve();
+        main.close();
+
+        if (isLocal) {
+            inStream.close();
+            // outStream.close();
+        }
+    }
+}
