3c172753ed8da9dd05a0b9b713dc1d35e175daef
lzwjava <lzwjava@gmail.com>
2021-10-20 03:05:40 +0800

    a

--- 574/src/Main.java
+++ 574/src/Main.java
@@ -28,7 +28,7 @@ public class Main {
             int[] nums = new int[n];
             for (int i = 0; i < n; i++) {
                 nums[i] = Integer.parseInt(st.nextToken());
-            }                                                                                                
+            }
         }
     }
 
