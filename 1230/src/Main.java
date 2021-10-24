089cf6dea202376e7d7842d3e8f7aad425a0d03c
lzwjava <lzwjava@gmail.com>
2021-10-25 03:59:06 +0800

    a

---
+++ 1230/src/Main.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);        
+    }
+   
+    void solve() throws IOException {
+        int c = Integer.parseInt(in.readLine());
+        while (c > 0) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int z = BigInteger.valueOf(x).modPow(BigInteger.valueOf(y), BigInteger.valueOf(n)).intValue();
+            out.append(String.format("%d\n", z));            
+            c--;
+        }
+    }
+
+    void close() throws IOException {
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.flush();
+            out.close();              
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inStream = null;
+        PrintStream outStream = null;
+        boolean isLocal = System.getProperty("os.name").equals("Mac OS X");        
+        if (isLocal) {
+            inStream = new FileInputStream("1.in");
+            // outStream = new PrintStream("1.out");
+            System.setIn(inStream);
+            // System.setOut(outStream);
+        }
+
+        Main main = new Main();
+        main.solve();
+        main.close();
+
+        if (isLocal) {
+            inStream.close();
+            // outStream.close();
+        }
+    }
+}
