8e1133bb8f7f8ce465cfd7bed8997eb8d64470aa
lzwjava <lzwjava@gmail.com>
2021-10-30 22:08:41 +0800

    a

--- 1230/src/Main.java
+++ 1230/src/Main.java
@@ -43,7 +43,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         FileInputStream inStream = null;
         PrintStream outStream = null;
-        boolean isLocal = System.getProperty("os.name").equals("Linux");        
+        boolean isLocal = System.getenv("LOCAL_JUDGE") != null;
         if (isLocal) {
             inStream = new FileInputStream("1.in");
             // outStream = new PrintStream("1.out");
--- java_sample/src/Main.java
+++ java_sample/src/Main.java
@@ -31,7 +31,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         FileInputStream inStream = null;
         PrintStream outStream = null;
-        boolean isLocal = System.getProperty("os.name").equals("Mac OS X");        
+        boolean isLocal = System.getenv("LOCAL_JUDGE") != null;
         if (isLocal) {
             inStream = new FileInputStream("1.in");
             // outStream = new PrintStream("1.out");
