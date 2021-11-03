e896c3e3504b1dfd9d4e7ff8726e6551ee84eec4
lzwjava <lzwjava@gmail.com>
2021-11-03 21:06:36 +0800

    a

--- 278/src/Main.java
+++ 278/src/Main.java
@@ -1,9 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -17,124 +12,6 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
-    char[] pieces = new char[]{'r', 'n', 'q', 'k'};
-
-    int indexOfPiece(char ch) {
-        for (int i = 0; i < pieces.length; i++) {
-            if (pieces[i] == ch) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    class Pos {
-        int x;
-        int y;
-        char ch;
-
-        Pos() {
-        }
-
-        Pos(char ch, int x, int y) {
-            this.ch = ch;
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    int[] dx = {-1, 0, 1, 0, -1, 1, -1, 1};
-    int[] dy = {0, -1, 0, 1, -1, 1, 1, -1};
-
-    int[] kx = {-1, -1, 1, 1, 2, 2, -2, -2};
-    int[] ky = {-2, 2, -2, 2, -1, 1, -1, 1};
-
-    boolean kill(Pos a, Pos b) {
-        if (a.ch == 'r') {
-            return a.x == b.x || a.y == b.y;
-        } else if (a.ch == 'n') {
-            for (int i = 0; i < 8; i++) {
-                int nx = a.x + kx[i];
-                int ny = a.y + ky[i];
-                if (nx == b.x && ny == b.y) {
-                    return true;
-                }
-            }
-        } else if (a.ch == 'q') {
-            return a.x == b.x || a.y == b.y || a.x + a.y == b.x + b.y || a.x - a.y == b.x - b.y;
-        } else if (a.ch == 'k') {
-            for (int i = 0; i < 8; i++) {
-                int nx = a.x + dx[i];
-                int ny = a.y + dy[i];
-                if (nx == b.x && ny == b.y) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    boolean check(ArrayList<Pos> positions, int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j) {
-                    if (kill(positions.get(i), positions.get(j))) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-
-    int maxn;
-
-    void permutation(ArrayList<Pos> positions, char piece, int cur, int m, int n) {
-        if (cur > maxn) {
-            maxn = cur;
-        }
-        int sx, sy;
-        if (cur == 0) {
-            sx = 0;
-            sy = 0;
-        } else {
-            Pos p = positions.get(cur - 1);
-//            int sum = p.x * m + p.y;
-            sx = p.x;
-            sy = p.y;
-        }
-        for (int x = sx; x < m; x++) {
-            for (int y = sy; y < n; y++) {
-                boolean ok = true;
-                for (int i = 0; i < cur; i++) {
-                    if (positions.get(i).x == x && positions.get(i).y == y) {
-                        ok = false;
-                        break;
-                    }
-                }
-                if (ok) {
-                    Pos pos = new Pos(piece, x, y);
-                    positions.add(pos);
-                    if (check(positions, cur + 1)) {
-                        permutation(positions, piece, cur + 1, m, n);
-                    }
-                    positions.remove(pos);
-                }
-            }
-        }
-    }
-
-    char[] removePiece(char ch) {
-        char[] chs = new char[3];
-        int p = 0;
-        for (int i = 0; i < 4; i++) {
-            if (pieces[i] != ch) {
-                chs[p++] = pieces[i];
-            }
-        }
-        return chs;
-    }
-
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
@@ -142,11 +19,17 @@ public class Main {
             char ch = st.nextToken().charAt(0);
             int m = Integer.parseInt(st.nextToken());
             int n = Integer.parseInt(st.nextToken());
-            maxn = 0;
-            ArrayList<Pos> positions = new ArrayList<Pos>();
-
-            permutation(positions, ch, 0, m, n);
-            out.append(String.format("%d\n", maxn));
+            int ans;
+            if (ch == 'r') {
+                ans = Math.min(m, n);
+            } else if (ch == 'k') {
+                ans = m * n / 2;
+            } else if (ch == 'Q') {
+                ans = Math.min(m, n);
+            } else {
+                ans = (int) ((m + 1) / 2) * (int) ((n + 1) / 2);
+            }
+            out.append(String.format("%d\n", ans));
             t--;
         }
     }
