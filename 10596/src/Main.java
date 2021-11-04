b78d8b248371c805b20a7186d1006965cb020e93
lzwjava <lzwjava@gmail.com>
2021-11-05 02:12:11 +0800

    a

---
+++ 10596/src/Main.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    class Pair {
+        int c1, c2;
+
+        Pair(int c1, int c2) {
+            this.c1 = c1;
+            this.c2 = c2;
+        }
+    }
+
+    void solve() throws IOException {
+        while (true) {
+            String line = in.readLine();
+            if (line == null) {
+                break;
+            }
+            StringTokenizer st = new StringTokenizer(line);
+            int n = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            Pair[] pairs = new Pair[r];
+            for (int i = 0; i < r; i++) {
+                st = new StringTokenizer(in.readLine());
+                int c1 = Integer.parseInt(st.nextToken());
+                int c2 = Integer.parseInt(st.nextToken());
+                pairs[i] = new Pair(c1, c2);
+            }
+            
+        }
+    }
+
+    void close() throws IOException {
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.flush();
+            out.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inStream = null;
+        PrintStream outStream = null;
+        boolean isLocal = System.getenv("LOCAL_JUDGE") != null;
+        if (isLocal) {
+            inStream = new FileInputStream("1.in");
+            // outStream = new PrintStream("1.out");
+            System.setIn(inStream);
+            // System.setOut(outStream);
+        }
+
+        Main main = new Main();
+        main.solve();
+        main.close();
+
+        if (isLocal) {
+            inStream.close();
+            // outStream.close();
+        }
+    }
+}
