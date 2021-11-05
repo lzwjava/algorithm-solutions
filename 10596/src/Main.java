83396e75cfbff9176eb9fdb9b8616a9b4ac3fd53
lzwjava <lzwjava@gmail.com>
2021-11-05 21:57:20 +0800

    a

--- 10596/src/Main.java
+++ 10596/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,14 +12,18 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
-    class Pair {
-        int c1, c2;
-
-        Pair(int c1, int c2) {
-            this.c1 = c1;
-            this.c2 = c2;
+    void dfs(int x, int n) {
+        vis[x] = true;
+        for (int i = 0; i < n; i++) {
+            if (grid[x][i] && !vis[i]) {
+                dfs(i, n);
+            }
         }
     }
+
+    int[] deg;
+    boolean[] vis;
+    boolean[][] grid;
 
     void solve() throws IOException {
         while (true) {
@@ -29,14 +34,45 @@ public class Main {
             StringTokenizer st = new StringTokenizer(line);
             int n = Integer.parseInt(st.nextToken());
             int r = Integer.parseInt(st.nextToken());
-            Pair[] pairs = new Pair[r];
+            deg = new int[n];
+            vis = new boolean[n];
+            Arrays.fill(vis, true);
+            grid = new boolean[n][n];
             for (int i = 0; i < r; i++) {
                 st = new StringTokenizer(in.readLine());
                 int c1 = Integer.parseInt(st.nextToken());
                 int c2 = Integer.parseInt(st.nextToken());
-                pairs[i] = new Pair(c1, c2);
+                deg[c1]++;
+                deg[c2]++;
+                grid[c1][c2] = grid[c2][c1] = true;
+                vis[c1] = vis[c2] = false;
             }
-            
+            boolean ok = true;
+            for (int i = 0; i < n; i++) {
+                if (deg[i] % 2 == 1) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (!ok) {
+                out.append(String.format("Not Possible\n"));
+            } else {
+                int c = 0;
+                for (int i = 0; i < n; i++) {
+                    if (!vis[i]) {
+                        dfs(i, n);
+                        c++;
+                        if (c > 1) {
+                            break;
+                        }
+                    }
+                }
+                if (c == 1) {
+                    out.append("Possible\n");
+                } else {
+                    out.append("Not Possible\n");
+                }
+            }
         }
     }
 
@@ -56,9 +92,9 @@ public class Main {
         boolean isLocal = System.getenv("LOCAL_JUDGE") != null;
         if (isLocal) {
             inStream = new FileInputStream("1.in");
-            // outStream = new PrintStream("1.out");
+//            outStream = new PrintStream("2.out");
             System.setIn(inStream);
-            // System.setOut(outStream);
+//            System.setOut(outStream);
         }
 
         Main main = new Main();
