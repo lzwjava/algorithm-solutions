b8fa66b4ef58e403f38a8173ef7db451f8f25093
lzwjava <lzwjava@gmail.com>
2021-11-09 19:54:57 +0800

    a

---
+++ 11152/src/Main.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    void solve() throws IOException {
+        while (true) {
+            String line = in.readLine();
+            if (line == null) {
+                break;
+            }
+            StringTokenizer st = new StringTokenizer(line);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            double s = a * b * 1.0 / 2;
+            double r = s * 2 * 1.0 / (a + b + c);
+            double roses = Math.PI * r * r;
+            double violets = s - roses;
+            double r2 = c * 1.0 / 2;
+            double sunflowers = Math.PI * r2 * r2 - s;
+            out.append(String.format("%.4f %.4f %.4f\n", sunflowers, violets, roses));
+        }
+    }
+
+    void close() throws IOException {
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.flush();
+            out.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inStream = null;
+        PrintStream outStream = null;
+        boolean isLocal = System.getenv("LOCAL_JUDGE") != null;
+        if (isLocal) {
+            inStream = new FileInputStream("1.in");
+            // outStream = new PrintStream("1.out");
+            System.setIn(inStream);
+            // System.setOut(outStream);
+        }
+
+        Main main = new Main();
+        main.solve();
+        main.close();
+
+        if (isLocal) {
+            inStream.close();
+            // outStream.close();
+        }
+    }
+}
