fa86b94f4ec04fe9b22ef65804bda65a7038dc8b
lzwjava <lzwjava@gmail.com>
2021-11-09 20:03:43 +0800

    a

--- 11152/src/Main.java
+++ 11152/src/Main.java
@@ -21,12 +21,15 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            double s = a * b * 1.0 / 2;
-            double r = s * 2 * 1.0 / (a + b + c);
+            double s = (a + b + c) * 1.0 / 2;
+            double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+            double r = area * 2 * 1.0 / (a + b + c);
             double roses = Math.PI * r * r;
-            double violets = s - roses;
-            double r2 = c * 1.0 / 2;
-            double sunflowers = Math.PI * r2 * r2 - s;
+            double violets = area - roses;
+            double cosA = (b * b + c * c - a * a) * 1.0 / (2 * b * c);
+            double sinA = Math.sqrt(1 - cosA * cosA);
+            double r2 = a * 1.0 / (sinA * 2);
+            double sunflowers = Math.PI * r2 * r2 - area;
             out.append(String.format("%.4f %.4f %.4f\n", sunflowers, violets, roses));
         }
     }
