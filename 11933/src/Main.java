9780c18a7a386c4d548534afbd3e3144df05a725
lzwjava <lzwjava@gmail.com>
2021-11-11 15:10:34 +0800

    a

--- 11933/src/Main.java
+++ 11933/src/Main.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
@@ -12,10 +9,40 @@ public class Main {
 
     Main() {
         in = new BufferedReader(new InputStreamReader(System.in));
-        out = new PrintWriter(System.out);        
+        out = new PrintWriter(System.out);
     }
-   
+
     void solve() throws IOException {
+        while (true) {
+            int n = Integer.parseInt(in.readLine());
+            if (n == 0) {
+                break;
+            }
+            String s = Integer.toBinaryString(n);
+            ArrayList<Integer> indices = new ArrayList<Integer>();
+            int len = s.length();
+            for (int i = 0; i < len; i++) {
+                int idx = len - 1 - i;
+                char ch = s.charAt(idx);
+                if (ch == '1') {
+                    indices.add(idx);
+                }
+            }
+            char[] a = new char[len];
+            Arrays.fill(a, '0');
+            char[] b = new char[len];
+            Arrays.fill(b, '0');
+            for (int i = 0; i < indices.size(); i++) {
+                if (i % 2 == 0) {
+                    a[indices.get(i)] = '1';
+                } else {
+                    b[indices.get(i)] = '1';
+                }
+            }
+            int an = Integer.parseInt(new String(a), 2);
+            int bn = Integer.parseInt(new String(b), 2);
+            out.append(String.format("%d %d\n", an, bn));
+        }
     }
 
     void close() throws IOException {
@@ -24,7 +51,7 @@ public class Main {
         }
         if (out != null) {
             out.flush();
-            out.close();              
+            out.close();
         }
     }
 
