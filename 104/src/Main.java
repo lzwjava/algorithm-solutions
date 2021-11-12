9003b31147e993dac72cb373e439cbbd51d65336
lzwjava <lzwjava@gmail.com>
2021-11-13 01:37:50 +0800

    a

--- 104/src/Main.java
+++ 104/src/Main.java
@@ -1,7 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
-import java.util.concurrent.ArrayBlockingQueue;
 
 public class Main {
 
@@ -13,24 +11,6 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
-    class State {
-        int idx;
-        double v;
-        int dist;
-        ArrayList<Integer> path;
-
-        State() {
-            this.path = new ArrayList<Integer>();
-        }
-
-        State(int idx, double v, int dist) {
-            this.idx = idx;
-            this.v = v;
-            this.dist = dist;
-            this.path = new ArrayList<Integer>();
-        }
-    }
-
     void solve() throws IOException {
         while (true) {
             String line = in.readLine();
@@ -38,7 +18,8 @@ public class Main {
                 break;
             }
             int n = Integer.parseInt(line.trim());
-            double[][] table = new double[n][n];
+//            double[][] table = new double[n][n];
+            double[][][] d = new double[n + 1][n][n];
             for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(in.readLine());
                 for (int j = 0; j < n; j++) {
@@ -48,42 +29,30 @@ public class Main {
                     } else {
                         v = Double.parseDouble(st.nextToken());
                     }
-                    table[i][j] = v;
+                    d[1][i][j] = v;
+//                    table[i][j] = v;
                 }
             }
-            int minDist = Integer.MAX_VALUE;
-            ArrayList<Integer> minPath = null;
-            double maxValue = 0;
-            for (int i = 0; i < n; i++) {
-                ArrayBlockingQueue<State> queue = new ArrayBlockingQueue<State>(n * n * 1000);
-                State start = new State(i, 1.0, 0);
-                start.path.add(i);
-                queue.add(start);
-                boolean found = false;
-                int dist = Integer.MAX_VALUE;
-                ArrayList<Integer> path = null;
-                while (!queue.isEmpty()) {
-                    State st = queue.poll();
-                    if (st.dist >= n - 1) {
-                        continue;
-                    }
-                    for (int j = 0; j < n; j++) {
-                        if (j != st.idx) {
-                            double nv = st.v * table[st.idx][j];
-                            State newState = new State(j, nv, st.dist + 1);
-                            newState.path.addAll(st.path);
-                            newState.path.add(j);
-                            queue.add(newState);
-                            double fv = nv * table[j][i];
-                            if (fv > 1) {
-                                if (st.dist + 1 < dist || (st.dist + 1 == dist && fv > maxValue)) {
-                                    found = true;
-                                    dist = st.dist + 1;
-                                    path = newState.path;
-                                    path.add(i);
-                                    maxValue = fv;
-                                }
+            boolean found = false;
+            int[][][] q = new int[n + 1][n][n];
+            int l, i = 0, j = 0;
+            for (l = 2; l <= n; l++) {
+                for (i = 0; i < n; i++) {
+                    for (j = 0; j < n; j++) {
+                        double mv = 0;
+                        int mk = 0;
+                        for (int k = 0; k < n; k++) {
+                            double v = d[l - 1][i][k] * d[1][k][j];
+                            if (v > mv) {
+                                mk = k;
+                                mv = v;
                             }
+                        }
+                        q[l][i][j] = mk;
+                        d[l][i][j] = mv;
+                        if (i == j && mv > 1.01) {
+                            found = true;
+                            break;
                         }
                     }
                     if (found) {
@@ -91,23 +60,26 @@ public class Main {
                     }
                 }
                 if (found) {
-                    if (minDist > dist) {
-                        minDist = dist;
-                        minPath = path;
-                    }
+                    break;
                 }
             }
-            if (minDist == Integer.MAX_VALUE) {
+            if (!found) {
                 out.append("no arbitrage sequence exists\n");
             } else {
-                for (int i = 0; i < minPath.size(); i++) {
-                    if (i != 0) {
-                        out.append(' ');
-                    }
-                    out.append(String.format("%d", minPath.get(i) + 1));
-                }
+                print(q, l, i, j);
+                out.append(String.format("%d", i + 1));
                 out.append('\n');
             }
+        }
+    }
+
+    void print(int[][][] q, int l, int i, int j) {
+        if (l == 1) {
+            out.append(String.format("%d ", i + 1));
+        } else {
+            int k = q[l][i][j];
+            print(q, l - 1, i, k);
+            print(q, 1, k, j);
         }
     }
 
