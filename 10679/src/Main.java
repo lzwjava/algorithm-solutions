52a5251ec51a91dbc6c4e9c1ab163b241e838acb
lzwjava <lzwjava@gmail.com>
2021-11-16 04:23:35 +0800

    a

---
+++ 10679/src/Main.java
@@ -0,0 +1,61 @@
+import java.io.*;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    void solve() throws IOException {
+        int k = Integer.parseInt(in.readLine());
+        while (k > 0) {
+            String s = in.readLine();
+            int q = Integer.parseInt(in.readLine());
+            while (q > 0) {
+                String a = in.readLine();
+                if (s.indexOf(a) != -1) {
+                    out.append("y\n");
+                } else {
+                    out.append("n\n");
+                }
+                q--;
+            }
+            k--;
+        }
+    }
+
+    void close() throws IOException {
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.flush();
+            out.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inStream = null;
+        PrintStream outStream = null;
+        boolean isLocal = System.getenv("LOCAL_JUDGE") != null;
+        if (isLocal) {
+            inStream = new FileInputStream("1.in");
+            // outStream = new PrintStream("1.out");
+            System.setIn(inStream);
+            // System.setOut(outStream);
+        }
+
+        Main main = new Main();
+        main.solve();
+        main.close();
+
+        if (isLocal) {
+            inStream.close();
+            // outStream.close();
+        }
+    }
+}
