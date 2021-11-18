ed3d07b6d4dec33b1dec1a77d012c80741e8853a
lzwjava <lzwjava@gmail.com>
2021-11-19 03:37:16 +0800

    a

---
+++ 2467/src/com/lintcode/BoundedBlockingStack.java
@@ -0,0 +1,25 @@
+package com.lintcode;
+
+import java.util.*;
+
+public class BoundedBlockingStack {
+    // Construct method: Set the length of the stack
+    public BoundedBlockingStack(int capacity) {
+
+    }
+
+    // Add data in relative columns
+    public void push(int element) {
+
+    }
+
+    // Get the data in the stack head
+    public int pop() {
+        return 0;
+    }
+
+    // Get the size of the stack
+    public int size() {
+        return 0;
+    }
+}
