459d005aa509864ca461fd4d8e21dfea279cce9f
lzwjava <lzwjava@gmail.com>
2021-11-19 03:24:31 +0800

    a

---
+++ 2471/src/com/lintcode/ThreadSafeLinkedList.java
@@ -0,0 +1,17 @@
+package com.lintcode;
+
+public class ThreadSafeLinkedList {
+
+    public ThreadSafeLinkedList() {
+    }
+
+    public void appendLeft(int element) {
+    }
+
+    public void appendRight(int element) {
+    }
+
+    public ListNode getLinkedList() {
+        return null;
+    }
+}
