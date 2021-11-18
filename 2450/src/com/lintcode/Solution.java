8494a641f32b7f41ae8dab00d988e59b3571d6f6
lzwjava <lzwjava@gmail.com>
2021-11-19 04:07:35 +0800

    a

--- 2450/src/com/lintcode/Solution.java
+++ 2450/src/com/lintcode/Solution.java
@@ -3,7 +3,14 @@ package com.lintcode;
 import java.util.function.IntConsumer;
 
 public class Solution {
-    public void printHelloWorld(IntConsumer intConsumer) {
-        
+    public void printHelloWorld(IntConsumer intConsumer) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                intConsumer.accept(0);
+            }
+        });
+        thread.start();
+        thread.join();
     }
 }
