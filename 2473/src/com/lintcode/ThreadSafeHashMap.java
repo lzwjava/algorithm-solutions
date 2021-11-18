d0a7a15bf500ee819dbf5e09405e207e69a79503
lzwjava <lzwjava@gmail.com>
2021-11-19 03:22:05 +0800

    a

---
+++ 2473/src/com/lintcode/ThreadSafeHashMap.java
@@ -0,0 +1,20 @@
+package com.lintcode;
+
+public class ThreadSafeHashMap extends AbstractHashMap {
+
+    public ThreadSafeHashMap(int baseSize) {
+        super(baseSize);
+    }
+
+    public void put(int key, int value) {
+        synchronized (this) {
+            super.put(key, value);
+        }
+    }
+
+    public int get(int index) {
+        synchronized (this) {
+            return super.get(index);
+        }
+    }
+}
