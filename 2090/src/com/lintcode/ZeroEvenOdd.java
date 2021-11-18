28afd37a1ec07a8c3ebf30c87964f1c24e1a83bc
lzwjava <lzwjava@gmail.com>
2021-11-19 04:46:37 +0800

    a

---
+++ 2090/src/com/lintcode/ZeroEvenOdd.java
@@ -0,0 +1,19 @@
+package com.lintcode;
+
+import java.util.function.IntConsumer;
+
+class ZeroEvenOdd {
+
+    public ZeroEvenOdd(int n) {
+    }
+
+    public void zero(IntConsumer printZero) throws InterruptedException {
+        
+    }
+
+    public void even(IntConsumer printEven) throws InterruptedException {
+    }
+
+    public void odd(IntConsumer printOdd) throws InterruptedException {
+    }
+}
