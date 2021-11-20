44a5e2fe80b5c573484ea605a50f92e027bb0463
lzwjava <lzwjava@gmail.com>
2021-11-20 09:31:22 +0800

    a

--- 2453/src/com/lintcode/Solution.java
+++ 2453/src/com/lintcode/Solution.java
@@ -1,33 +1,37 @@
 package com.lintcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.function.Consumer;
 
 public class Solution {
     public void runSumInThread(int n, List<Integer> arr, Consumer<int[]> appendTheRangeAnswer) throws Exception {
-        CountDownLatch latch = new CountDownLatch(n);
-        int len = (int) Math.ceil(arr.size() / n);
-        List<Thread> workers = new ArrayList<>();
-        for (int i = 0; i < len; i++) {
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+        int len = (int) Math.ceil(arr.size() * 1.0 / n);
+        ArrayList<Future<?>> futures = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             int start = i * len;
             int end = (i + 1) * len;
             if (end > arr.size()) {
                 end = arr.size();
             }
-
             int finalEnd = end;
-            Thread thread = new Thread(() -> {
+            Future<?> future = executor.submit(() -> {
                 int sum = 0;
                 for (int j = start; j < finalEnd; j++) {
                     sum += arr.get(j);
                 }
                 appendTheRangeAnswer.accept(new int[]{start, finalEnd, sum});
-                latch.countDown();
             });
-            workers.add(thread);
+            futures.add(future);
         }
-        latch.await();
+        for (Future future : futures) {
+            future.get();
+        }
+        executor.shutdown();
     }
 }
