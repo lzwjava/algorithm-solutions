515e50b5892cc7d6500894759d8fd8c286a38e53
lzwjava <lzwjava@gmail.com>
2021-11-24 02:45:20 +0800

    a

--- 10496/src/Main.java
+++ 10496/src/Main.java
@@ -29,6 +29,43 @@ public class Main {
 
     int n, m;
 
+    Point st;
+
+    Point[] points;
+
+    int minDist;
+
+    void permutation(int[] nums, boolean[] vis, int cur, int k, int dist) {
+        if (dist > minDist) {
+            return;
+        }
+        if (cur == k) {
+            int finalDist = dist + calDist(points[nums[cur - 1]], st);
+            if (finalDist < minDist) {
+                minDist = finalDist;
+            }
+            return;
+        }
+        for (int i = 0; i < k; i++) {
+            if (!vis[i]) {
+                vis[i] = true;
+                nums[cur] = i;
+                Point last;
+                if (cur == 0) {
+                    last = st;
+                } else {
+                    last = points[nums[cur - 1]];
+                }
+                int ndist = dist + calDist(points[i], last);
+                permutation(nums, vis, cur + 1, k, ndist);
+                vis[i] = false;
+            }
+        }
+    }
+
+    private int calDist(Point a, Point b) {
+        return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+    }
 
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
@@ -36,12 +73,17 @@ public class Main {
             Point point = readPoint();
             n = point.x;
             m = point.x;
-            Point st = readPoint();
+            st = readPoint();
             int k = Integer.parseInt(in.readLine());
-            Point[] points = new Point[k];
+            points = new Point[k];
             for (int i = 0; i < k; i++) {
                 points[i] = readPoint();
             }
+            int[] nums = new int[k];
+            boolean[] vis = new boolean[k];
+            minDist = Integer.MAX_VALUE;
+            permutation(nums, vis, 0, k, 0);
+            out.append(String.format("The shortest path has length %d\n", minDist));
             t--;
         }
     }
