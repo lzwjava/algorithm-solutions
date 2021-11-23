457303be0ccda329384fdef15058b56737faafe6
lzwjava <lzwjava@gmail.com>
2021-11-23 22:26:32 +0800

    a

---
+++ 10940/src/Main.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    void solve() throws IOException {
+        while (true) {
+            int n = Integer.parseInt(in.readLine());
+            if (n == 0) {
+                break;
+            }
+            ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(n);
+            for (int i = 1; i <= n; i++) {
+                queue.add(i);
+            }
+            while (queue.size() != 1) {
+                queue.poll();
+                Integer i = queue.poll();
+                queue.add(i);
+            }
+            out.append(String.format("%d\n", queue.poll()));
+        }
+    }
+
+    void close() throws IOException {
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.flush();
+            out.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inStream = null;
+        PrintStream outStream = null;
+        boolean isLocal = System.getenv("LOCAL_JUDGE") != null;
+        if (isLocal) {
+            inStream = new FileInputStream("1.in");
+            // outStream = new PrintStream("1.out");
+            System.setIn(inStream);
+            // System.setOut(outStream);
+        }
+
+        Main main = new Main();
+        main.solve();
+        main.close();
+
+        if (isLocal) {
+            inStream.close();
+            // outStream.close();
+        }
+    }
+}
