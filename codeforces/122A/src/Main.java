698d4d500e3420e3d7f426ed90bd0c1539cd229e
lzwjava <lzwjava@gmail.com>
2021-11-29 05:36:54 +0800

    a

---
+++ codeforces/122A/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if (n % 4 == 0 || n % 7 == 0 || n % 47 == 0 || n % 447 == 0 || n % 477 == 0 || n % 747 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+}
