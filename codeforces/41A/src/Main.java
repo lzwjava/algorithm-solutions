6683e62173b4f394194f75e230c386bf1de20ce2
lzwjava <lzwjava@gmail.com>
2021-11-29 05:42:54 +0800

    a

--- codeforces/41A/src/Main.java
+++ codeforces/41A/src/Main.java
@@ -6,7 +6,12 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String a = in.next();
         String b = in.next();
-        
+        String rb = new StringBuilder(b).reverse().toString();
+        if (a.equals(rb)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 
 }
