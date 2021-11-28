097bd11528ef38ec8ea40eb39027d6d943f775a0
lzwjava <lzwjava@gmail.com>
2021-11-28 22:57:31 +0800

    a

--- codeforces/1609A/src/Main.java
+++ codeforces/1609A/src/Main.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -11,6 +14,73 @@ public class Main {
     }
 
     void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            int n = Integer.parseInt(in.readLine());
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            ArrayList<Long> ns = new ArrayList<Long>();
+            for (int i = 0; i < n; i++) {
+                long v = Long.parseLong(st.nextToken());
+                ns.add(v);
+            }
+            Collections.sort(ns);
+            int max = -1000;
+            int cnt = 0;
+            while (true) {
+                int sum = 0;
+                for (int k = 0; k < ns.size(); k++) {
+                    sum += ns.get(k);
+                }
+                if (sum > max) {
+                    max = sum;
+                } else {
+                    cnt++;
+                    if (cnt > 10000) {
+                        break;
+                    }
+                }
+
+                int len = ns.size();
+                int even = 0;
+                int eveni = -1;
+                for (int i = 0; i < len; i++) {
+                    if (ns.get(i) % 2 == 0) {
+                        even++;
+                        eveni = i;
+                    }
+                }
+                int i = -1;
+                if (even == 1) {
+                    i = eveni;
+                } else {
+                    for (i = len - 1; i >= 0; i--) {
+                        if (ns.get(i) % 2 == 0 && i != len - 1) {
+                            break;
+                        }
+                    }
+                }
+                int j = -1;
+                for (j = len - 1; j >= 0; j--) {
+                    if (j != i) {
+                        break;
+                    }
+                }
+                if (i == -1 || j == -1) {
+                    break;
+                }
+                long vi = ns.get(i);
+                long vj = ns.get(j);
+                long ai = vi / 2;
+                long aj = vj * 2;
+                ns.remove((Long) vi);
+                ns.remove((Long) vj);
+                ns.add(ai);
+                ns.add(aj);
+                Collections.sort(ns);
+            }
+            out.append(String.format("%d\n", max));
+            t--;
+        }
     }
 
     void close() throws IOException {
