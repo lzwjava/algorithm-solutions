58682e4004f5ae6561ae999a5c4cc8d955de7ab5
lzwjava <lzwjava@gmail.com>
2021-11-29 02:47:21 +0800

    a

--- codeforces/50A/src/Main.java
+++ codeforces/50A/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -11,7 +12,26 @@ public class Main {
     }
 
     void solve() throws IOException {
-        
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int ans;
+        if (m % 2 == 0 && n % 2 == 0) {
+            ans = m * n / 2;
+        } else if (m % 2 == 0 || n % 2 == 0) {
+            if (m % 2 != 0) {
+                int t = m;
+                m = n;
+                n = t;
+            }
+            int n1 = n - 1;
+            ans = m * n1 / 2 + m / 2;
+        } else {
+            int m1 = m - 1;
+            int n1 = n - 1;
+            ans = m1 * n1 / 2 + m / 2 + n / 2;
+        }
+        out.append(String.format("%d\n", ans));
     }
 
     void close() throws IOException {
