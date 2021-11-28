b74da0451e9b93e2b37fad36f6bbeadd347fd991
lzwjava <lzwjava@gmail.com>
2021-11-29 05:25:13 +0800

    a

--- codeforces/266B/src/Main.java
+++ codeforces/266B/src/Main.java
@@ -7,7 +7,22 @@ public class Main {
         int n = in.nextInt();
         int t = in.nextInt();
         String s = in.next();
-        
+        while (t > 0) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; ) {
+                if (i + 1 < n && s.charAt(i) == 'B' && s.charAt(i + 1) == 'G') {
+                    sb.append('G');
+                    sb.append('B');
+                    i += 2;
+                } else {
+                    sb.append(s.charAt(i));
+                    i++;
+                }
+            }
+            s = sb.toString();
+            t--;
+        }
+        System.out.println(s);
     }
 
 }
