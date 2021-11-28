a25066ed17bf4ca7d417a2a77834eb1406d85283
lzwjava <lzwjava@gmail.com>
2021-11-29 07:55:25 +0800

    a

--- codeforces/61A/src/Main.java
+++ codeforces/61A/src/Main.java
@@ -6,7 +6,16 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String a = in.next();
         String b = in.next();
-        
+        StringBuilder sb = new StringBuilder();
+        int n = a.length();
+        for (int i = 0; i < n; i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                sb.append('1');
+            } else {
+                sb.append('0');
+            }
+        }
+        System.out.println(sb.toString());
     }
 
     public static void main(String[] args) {
