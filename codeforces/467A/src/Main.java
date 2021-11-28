5c4cb8c43f14f359932a584a0fe3bce154e6c94b
lzwjava <lzwjava@gmail.com>
2021-11-29 06:15:07 +0800

    a

--- codeforces/467A/src/Main.java
+++ codeforces/467A/src/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        int n = in.nextInt();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            int p = in.nextInt();
+            int q = in.nextInt();
+            if (p + 2 <= q) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
     }
 
 }
