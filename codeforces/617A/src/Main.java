0b3bf1c2c4f48856dfec37e250eb3854071d71e9
lzwjava <lzwjava@gmail.com>
2021-11-29 04:38:38 +0800

    a

---
+++ codeforces/617A/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int ans = (int) Math.ceil(x * 1.0 / 5);
+        System.out.println(ans);
+    }
+
+}
