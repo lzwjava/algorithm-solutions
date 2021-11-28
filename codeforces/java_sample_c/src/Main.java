7dde152c7ca321ff9fde254f867081810d53dea0
lzwjava <lzwjava@gmail.com>
2021-11-29 04:25:16 +0800

    a

--- codeforces/java_sample_c/src/Main.java
+++ codeforces/java_sample_c/src/Main.java
@@ -1,32 +1,9 @@
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 
-    Scanner in;
-    PrintWriter out;
-
-    Main() {
-        in = new Scanner(System.in);
-        out = new PrintWriter(System.out);
-    }
-
-    void solve() {
-    }
-
-    void close() {
-        if (in != null) {
-            in.close();
-        }
-        if (out != null) {
-            out.flush();
-            out.close();
-        }
-    }
-
     public static void main(String[] args) {
-        Main main = new Main();
-        main.solve();
-        main.close();
+        Scanner in = new Scanner(System.in);
     }
+    
 }
