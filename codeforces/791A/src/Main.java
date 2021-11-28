40b93ea566b3ff72e3cf5ec4adb89e9afc1a45d0
lzwjava <lzwjava@gmail.com>
2021-11-29 04:19:03 +0800

    a

--- codeforces/791A/src/Main.java
+++ codeforces/791A/src/Main.java
@@ -1,23 +1,21 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
 
-    BufferedReader in;
+    Scanner in;
     PrintWriter out;
 
     Main() {
-        in = new BufferedReader(new InputStreamReader(System.in));
+        in = new Scanner(System.in);
         out = new PrintWriter(System.out);
     }
 
     void solve() throws IOException {
-        StringTokenizer st = new StringTokenizer(in.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        int a = in.nextInt();
+        int b = in.nextInt();
+        
     }
 
     void close() throws IOException {
