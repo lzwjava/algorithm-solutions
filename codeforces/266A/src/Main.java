cc4f24f3b3f5d364bc8bc749726eb36ad5eb2852
lzwjava <lzwjava@gmail.com>
2021-11-29 03:45:38 +0800

    a

--- codeforces/266A/src/Main.java
+++ codeforces/266A/src/Main.java
@@ -14,7 +14,23 @@ public class Main {
     }
 
     void solve() throws IOException {
-        
+        int n = Integer.parseInt(in.readLine());
+        String s = in.readLine();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (i == 0) {
+                sb.append(ch);
+            } else {
+                if (ch == s.charAt(i - 1)) {
+                    continue;
+                } else {
+                    sb.append(ch);
+                }
+            }
+        }
+        int cnt = s.length() - sb.toString().length();
+        out.append(String.format("%d\n", cnt));
     }
 
     void close() throws IOException {
