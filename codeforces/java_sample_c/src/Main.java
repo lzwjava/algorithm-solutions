7ad7a73767f3e4acf96dd23430a6e896694cd252
lzwjava <lzwjava@gmail.com>
2021-11-29 06:59:27 +0800

    a

--- codeforces/java_sample_c/src/Main.java
+++ codeforces/java_sample_c/src/Main.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    void solve() {
         Scanner in = new Scanner(System.in);
     }
-    
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+
 }
