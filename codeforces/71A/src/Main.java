a3362ee7caa59d45cda23a5585c4a0ca0adafe10
lzwjava <lzwjava@gmail.com>
2021-11-28 22:13:07 +0800

    a

--- codeforces/71A/src/Main.java
+++ codeforces/71A/src/Main.java
@@ -9,13 +9,18 @@ public class Main {
         in = new BufferedReader(new InputStreamReader(System.in));
         out = new PrintWriter(System.out);
     }
-
-    int readInt() throws IOException {
-        return Integer.parseInt(in.readLine());
-    }
-
+    
     void solve() throws IOException {
-        
+        int n = Integer.parseInt(in.readLine());
+        while (n > 0) {
+            String s = in.readLine();
+            int len = s.length();
+            if (len > 10) {
+                s = String.format("%c%d%c", s.charAt(0), len - 2, s.charAt(len - 1));
+            }
+            out.append(String.format("%s\n", s));
+            n--;
+        }
     }
 
     void close() throws IOException {
