2f4f06e8b8ca1af3acd9f6eb3e4c52927b9b63f6
lzwjava <lzwjava@gmail.com>
2021-11-29 08:46:58 +0800

    a

--- codeforces/479A/src/Main.java
+++ codeforces/479A/src/Main.java
@@ -1,13 +1,19 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
-        int a, b, c;
-        a = in.nextInt();
-        b = in.nextInt();
-        c = in.nextInt();
+        int[] ns = new int[3];
+        for (int i = 0; i < 3; i++) {
+            ns[i] = in.nextInt();
+        }
+        Arrays.sort(ns);
+        int ans = 0;
+        if (ns[0] > 1) {
+            ans = ns[0] * ns[1] * ns[2];
+        }
     }
 
     public static void main(String[] args) {
