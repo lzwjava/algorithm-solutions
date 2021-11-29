72dfd1cd66b57aa316a32a10e6d973b00a7143c3
lzwjava <lzwjava@gmail.com>
2021-11-29 11:57:36 +0800

    a

---
+++ codeforces/208A/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    void solve() {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String ns = s.replace("WUB", " ");
+        ns = ns.replaceAll("\\s+", " ");
+        System.out.println(ns.trim());
+    }
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+
+}
