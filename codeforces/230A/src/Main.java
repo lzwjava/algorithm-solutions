7ba779a109734c3478761c4d263c05c3f31ba819
lzwjava <lzwjava@gmail.com>
2021-11-29 23:59:01 +0800

    a

--- codeforces/230A/src/Main.java
+++ codeforces/230A/src/Main.java
@@ -1,17 +1,38 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
+    class Dragon implements Comparable<Dragon> {
+        int x, y;
+
+        Dragon(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Dragon o) {
+            return Integer.compare(x, o.x);
+        }
+    }
 
     void solve() {
         Scanner in = new Scanner(System.in);
         int s = in.nextInt();
         int n = in.nextInt();
-        boolean ok = true;
+        Dragon[] dragons = new Dragon[n];
         for (int i = 0; i < n; i++) {
             int x = in.nextInt();
             int y = in.nextInt();
-            if (s >= x) {
-                s += y;
+            dragons[i] = new Dragon(x, y);
+        }
+        Arrays.sort(dragons);
+        boolean ok = true;
+        for (int i = 0; i < n; i++) {
+            Dragon d = dragons[i];
+            if (s >= d.x) {
+                s += d.y;
             } else {
                 ok = false;
                 break;
