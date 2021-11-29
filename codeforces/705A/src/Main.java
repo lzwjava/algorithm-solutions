bfd12ac927da72f82d3504b3bd3b93a02e83b955
lzwjava <lzwjava@gmail.com>
2021-11-29 08:11:27 +0800

    a

---
+++ codeforces/705A/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    void solve() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        boolean hate = true;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n - 1; i++) {
+            if (hate) {
+                sb.append("I hate that ");
+            } else {
+                sb.append("I love that ");
+            }
+            hate = !hate;
+        }
+        if (hate) {
+            sb.append("I hate it");
+        } else {
+            sb.append("I love it");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+
+}
