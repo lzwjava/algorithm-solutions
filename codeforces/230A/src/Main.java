1040462847750bcd87649b28b03cdddfc9b86a28
lzwjava <lzwjava@gmail.com>
2021-11-29 23:48:27 +0800

    a

--- codeforces/230A/src/Main.java
+++ codeforces/230A/src/Main.java
@@ -4,6 +4,24 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int n = in.nextInt();
+        boolean ok = true;
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            if (s >= x) {
+                s -= x;
+                s += y;
+            } else {
+                break;
+            }
+        }
+        if (ok) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 
     public static void main(String[] args) {
