e9a8091912b77acd39cd3e07da0cf89c045eeaf1
lzwjava <lzwjava@gmail.com>
2021-11-29 08:53:36 +0800

    a

--- codeforces/479A/src/Main.java
+++ codeforces/479A/src/Main.java
@@ -9,11 +9,17 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             ns[i] = in.nextInt();
         }
-        Arrays.sort(ns);
-        int ans = 0;
-        if (ns[0] > 1) {
-            ans = ns[0] * ns[1] * ns[2];
+        int[] cs = ns.clone();
+        Arrays.sort(cs);
+        int ans;
+        if (cs[2] == 1) {
+            ans = 3;
+        } else if (cs[0] > 1) {
+            ans = cs[0] * cs[1] * cs[2];
+        } else {
+            ans = Integer.max((ns[0] + ns[1]) * ns[2], ns[0] * (ns[1] + ns[2]));
         }
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
