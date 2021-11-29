14938f2f8e5d6a41de89416fae88ff8d8efbe8ed
lzwjava <lzwjava@gmail.com>
2021-11-30 06:22:27 +0800

    a

--- codeforces/339B/src/Main.java
+++ codeforces/339B/src/Main.java
@@ -10,7 +10,19 @@ public class Main {
         for (int i = 0; i < m; i++) {
             as[i] = in.nextInt();
         }
-        
+        int p = 1;
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
+            if (as[i] == p) {
+                // do
+            } else {
+                while (as[i] != p) {
+                    p = p % n + 1;
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
     }
 
     public static void main(String[] args) {
