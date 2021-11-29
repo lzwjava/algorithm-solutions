283d42cd9911c06a78fc798f903873adc69bf5e7
lzwjava <lzwjava@gmail.com>
2021-11-30 00:17:27 +0800

    a

---
+++ codeforces/4C/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+    void solve() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Set<String> names = new HashSet<>();
+        Map<String, Integer> count = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String name = in.next();
+            if (!names.contains(name)) {
+                System.out.println("OK");
+                names.add(name);
+            } else {
+                Integer c = count.get(name);
+                if (c == null) {
+                    c = 0;
+                }
+                c++;
+                String s = String.format("%s%d", name, c);
+                names.add(s);
+                count.put(name, c);
+                System.out.println(s);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+
+}
