3bd7d9971fa5fa0e2ef0e1fa1922e4bf0d1c5054
lzwjava <lzwjava@gmail.com>
2021-11-29 08:36:04 +0800

    a

---
+++ codeforces/580A/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    void solve() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int prev = -1;
+        int len = 0;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int v = in.nextInt();
+            if (v >= prev) {
+                len++;
+            } else {
+                len = 1;
+            }
+            if (len > max) {
+                max = len;
+            }
+            prev = v;
+        }
+        System.out.println(max);
+    }
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+
+}
