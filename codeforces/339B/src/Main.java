b9709f1ca44911076a04b8fca0f824eace0fc599
lzwjava <lzwjava@gmail.com>
2021-11-30 05:46:29 +0800

    a

--- codeforces/339B/src/Main.java
+++ codeforces/339B/src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] as = new int[m];
+        for (int i = 0; i < m; i++) {
+            as[i] = in.nextInt();
+        }
         
     }
 
