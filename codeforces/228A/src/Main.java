053f4d10bfb99c2b13be91ac8ef9867addc88b0f
lzwjava <lzwjava@gmail.com>
2021-11-29 09:15:38 +0800

    a

--- codeforces/228A/src/Main.java
+++ codeforces/228A/src/Main.java
@@ -1,10 +1,18 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
-        
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < 4; i++) {
+            int v = in.nextInt();
+            set.add(v);
+        }
+        int ans = 4 - set.size();
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
