c2636b30f7cd308367590f48510f5335fd972533
lzwjava <lzwjava@gmail.com>
2021-11-30 11:28:18 +0800

    a

---
+++ codeforces/1409A/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    void solve() {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t > 0) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int d = Math.abs(a - b);
+            int c = 0;
+            if (d > 10) {
+                c += (int) Math.ceil(d * 1.0 / 10);
+            } else {
+                c = 1;
+            }
+            System.out.print(c);
+            t--;
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+
+}
