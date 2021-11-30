3324fda96844b7564bfad8d57cfec3635b98aefa
lzwjava <lzwjava@gmail.com>
2021-12-01 01:56:59 +0800

    a

--- codeforces/1353B/src/Main.java
+++ codeforces/1353B/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -16,6 +17,23 @@ public class Main {
             for (int i = 0; i < n; i++) {
                 b[i] = in.nextInt();
             }
+            Arrays.sort(a);
+            Arrays.sort(b);
+            for (int i = 0; i < k; i++) {
+                int j = n - 1 - i;
+                if (a[i] < b[j]) {
+                    int p = a[i];
+                    a[i] = b[j];
+                    b[j] = p;
+                } else {
+                    break;
+                }
+            }
+            int s = 0;
+            for (int i = 0; i < n; i++) {
+                s += a[i];
+            }
+            System.out.println(s);
             t--;
         }
     }
