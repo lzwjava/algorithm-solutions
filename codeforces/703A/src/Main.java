0c37e5b5dc831fcccca9b602585af53dded876b0
lzwjava <lzwjava@gmail.com>
2021-12-01 04:24:39 +0800

    a

--- codeforces/703A/src/Main.java
+++ codeforces/703A/src/Main.java
@@ -5,11 +5,23 @@ public class Main {
     void solve() {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        int mw = 0, cw = 0;
         while (n > 0) {
             int m = in.nextInt();
             int c = in.nextInt();
-            
+            if (m > c) {
+                mw++;
+            } else if (m < c) {
+                cw++;
+            }
             n--;
+        }
+        if (mw > cw) {
+            System.out.println("Mishka");
+        } else if (mw < cw) {
+            System.out.println("Chris");
+        } else {
+            System.out.println("Friendship is magic!^^");
         }
     }
 
