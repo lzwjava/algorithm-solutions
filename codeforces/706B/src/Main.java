8628f3ccb824530ff602b536e44a1598373de45f
lzwjava <lzwjava@gmail.com>
2021-12-01 03:35:56 +0800

    a

--- codeforces/706B/src/Main.java
+++ codeforces/706B/src/Main.java
@@ -1,19 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    void solve() {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+    void solve() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
         int[] x = new int[n];
+        StringTokenizer st = new StringTokenizer(in.readLine());
         for (int i = 0; i < n; i++) {
-            x[i] = in.nextInt();
+            x[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(x);
-        int q = in.nextInt();
+        int q = Integer.parseInt(in.readLine());
         while (q > 0) {
-            int m = in.nextInt();
+            int m = Integer.parseInt(in.readLine());
             int i = Arrays.binarySearch(x, m);
             int ans;
             if (i >= 0) {
@@ -30,7 +34,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Main().solve();
     }
 
