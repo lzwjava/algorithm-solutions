7c799dad6fa64feb9fe330cd040c42cf7a5c502a
lzwjava <lzwjava@gmail.com>
2021-12-01 01:46:34 +0800

    a

--- codeforces/1353B/src/Main.java
+++ codeforces/1353B/src/Main.java
@@ -6,7 +6,16 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         while (t > 0) {
-            
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = in.nextInt();
+            }
+            for (int i = 0; i < n; i++) {
+                b[i] = in.nextInt();
+            }
             t--;
         }
     }
