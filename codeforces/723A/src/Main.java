6a20f397bfea5b8307293260d269052edad195ea
lzwjava <lzwjava@gmail.com>
2021-11-30 16:22:34 +0800

    a

--- codeforces/723A/src/Main.java
+++ codeforces/723A/src/Main.java
@@ -10,12 +10,8 @@ public class Main {
             xs[i] = in.nextInt();
         }
         Arrays.sort(xs);
-        double p = (xs[0] + xs[2]) * 1.0 / 2;
-        double s = 0;
-        for (int i = 0; i < 3; i++) {
-            s += Math.abs(p - xs[i]);
-        }
-        System.out.println((int) s);
+        int ans = xs[2] - xs[0];
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
