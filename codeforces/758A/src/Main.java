8bb13af04a531732e8aa33bca5ec16a63fa53780
lzwjava <lzwjava@gmail.com>
2021-11-30 16:51:30 +0800

    a

--- codeforces/758A/src/Main.java
+++ codeforces/758A/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
     }
 
     public static void main(String[] args) {
