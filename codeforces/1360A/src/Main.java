7a2f828e2d51d29b2a3c444aae8690c8058919cf
lzwjava <lzwjava@gmail.com>
2021-12-01 04:18:06 +0800

    a

--- codeforces/1360A/src/Main.java
+++ codeforces/1360A/src/Main.java
@@ -8,7 +8,17 @@ public class Main {
         while (t > 0) {
             int a = in.nextInt();
             int b = in.nextInt();
-            
+            if (b > a) {
+                int p = a;
+                a = b;
+                b = p;
+            }
+            int e1 = Integer.max(a, b * 2);
+            int e2 = Integer.max(a * 2, b);
+            int e3 = Integer.max(a + b, a);
+            int min = Integer.min(e1, e2);
+            int m = Integer.min(min, e3);
+            System.out.println(m * m);
             t--;
         }
     }
