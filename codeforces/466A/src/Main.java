c898aa414cb70fc774dec76a6f730a7d7bcf0334
lzwjava <lzwjava@gmail.com>
2021-11-30 14:46:38 +0800

    a

--- codeforces/466A/src/Main.java
+++ codeforces/466A/src/Main.java
@@ -4,6 +4,23 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int ans;
+        if (b >= a * m) {
+            // b/m >=a
+            ans = n * a;
+        } else {
+            if (n < m) {
+                ans = n * a;
+            } else {
+                int d = n / m;
+                ans = b * d + (n % m) * a;
+            }
+        }
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
