676830f9c6466100b2b29078225945c3c6859932
lzwjava <lzwjava@gmail.com>
2021-12-01 01:03:41 +0800

    a

--- codeforces/500A/src/Main.java
+++ codeforces/500A/src/Main.java
@@ -6,7 +6,29 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int t = in.nextInt();
-
+        int[] a = new int[n];
+        for (int i = 0; i < n - 1; i++) {
+            a[i + 1] = in.nextInt();
+        }
+        int p = 1;
+        boolean ans = false;
+        while (p <= n) {
+            if (p == t) {
+                ans = true;
+                break;
+            }
+            if (p > t) {
+                ans = false;
+                break;
+            }
+            int np = p + a[p];
+            p = np;
+        }
+        if (ans) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 
     public static void main(String[] args) {
