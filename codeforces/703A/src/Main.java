ca967e12f45553e18d7de85ed92f4bac56ace76f
lzwjava <lzwjava@gmail.com>
2021-12-01 04:23:15 +0800

    a

--- codeforces/703A/src/Main.java
+++ codeforces/703A/src/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        while (n > 0) {
+            int m = in.nextInt();
+            int c = in.nextInt();
+            
+            n--;
+        }
     }
 
     public static void main(String[] args) {
