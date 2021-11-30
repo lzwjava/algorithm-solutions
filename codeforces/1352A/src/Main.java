a4b62baadcf6480381495702abca2c1d561943d8
lzwjava <lzwjava@gmail.com>
2021-11-30 11:53:28 +0800

    a

--- codeforces/1352A/src/Main.java
+++ codeforces/1352A/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +8,25 @@ public class Main {
         int t = in.nextInt();
         while (t > 0) {
             int n = in.nextInt();
-            
+            String s = String.format("%d", n);
+            ArrayList<Integer> list = new ArrayList<>();
+            int p = 1;
+            for (int i = s.length() - 1; i >= 0; i--) {
+                int digit = s.charAt(i) - '0';
+                if (digit != 0) {
+                    int a = digit * p;
+                    list.add(a);
+                }
+                p *= 10;
+            }
+            System.out.println(list.size());
+            for (int i = 0; i < list.size(); i++) {
+                if (i != 0) {
+                    System.out.print(' ');
+                }
+                System.out.print(list.get(i));
+            }
+            System.out.println();
             t--;
         }
     }
