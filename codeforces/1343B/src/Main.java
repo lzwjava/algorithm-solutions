3b35c2239b16b0e6368e00c88b833f5c4d549e35
lzwjava <lzwjava@gmail.com>
2021-11-30 15:19:45 +0800

    a

--- codeforces/1343B/src/Main.java
+++ codeforces/1343B/src/Main.java
@@ -1,17 +1,20 @@
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
         int t = in.nextInt();
         while (t > 0) {
             int n = in.nextInt();
             int d = n / 2;
             if (d % 2 == 1) {
-                System.out.println("NO");
+                out.append("NO\n");
             } else {
-                System.out.println("YES");
+                out.append("YES\n");
                 int[] as = new int[n];
                 int p = 2;
                 for (int i = 0; i < d; i++) {
@@ -26,14 +29,17 @@ public class Main {
                 as[n - 1] = p + d;
                 for (int i = 0; i < n; i++) {
                     if (i != 0) {
-                        System.out.print(' ');
+                        out.append(' ');
                     }
-                    System.out.print(as[i]);
+                    out.append(String.format("%d", as[i]));
                 }
-                System.out.println();
+                out.append('\n');
             }
             t--;
         }
+
+        out.flush();
+        out.close();
     }
 
     public static void main(String[] args) {
