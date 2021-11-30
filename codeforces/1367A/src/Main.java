737f1c02a45981e0ed4339000d9d6d44cc7f4d47
lzwjava <lzwjava@gmail.com>
2021-11-30 12:34:18 +0800

    a

--- codeforces/1367A/src/Main.java
+++ codeforces/1367A/src/Main.java
@@ -7,6 +7,14 @@ public class Main {
         int t = in.nextInt();
         while (t > 0) {
             String s = in.next();
+            String sub = s.substring(1, s.length() - 1);
+            StringBuilder sb = new StringBuilder();
+            sb.append(s.charAt(0));
+            for (int i = 0; i < sub.length(); i += 2) {
+                sb.append(sub.charAt(i));
+            }
+            sb.append(s.charAt(s.length() - 1));
+            System.out.println(sb.toString());
             t--;
         }
     }
