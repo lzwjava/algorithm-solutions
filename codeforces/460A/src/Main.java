c1d1021c607becfaf8c946755bba13dbcb98a6b4
lzwjava <lzwjava@gmail.com>
2021-12-01 06:40:59 +0800

    a

--- codeforces/460A/src/Main.java
+++ codeforces/460A/src/Main.java
@@ -4,6 +4,20 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int cnt = 0;
+        for (int i = 1; ; i++) {
+            if (n == 0) {
+                break;
+            }
+            cnt++;
+            n--;
+            if (i % m == 0) {
+                n += 1;
+            }
+        }
+        System.out.println(cnt);
     }
 
     public static void main(String[] args) {
