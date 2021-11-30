e5f7d4e017ec8db9392ceb984bdd97dd2422cb0f
lzwjava <lzwjava@gmail.com>
2021-12-01 03:36:49 +0800

    a

--- codeforces/706B/src/Main.java
+++ codeforces/706B/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -8,6 +9,7 @@ public class Main {
 
     void solve() throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
         int n = Integer.parseInt(in.readLine());
         int[] x = new int[n];
         StringTokenizer st = new StringTokenizer(in.readLine());
@@ -29,9 +31,11 @@ public class Main {
                 i = -(i + 1);
                 ans = i;
             }
-            System.out.println(ans);
+            out.append(String.format("%d\n", ans));
             q--;
         }
+        out.flush();
+        out.close();
     }
 
     public static void main(String[] args) throws IOException {
