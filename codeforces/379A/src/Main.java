80588b42720c64c21d9a33eb85b13962a3a45dfa
lzwjava <lzwjava@gmail.com>
2021-12-01 07:51:11 +0800

    a

--- codeforces/379A/src/Main.java
+++ codeforces/379A/src/Main.java
@@ -6,7 +6,19 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
         int b = in.nextInt();
-        
+        int s = 0;
+        int c = 0;
+        while (a > 0) {
+            s += a;
+            c += a;
+            if (c >= b) {
+                a = c / b;
+                c -= a * b;
+            } else {
+                break;
+            }
+        }
+        System.out.println(s);
     }
 
     public static void main(String[] args) {
