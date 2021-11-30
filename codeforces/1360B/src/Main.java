690f899a434df10ca29a2fbe4cba0d5262a23dc3
lzwjava <lzwjava@gmail.com>
2021-12-01 06:03:16 +0800

    a

--- codeforces/1360B/src/Main.java
+++ codeforces/1360B/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,7 +12,18 @@ public class Main {
             for (int i = 0; i < n; i++) {
                 s[i] = in.nextInt();
             }
-            
+            Arrays.sort(s);
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < n - 1; i++) {
+                int d = s[i + 1] - s[i];
+                if (d < min) {
+                    min = d;
+                    if (min == 0) {
+                        break;
+                    }
+                }
+            }
+            System.out.println(min);
             t--;
         }
     }
