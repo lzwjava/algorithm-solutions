083ee652fa95a1be195d3e417ffe094d7daabfbd
lzwjava <lzwjava@gmail.com>
2021-11-30 13:03:12 +0800

    a

--- codeforces/455A/src/Main.java
+++ codeforces/455A/src/Main.java
@@ -1,8 +1,38 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+    class Item implements Comparable<Item> {
+        int v;
+        int c;
+
+        Item(int v, int c) {
+            this.v = v;
+            this.c = c;
+        }
+
+        @Override
+        public int compareTo(Item o) {
+            if (c != o.c) {
+                return Integer.compare(o.c, c);
+            } else {
+                return Integer.compare(o.v, v);
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Item item = (Item) o;
+            return v == item.v;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(v);
+        }
+    }
 
     void solve() {
         Scanner in = new Scanner(System.in);
@@ -20,6 +50,13 @@ public class Main {
             c++;
             map.put(as[i], c);
         }
+        ArrayList<Item> list = new ArrayList<>();
+        for (Integer key : map.keySet()) {
+            Integer c = map.get(key);
+            list.add(new Item(key, c));
+        }
+        Collections.sort(list);
+
     }
 
     public static void main(String[] args) {
