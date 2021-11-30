83919201afe34a661497a4c82d9566de645b0b44
lzwjava <lzwjava@gmail.com>
2021-12-01 06:54:45 +0800

    a

--- codeforces/489C/src/Main.java
+++ codeforces/489C/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int s = in.nextInt();
+        
     }
 
     public static void main(String[] args) {
