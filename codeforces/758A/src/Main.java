ce4644b94976a1abd71c4b2f4ff6e87c081c39f5
lzwjava <lzwjava@gmail.com>
2021-11-30 22:35:07 +0800

    a

--- codeforces/758A/src/Main.java
+++ codeforces/758A/src/Main.java
@@ -6,6 +6,16 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] a = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+            max = Integer.max(max, a[i]);
+        }
+        int s = 0;
+        for (int i = 0; i < n; i++) {
+            s += max - a[i];
+        }
+        System.out.println(s);
     }
 
     public static void main(String[] args) {
