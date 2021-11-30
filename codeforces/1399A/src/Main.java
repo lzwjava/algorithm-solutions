e2d38901d5144bb69e67dbda46251ff8c615aa94
lzwjava <lzwjava@gmail.com>
2021-11-30 10:45:53 +0800

    a

--- codeforces/1399A/src/Main.java
+++ codeforces/1399A/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,12 +6,15 @@ public class Main {
     void solve() {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        int n = in.nextInt();
-        int[] as = new int[n];
-        for (int i = 0; i < n; i++) {
-            as[i] = in.nextInt();
+        while (t > 0) {
+            int n = in.nextInt();
+            int[] as = new int[n];
+            for (int i = 0; i < n; i++) {
+                as[i] = in.nextInt();
+            }
+            Arrays.sort(as);
+            t--;
         }
-        
     }
 
     public static void main(String[] args) {
