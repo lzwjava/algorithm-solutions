88eb6556c6a3ec9546a4e7fb960d0d3321293c2c
lzwjava <lzwjava@gmail.com>
2021-11-30 12:14:07 +0800

    a

--- codeforces/472A/src/Main.java
+++ codeforces/472A/src/Main.java
@@ -4,27 +4,35 @@ import java.util.Scanner;
 public class Main {
 
     int maxn = 1000001;
-    boolean[] isPrimes;
+    boolean[] isPrime;
 
     void calPrimes() {
-        isPrimes = new boolean[maxn];
-        Arrays.fill(isPrimes, true);
-        isPrimes[0] = isPrimes[1] = false;
+        isPrime = new boolean[maxn];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
         for (int i = 4; i < maxn; i += 2) {
-            isPrimes[i] = false;
+            isPrime[i] = false;
         }
         for (int i = 3; i < maxn; i += 2) {
-            if (isPrimes[i]) {
+            if (isPrime[i]) {
                 for (int j = i * i; j < maxn; j += i) {
-                    isPrimes[j] = false;
+                    isPrime[j] = false;
                 }
             }
         }
     }
 
     void solve() {
+        calPrimes();
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        for (int i = 4; i <= n / 2; i++) {
+            int j = n - i;
+            if (!isPrime[i] && !isPrime[j]) {
+                System.out.println(String.format("%d %d", i, j));
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
