1ebda843b48dfffd3267ab79f6fd9de5b40e7058
lzwjava <lzwjava@gmail.com>
2021-11-30 12:59:19 +0800

    a

--- codeforces/455A/src/Main.java
+++ codeforces/455A/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -9,7 +11,15 @@ public class Main {
         for (int i = 0; i < n; i++) {
             as[i] = in.nextInt();
         }
-        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            Integer c = map.get(as[i]);
+            if (c == null) {
+                c = 0;
+            }
+            c++;
+            map.put(as[i], c);
+        }
     }
 
     public static void main(String[] args) {
