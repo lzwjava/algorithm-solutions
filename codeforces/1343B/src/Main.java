9d728e381af406af6ca95ec24e8d66549d0f7e35
lzwjava <lzwjava@gmail.com>
2021-11-30 14:57:20 +0800

    a

---
+++ codeforces/1343B/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    void solve() {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t > 0) {
+            t--;
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+
+}
