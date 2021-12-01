1067b9f1e1495361f5a52d2c5d2d4ce4746a1fc5
lzwjava <lzwjava@gmail.com>
2021-12-01 23:02:50 +0800

    a

--- codeforces/1613B/src/Main.java
+++ codeforces/1613B/src/Main.java
@@ -1,16 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    void solve() {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
+    void solve() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine());
         while (t > 0) {
-            int n = in.nextInt();
+            int n = Integer.parseInt(in.readLine());
             int[] a = new int[n];
+            StringTokenizer st = new StringTokenizer(in.readLine());
             for (int i = 0; i < n; i++) {
-                a[i] = in.nextInt();
+                a[i] = Integer.parseInt(st.nextToken());
             }
             int p = n / 2;
             Arrays.sort(a);
@@ -21,7 +27,7 @@ public class Main {
                         int m = a[i] % a[j];
                         int index = Arrays.binarySearch(a, m);
                         if (index < 0) {
-                            System.out.println(String.format("%d %d", a[i], a[j]));
+                            out.append(String.format("%d %d\n", a[i], a[j]));
                             c++;
                             if (c == p) {
                                 break;
@@ -35,9 +41,11 @@ public class Main {
             }
             t--;
         }
+        out.flush();
+        out.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Main().solve();
     }
 
