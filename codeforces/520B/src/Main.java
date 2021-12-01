cc1e3929a68e04284ec1459ee53af5f3af0d20eb
lzwjava <lzwjava@gmail.com>
2021-12-02 01:35:35 +0800

    a

--- codeforces/520B/src/Main.java
+++ codeforces/520B/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
     }
 
     public static void main(String[] args) {
