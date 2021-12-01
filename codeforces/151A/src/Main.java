ea5eae4695dd61aa8a099086a5c5fb99be66630b
lzwjava <lzwjava@gmail.com>
2021-12-01 10:24:28 +0800

    a

--- codeforces/151A/src/Main.java
+++ codeforces/151A/src/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        int n, k, l, c, d, p, nl, np;
+        n = in.nextInt();
+        k = in.nextInt();
+        l = in.nextInt();
+        c = in.nextInt();
+        d = in.nextInt();
+        p = in.nextInt();
+        nl = in.nextInt();
+        np = in.nextInt();
+        
     }
 
     public static void main(String[] args) {
