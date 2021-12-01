84cfa63f953c878601ff8b2a2802ab73e3432a55
lzwjava <lzwjava@gmail.com>
2021-12-02 04:57:43 +0800

    a

---
+++ codeforces/707A/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    void solve() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        boolean color = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                char c = in.next().charAt(0);
+                String s = "CMY";
+                int p = s.indexOf(c);
+                if (p >= 0) {
+                    color = true;
+                    break;
+                }
+            }
+            if (color) {
+                break;
+            }
+        }
+        if (color) {
+            System.out.println("#Color");
+        } else {
+            System.out.println("#Black&White");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+
+}
