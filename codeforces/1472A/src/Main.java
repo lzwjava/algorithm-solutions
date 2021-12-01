5fd33cdf655c5e4baf9f9c99888f1a63c33a844b
lzwjava <lzwjava@gmail.com>
2021-12-02 03:18:19 +0800

    a

--- codeforces/1472A/src/Main.java
+++ codeforces/1472A/src/Main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 
+    int dp(int w, int h) {
+        if (w % 2 == 1 && h % 2 == 1) {
+            return 0;
+        }
+        int v = 0;
+        if (w % 2 == 0) {
+            v = Integer.max(v, dp(w / 2, h));
+        }
+        if (h % 2 == 0) {
+            v = Integer.max(v, dp(w, h / 2));
+        }
+        return v;
+    }
+
     void solve() {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
@@ -9,6 +23,12 @@ public class Main {
             int w = in.nextInt();
             int h = in.nextInt();
             int n = in.nextInt();
+            int p = dp(w, h);
+            if (p >= n) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
             t--;
         }
     }
