40ff92e6577e7ec25dcba35725d85f33805c9ec7
lzwjava <lzwjava@gmail.com>
2021-12-02 04:01:22 +0800

    a

--- codeforces/1475B/src/Main.java
+++ codeforces/1475B/src/Main.java
@@ -4,6 +4,29 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t > 0) {
+            int n = in.nextInt();
+            int c = 0;
+            while (n >= 2020) {
+                n -= 2020;
+                c++;
+            }
+            boolean ok = false;
+            if (n == 0) {
+                ok = true;
+            } else {
+                if (n <= c) {
+                    ok = true;
+                }
+            }
+            if (ok) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            t--;
+        }
     }
 
     public static void main(String[] args) {
