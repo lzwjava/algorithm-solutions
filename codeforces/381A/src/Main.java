3db980c81e6b009a57f930dbe51e3c1652c8c0b0
lzwjava <lzwjava@gmail.com>
2021-12-02 02:41:03 +0800

    a

--- codeforces/381A/src/Main.java
+++ codeforces/381A/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
@@ -5,10 +6,33 @@ public class Main {
     void solve() {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] a = new int[n];
+        LinkedList<Integer> list = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            a[i] = in.nextInt();
+            list.add(in.nextInt());
         }
+        int a = 0, b = 0;
+        boolean p = true;
+        while (!list.isEmpty()) {
+            int f = list.getFirst();
+            int l = list.getLast();
+            if (f > l) {
+                if (p) {
+                    a += f;
+                } else {
+                    b += f;
+                }
+                list.removeFirst();
+            } else {
+                if (p) {
+                    a += l;
+                } else {
+                    b += l;
+                }
+                list.removeLast();
+            }
+            p = !p;
+        }
+        System.out.println(String.format("%d %d\n", a, b));
     }
 
     public static void main(String[] args) {
