c27325c5e9f765c6e303b14bab4e8947b1cbb88d
lzwjava <lzwjava@gmail.com>
2021-12-02 03:24:52 +0800

    a

--- codeforces/1472A/src/Main.java
+++ codeforces/1472A/src/Main.java
@@ -1,24 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
+    Map<String, Integer> map;
+
     int dp(int w, int h) {
+        if (w > h) {
+            return dp(h, w);
+        }
+        String key = String.format("%d,%d", w, h);
+        Integer v = map.get(key);
+        if (v != null) {
+            return v;
+        }
         if (w % 2 == 1 && h % 2 == 1) {
-            return 1;
+            v = 1;
+        } else {
+            v = 0;
+            if (w % 2 == 0) {
+                v = Integer.max(v, dp(w / 2, h) * 2);
+            }
+            if (h % 2 == 0) {
+                v = Integer.max(v, dp(w, h / 2) * 2);
+            }
         }
-        int v = 0;
-        if (w % 2 == 0) {
-            v = Integer.max(v, dp(w / 2, h) * 2);
-        }
-        if (h % 2 == 0) {
-            v = Integer.max(v, dp(w, h / 2) * 2);
-        }
+        map.put(key, v);
         return v;
     }
 
     void solve() {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
+        map = new HashMap<>();
         while (t > 0) {
             int w = in.nextInt();
             int h = in.nextInt();
