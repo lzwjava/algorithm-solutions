4f0539a8ddf3ab2f7170c95bb3f3f79109c8fcd4
lzwjava <lzwjava@gmail.com>
2021-12-01 08:42:09 +0800

    a

--- codeforces/1370A/src/Main.java
+++ codeforces/1370A/src/Main.java
@@ -2,11 +2,25 @@ import java.util.Scanner;
 
 public class Main {
 
+    int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
     void solve() {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         while (t > 0) {
             int n = in.nextInt();
+            int ans;
+            if (n % 2 == 0) {
+                ans = n / 2;
+            } else {
+                ans = (n - 1) / 2;
+            }
+            System.out.println(ans);
             t--;
         }
     }
