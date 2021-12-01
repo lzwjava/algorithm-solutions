7451b03e77d44b4fa54cad43c7ef8411ccbfb367
lzwjava <lzwjava@gmail.com>
2021-12-02 00:36:46 +0800

    a

--- codeforces/1613B/src/Main.java
+++ codeforces/1613B/src/Main.java
@@ -2,10 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -18,23 +15,30 @@ public class Main {
             int[] a = new int[n];
             StringTokenizer st = new StringTokenizer(in.readLine());
             Set<Integer> set = new HashSet<>();
+            PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return Integer.compare(o2, o1);
+                }
+            });
             for (int i = 0; i < n; i++) {
                 a[i] = Integer.parseInt(st.nextToken());
                 set.add(a[i]);
+                queue.add(a[i]);
             }
             int p = n / 2;
             int c = 0;
-            Arrays.sort(a);
-            for (int i = n - 1; i >= 0; i--) {
-                for (int j = 0; j < i; j++) {
-                    int min = Integer.min(a[i], a[j]);
-                    int max = Integer.max(a[i], a[j]);
-                    int m = max % min;
-                    if (!set.contains(m)) {
-                        out.append(String.format("%d %d\n", max, min));
-                        c++;
-                        if (c == p) {
-                            break;
+            while (!queue.isEmpty()) {
+                int x = queue.poll();
+                for (int i = 0; i < n; i++) {
+                    if (x > a[i]) {
+                        int m = x % a[i];
+                        if (!set.contains(m)) {
+                            out.append(String.format("%d %d\n", x, a[i]));
+                            c++;
+                            if (c == p) {
+                                break;
+                            }
                         }
                     }
                 }
