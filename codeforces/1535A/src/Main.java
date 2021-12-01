8b6e3c6862367a74b0adf66c0a73e12583c937b7
lzwjava <lzwjava@gmail.com>
2021-12-02 07:20:27 +0800

    a

--- codeforces/1535A/src/Main.java
+++ codeforces/1535A/src/Main.java
@@ -13,6 +13,14 @@ public class Main {
             }
             int[] a = s.clone();
             Arrays.sort(a);
+            int b, c;
+            b = Integer.max(s[0], s[1]);
+            c = Integer.max(s[2], s[3]);
+            if (b + c == a[2] + a[3]) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
             t--;
         }
     }
