76df3c6e6aec3036288f8d3421bbc02dfe845d4a
lzwjava <lzwjava@gmail.com>
2021-12-01 22:39:56 +0800

    a

---
+++ codeforces/1613A/src/Main.java
@@ -0,0 +1,38 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    BigInteger bi(int x, int p) {
+        BigInteger x1b = BigInteger.valueOf(x);
+        BigInteger bp = BigInteger.valueOf(10);
+        return x1b.multiply(bp.pow(p));
+    }
+
+    void solve() {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t > 0) {
+            int x1 = in.nextInt();
+            int p1 = in.nextInt();
+            int x2 = in.nextInt();
+            int p2 = in.nextInt();
+            BigInteger a = bi(x1, p1);
+            BigInteger b = bi(x2, p2);
+            int c = a.compareTo(b);
+            if (c > 0) {
+                System.out.println(">");
+            } else if (c == 0) {
+                System.out.println("=");
+            } else {
+                System.out.println("<");
+            }
+            t--;
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+
+}
