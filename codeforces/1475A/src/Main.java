81e4b60ebe82cde880b1c8b35e59898e69cd929a
lzwjava <lzwjava@gmail.com>
2021-12-01 09:31:48 +0800

    a

--- codeforces/1475A/src/Main.java
+++ codeforces/1475A/src/Main.java
@@ -1,52 +1,23 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    ArrayList<Integer> list;
-
-    void calPrimes() {
-        int maxn = 10000001;
-        boolean[] primes = new boolean[maxn];
-        Arrays.fill(primes, true);
-        primes[0] = primes[1] = false;
-        for (int i = 4; i < maxn; i += 2) {
-            primes[i] = false;
-        }
-        int sm = (int) Math.sqrt(maxn);
-        for (int i = 3; i <= sm; i += 2) {
-            if (primes[i]) {
-                for (int j = i * i; j < maxn; j += i) {
-                    primes[j] = false;
-                }
-            }
-        }
-        list = new ArrayList<>();
-        for (int i = 0; i < maxn; i++) {
-            if (primes[i]) {
-                list.add(i);
-            }
-        }
-    }
-
     void solve() {
-        calPrimes();
-
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         while (t > 0) {
             long n = in.nextLong();
-            int sn = (int) Math.sqrt(n);
-            boolean odd = true;
-            for (int i = 1; i < list.size(); i++) {
-                int a = list.get(i);
-                if (a > sn) {
-                    break;
+            boolean odd;
+            if (n % 2 == 1) {
+                odd = true;
+            } else {
+                while (n % 2 == 0) {
+                    n /= 2;
                 }
-                if (n % a == 0) {
+                if (n == 1) {
+                    odd = false;
+                } else {
                     odd = true;
-                    break;
                 }
             }
             if (odd) {
