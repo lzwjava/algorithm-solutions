d9611e471596d7e7ad72961f040826d17ee19e49
lzwjava <lzwjava@gmail.com>
2021-12-02 07:49:59 +0800

    a

--- java_sample/src/Main.java
+++ java_sample/src/Main.java
@@ -1,50 +1,13 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
-    BufferedReader in;
-    PrintWriter out;
-
-    Main() {
-        in = new BufferedReader(new InputStreamReader(System.in));
-        out = new PrintWriter(System.out);
+    void solve() {
+        Scanner in = new Scanner(System.in);
     }
 
-    int readInt() throws IOException {
-        return Integer.parseInt(in.readLine());
+    public static void main(String[] args) {
+        new Main().solve();
     }
 
-    void solve() throws IOException {
-    }
-
-    void close() throws IOException {
-        if (in != null) {
-            in.close();
-        }
-        if (out != null) {
-            out.flush();
-            out.close();
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        FileInputStream inStream = null;
-        PrintStream outStream = null;
-        boolean isLocal = System.getenv("LOCAL_JUDGE") != null;
-        if (isLocal) {
-            inStream = new FileInputStream("1.in");
-            // outStream = new PrintStream("1.out");
-            System.setIn(inStream);
-            // System.setOut(outStream);
-        }
-
-        Main main = new Main();
-        main.solve();
-        main.close();
-
-        if (isLocal) {
-            inStream.close();
-            // outStream.close();
-        }
-    }
 }
