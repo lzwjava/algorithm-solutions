c05e9342ac5d95c789994fd4b794f9d1dea46bb1
lzwjava <lzwjava@gmail.com>
2021-12-02 02:57:15 +0800

    a

--- codeforces/431A/src/Main.java
+++ codeforces/431A/src/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        int[] a = new int[4];
+        for (int i = 0; i < 4; i++) {
+            a[i] = in.nextInt();
+        }
+        String s = in.next();
+        int p = 0;
+        for (char c : s.toCharArray()) {
+            int d = c - '0';
+            p += a[d - 1];
+        }
+        System.out.println(p);
     }
 
     public static void main(String[] args) {
