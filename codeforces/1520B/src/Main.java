ddc7b13c8bd37aadd87e555c466f09477593bcc4
lzwjava <lzwjava@gmail.com>
2021-12-02 06:10:21 +0800

    a

--- codeforces/1520B/src/Main.java
+++ codeforces/1520B/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +9,26 @@ public class Main {
         int t = in.nextInt();
         while (t > 0) {
             int n = in.nextInt();
+            String s = String.format("%d", n);
+            int len = s.length();
+            int a = (len - 1) * 9;
+
+            List<Integer> list = new ArrayList<>();
+            for (int i = 1; i <= 9; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < len; j++) {
+                    sb.append(String.valueOf(i));
+                }
+                list.add(Integer.parseInt(sb.toString()));
+            }
+            for (int x : list) {
+                if (n >= x) {
+                    a++;
+                } else {
+                    break;
+                }
+            }
+            System.out.println(a);
             t--;
         }
     }
