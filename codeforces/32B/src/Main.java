4f85645c8525ece5b262cd618ded2bf728d8b35a
lzwjava <lzwjava@gmail.com>
2021-12-02 06:30:38 +0800

    a

--- codeforces/32B/src/Main.java
+++ codeforces/32B/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     void solve() {
         Scanner in = new Scanner(System.in);
+        String s = in.next();
+        for (int i = 0; i < s.length(); i++) {
+            
+        }
     }
 
     public static void main(String[] args) {
