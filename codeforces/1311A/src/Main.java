e39b9255232d36fff1f5c920c45214b6ed778485
lzwjava <lzwjava@gmail.com>
2021-12-02 04:37:01 +0800

    a

--- codeforces/1311A/src/Main.java
+++ codeforces/1311A/src/Main.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    int dp(int a, int b) {
+        
+    }
+
     void solve() {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         while (t > 0) {
             int a = in.nextInt();
             int b = in.nextInt();
+            int ans = dp(a, b);
+            System.out.println(ans);
             t--;
         }
     }
