52a9ff20d63daaea96108fd5af9797b7defc6c67
lzwjava <lzwjava@gmail.com>
2021-12-02 01:59:24 +0800

    a

--- codeforces/520B/src/Main.java
+++ codeforces/520B/src/Main.java
@@ -2,36 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    int dp(int n, int m) {
+        if (n >= m) {
+            return n - m;
+        }
+        
+    }
+
     void solve() {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int m = in.nextInt();
-        int c = 0;
-        if (n < m) {
-            while (n * 2 < m) {
-                n *= 2;
-                c++;
-            }
-            if (n * 2 == m) {
-                c++;
-            } else {
-                while ((n - 1) * 2 >= m) {
-                    n--;
-                    c++;
-                }
-                n *= 2;
-                c++;
-                while (n > m) {
-                    n--;
-                    c++;
-                }
-            }
-        } else {
-            while (n > m) {
-                n--;
-                c++;
-            }
-        }
+        dp(n, m);
         System.out.print(c);
     }
 
