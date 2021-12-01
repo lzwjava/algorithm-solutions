eb67571d5fd5d148b6fb8b4b4670e1aa6929d596
lzwjava <lzwjava@gmail.com>
2021-12-01 22:56:10 +0800

    a

--- codeforces/1613B/src/Main.java
+++ codeforces/1613B/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,7 +12,27 @@ public class Main {
             for (int i = 0; i < n; i++) {
                 a[i] = in.nextInt();
             }
-            
+            int p = n / 2;
+            Arrays.sort(a);
+            int c = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i != j) {
+                        int m = a[i] % a[j];
+                        int index = Arrays.binarySearch(a, m);
+                        if (index < 0) {
+                            System.out.println(String.format("%d %d", a[i], a[j]));
+                            c++;
+                            if (c == p) {
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (c == p) {
+                    break;
+                }
+            }
             t--;
         }
     }
