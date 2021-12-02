0149d196c3af039a7f40d9a3ca4b51bea954c627
lzwjava <lzwjava@gmail.com>
2021-12-03 04:31:16 +0800

    a

--- 225/src/Main.java
+++ 225/src/Main.java
@@ -2,8 +2,29 @@ import java.util.Scanner;
 
 public class Main {
 
+    class Point {
+        int x, y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     void solve() {
         Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t > 0) {
+            int longest = in.nextInt();
+            int blocked = in.nextInt();
+            Point[] points = new Point[blocked];
+            for (int i = 0; i < blocked; i++) {
+                int x = in.nextInt();
+                int y = in.nextInt();
+                points[i] = new Point(x, y);
+            }
+            t--;
+        }
     }
 
     public static void main(String[] args) {
