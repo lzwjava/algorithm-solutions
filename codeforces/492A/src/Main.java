7403876bf26f836a2a6160f55081aa1f14f32b99
lzwjava <lzwjava@gmail.com>
2021-12-02 11:04:51 +0800

    a

--- codeforces/492A/src/Main.java
+++ codeforces/492A/src/Main.java
@@ -5,6 +5,20 @@ public class Main {
     void solve() {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        int maxn = 100;
+        int[] p = new int[maxn];
+        for (int i = 0; i < maxn; i++) {
+            int d = i + 1;
+            p[i] = (1 + d) * d / 2;
+        }
+        int i = 0;
+        while (true) {
+            if (n - p[i] >= 0) {
+                n -= p[i];
+            } else {
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
