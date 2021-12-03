a6991db51f7756d835276528090d7e027d9d5da1
lzwjava <lzwjava@gmail.com>
2021-12-03 10:06:31 +0800

    a

--- uva/211/src/Main.java
+++ uva/211/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -13,8 +14,14 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
-    void solve() {
-        
+    void solve() throws IOException {
+        int[][] grid = new int[7][8];
+        for (int i = 0; i < 7; i++) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            for (int j = 0; j < 8; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
     }
 
     void close() throws IOException {
