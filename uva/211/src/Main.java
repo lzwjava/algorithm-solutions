c797bbe6e0e32eb65b8769f9daea2b92cd91b49b
lzwjava <lzwjava@gmail.com>
2021-12-03 10:59:28 +0800

    a

--- uva/211/src/Main.java
+++ uva/211/src/Main.java
@@ -1,4 +1,6 @@
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -6,6 +8,7 @@ public class Main {
 
     Scanner in;
     PrintWriter out;
+    int caseNum;
 
     Main() {
         in = new Scanner(System.in);
@@ -15,6 +18,8 @@ public class Main {
     int[][] grid;
     int[][] nums;
     Bone[] bones;
+    int cnt;
+    boolean[] vis;
 
     class Bone {
         int id;
@@ -31,8 +36,24 @@ public class Main {
     int[] dx = new int[]{1, 0};
     int[] dy = new int[]{0, 1};
 
+    void print(int[][] g) {
+        out.append('\n');
+        int n = g.length;
+        int m = g[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                out.append(String.format("%4d", g[i][j]));
+            }
+            out.append('\n');
+        }
+        out.append('\n');
+    }
+
     void dfs(int cur) {
         if (cur == 56) {
+            cnt++;
+            print(nums);
+            out.flush();
             return;
         }
         int x = cur / 8;
@@ -41,6 +62,9 @@ public class Main {
             dfs(cur + 1);
         } else {
             for (int i = 0; i < bones.length; i++) {
+                if (vis[i]) {
+                    continue;
+                }
                 Bone bn = bones[i];
                 for (int d = 0; d < 2; d++) {
                     int nx = x + dx[d];
@@ -48,7 +72,14 @@ public class Main {
                     if (nx >= 7 || ny >= 8) {
                         continue;
                     }
-                    for (int k = 0; k < 2; k++) {
+                    if (nums[nx][ny] != 0) {
+                        continue;
+                    }
+                    int v = 1;
+                    if (bn.a != bn.b) {
+                        v = 2;
+                    }
+                    for (int k = 0; k < v; k++) {
                         int p, q;
                         if (k == 0) {
                             p = bn.a;
@@ -58,7 +89,11 @@ public class Main {
                             q = bn.a;
                         }
                         if (p == grid[x][y] && q == grid[nx][ny]) {
-                            
+                            nums[x][y] = nums[nx][ny] = bn.id;
+                            vis[i] = true;
+                            dfs(cur + 1);
+                            vis[i] = false;
+                            nums[x][y] = nums[nx][ny] = 0;
                         }
                     }
                 }
@@ -67,14 +102,15 @@ public class Main {
     }
 
     void solve() throws IOException {
-        int p = 1;
+        int p = 0;
         bones = new Bone[28];
-        for (int i = 0; i < 6; i++) {
-            for (int j = i; j < 6; j++) {
-                Bone bone = new Bone(p, i, j);
+        for (int i = 0; i <= 6; i++) {
+            for (int j = i; j <= 6; j++) {
+                Bone bone = new Bone(p + 1, i, j);
                 bones[p++] = bone;
             }
         }
+        caseNum = 1;
         while (in.hasNextInt()) {
             grid = new int[7][8];
             for (int i = 0; i < 7; i++) {
@@ -83,7 +119,19 @@ public class Main {
                 }
             }
             nums = new int[7][8];
+            vis = new boolean[28];
+            cnt = 0;
+            if (caseNum != 1) {
+                out.append("\n\n\n\n\n");
+            }
+            out.append(String.format("Layout #%d:\n\n", caseNum));
+            print(grid);
+            out.append(String.format("Maps resulting from layout #%d are:\n\n", caseNum));
+
             dfs(0);
+
+            out.append(String.format("\nThere are %d solution(s) for layout #%d.\n", cnt, caseNum));
+            caseNum++;
         }
     }
 
@@ -94,7 +142,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-//        System.setOut(new PrintStream(new FileOutputStream("1.out")));
+        System.setOut(new PrintStream(new FileOutputStream("1.out")));
         Main m = new Main();
         m.solve();
         m.close();
