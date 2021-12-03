0ea3d66d3684281e05c376b3b38119d9df9f7f7c
lzwjava <lzwjava@gmail.com>
2021-12-03 11:54:59 +0800

    a

--- uva/818/src/Main.java
+++ uva/818/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -13,8 +15,24 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
-    void solve() {
-        
+    class Pair {
+        int a, b;
+
+        Pair(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+    void solve() throws IOException {
+        while (true) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            if (n == 0) {
+                break;
+            }
+            ArrayList<Pair> list = new ArrayList<>();
+        }
     }
 
     void close() throws IOException {
