d71a0d59305a7b977206fe1c296a32ae81fe69a6
lzwjava <lzwjava@gmail.com>
2021-12-03 13:56:24 +0800

    a

--- uva/818/src/Main.java
+++ uva/818/src/Main.java
@@ -2,10 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -17,12 +14,17 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
-    class Pair {
+    class Pair implements Comparable<Pair> {
         int a, b;
 
         Pair(int a, int b) {
             this.a = a;
             this.b = b;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return Integer.compare(a, o.a);
         }
     }
 
@@ -58,28 +60,46 @@ public class Main {
         }
     }
 
+    void count(Map<Integer, Integer> map, int a) {
+        Integer count = map.get(a);
+        if (count == null) {
+            count = 0;
+        }
+        count++;
+        map.put(a, count);
+    }
+
     void dfs(boolean[] cut, int cur, int cnt) {
         if (cnt > min) {
             return;
         }
         if (cur == m) {
+//            if (cut[2]) {
+//                System.out.println();
+//            }
             parent = new int[n + 1];
             rank = new int[n + 1];
             for (int i = 1; i <= n; i++) {
                 parent[i] = i;
             }
             boolean cyclic = false;
+
+            List<Pair> pairs = new ArrayList<>();
+
             for (int i = 0; i < m; i++) {
                 if (!cut[i]) {
                     Pair p = list.get(i);
-                    int fa = find(p.a);
-                    int fb = find(p.b);
-                    if (fa == fb) {
-                        cyclic = true;
-                        break;
-                    } else {
-                        union(p.a, p.b);
-                    }
+                    pairs.add(p);
+                }
+            }
+            for (Pair p : pairs) {
+                int fa = find(p.a);
+                int fb = find(p.b);
+                if (fa == fb) {
+                    cyclic = true;
+                    break;
+                } else {
+                    union(p.a, p.b);
                 }
             }
             if (!cyclic) {
@@ -89,15 +109,52 @@ public class Main {
                     set.add(fi);
                 }
                 int group = set.size();
-                int link = group - 1;
-                int ans;
-                if (cnt >= link) {
-                    ans = cnt;
-                } else {
-                    ans = cnt + link - cnt;
+                boolean single = true;
+                for (int g : set) {
+                    List<Pair> groupPairs = new ArrayList<>();
+                    for (Pair p : pairs) {
+                        int ga = find(p.a);
+                        if (ga == g) {
+                            groupPairs.add(p);
+                        }
+                    }
+                    if (groupPairs.size() == 0) {
+                        continue;
+                    }
+                    Map<Integer, Integer> map = new HashMap<>();
+                    int gn = groupPairs.size();
+                    for (int i = 0; i < gn; i++) {
+                        Pair p = groupPairs.get(i);
+                        count(map, p.a);
+                        count(map, p.b);
+                    }
+                    int c1 = 0, c2 = 0, c3 = 0;
+                    for (int key : map.keySet()) {
+                        int c = map.get(key);
+                        if (c == 1) {
+                            c1++;
+                        } else if (c == 2) {
+                            c2++;
+                        } else {
+                            c3++;
+                        }
+                    }
+                    if (c3 > 0 || c1 != 2) {
+                        single = false;
+                        break;
+                    }
                 }
-                if (ans < min) {
-                    min = ans;
+                if (single) {
+                    int link = group - 1;
+                    int ans;
+                    if (cnt >= link) {
+                        ans = cnt;
+                    } else {
+                        ans = cnt + link - cnt;
+                    }
+                    if (ans < min) {
+                        min = ans;
+                    }
                 }
             }
             return;
