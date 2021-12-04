f58d855dbacf300717787d004e2a3176c8dff5ab
lzwjava <lzwjava@gmail.com>
2021-12-04 08:38:28 +0800

    a

--- uva/12558/src/Main.java
+++ uva/12558/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,18 +15,37 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
+    int a, b, k;
+    int[] ks;
+    double target;
+
+    void dfs(List<Integer> dms, int start, double sum) {
+        if (Math.abs(target - sum) < 1e-8) {
+            return;
+        }
+        if (sum > target) {
+            return;
+        }
+        for (int i = start; ; i++) {
+            dms.add(i);
+            
+            dms.remove(dms.size() - 1);
+        }
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
-            int a, b, k;
+
             StringTokenizer st = new StringTokenizer(in.readLine());
             a = Integer.parseInt(st.nextToken());
             b = Integer.parseInt(st.nextToken());
             k = Integer.parseInt(st.nextToken());
-            int[] ks = new int[k];
+            ks = new int[k];
             for (int i = 0; i < k; i++) {
                 ks[i] = Integer.parseInt(st.nextToken());
             }
+            target = a * 1.0 / b;
             t--;
         }
     }
