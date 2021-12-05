6a20db730e8bc6c195de865e8e8406f87226a928
lzwjava <lzwjava@gmail.com>
2021-12-05 13:21:36 +0800

    a

--- uva/12113/src/Main.java
+++ uva/12113/src/Main.java
@@ -14,7 +14,8 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
-    char[][] grid;
+    char[][][] grids;
+    int cnt;
 
     void draw(char[][] g, int v) {
         char[][] gt = new char[][]{
@@ -26,7 +27,7 @@ public class Main {
         int oy = (v % 3) * 2;
         for (int x = 0; x < gt.length; x++) {
             for (int y = 0; y < gt[0].length; y++) {
-                if (x == 0 && (y == 0 || y == 4)) {
+                if (x == 0 && (y == 0 || y == 2 || y == 4)) {
                     continue;
                 }
                 int nx = ox + x;
@@ -57,7 +58,7 @@ public class Main {
         out.append('\n');
     }
 
-    boolean permutation(int[] nums, boolean[] vis, int cur, int n) {
+    void permutation(int[] nums, boolean[] vis, int cur, int n) {
         if (cur == n) {
             char[][] g = new char[5][9];
             for (int i = 0; i < 5; i++) {
@@ -66,24 +67,40 @@ public class Main {
             for (int i = 0; i < n; i++) {
                 draw(g, nums[i]);
             }
-            if (equal(grid, g)) {
-                return true;
-            }
-            return false;
+            addToAns(g);
+            return;
         }
         for (int i = 0; i < 9; i++) {
             if (!vis[i]) {
                 nums[cur] = i;
-                boolean found = permutation(nums, vis, cur + 1, n);
-                if (found) {
-                    return true;
-                }
+                permutation(nums, vis, cur + 1, n);
+            }
+        }
+    }
+
+    void addToAns(char[][] g) {
+        grids[cnt++] = g;
+    }
+
+    boolean find(char[][] g) {
+        for (int i = 0; i < cnt; i++) {
+            if (equal(grids[i], g)) {
+                return true;
             }
         }
         return false;
     }
 
     void solve() throws IOException {
+        int maxn = 597875;
+        grids = new char[maxn][5][9];
+        cnt = 0;
+        for (int p = 1; p <= 6; p++) {
+            int[] nums = new int[p];
+            boolean[] vis = new boolean[9];
+            permutation(nums, vis, 0, p);
+        }
+
         int caseNum = 1;
         while (true) {
             String line = in.readLine();
@@ -91,7 +108,7 @@ public class Main {
                 break;
             }
             int i = 0;
-            grid = new char[5][9];
+            char[][] grid = new char[5][9];
             do {
                 for (int j = 0; j < 9; j++) {
                     grid[i][j] = line.charAt(j);
@@ -101,16 +118,7 @@ public class Main {
                     line = in.readLine();
                 }
             } while (i < 5);
-            boolean ok = false;
-            for (int p = 1; p <= 6; p++) {
-                int[] nums = new int[p];
-                boolean[] vis = new boolean[9];
-                boolean found = permutation(nums, vis, 0, p);
-                if (found) {
-                    ok = true;
-                    break;
-                }
-            }
+            boolean ok = find(grid);
             out.append(String.format("Case %d: ", caseNum));
             if (ok) {
                 out.append("Yes\n");
