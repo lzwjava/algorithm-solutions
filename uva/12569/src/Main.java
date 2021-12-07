13396276a399dccf32a8bfa8d10b215011e4fec6
lzwjava <lzwjava@gmail.com>
2021-12-07 14:20:39 +0800

    a

--- uva/12569/src/Main.java
+++ uva/12569/src/Main.java
@@ -14,16 +14,31 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
+    int n, m, s, t;
+    int[] os;
+    boolean[][] grid;
+
     void solve() throws IOException {
-        int t = Integer.parseInt(in.readLine());
-        while (t > 0) {
+        int tt = Integer.parseInt(in.readLine());
+        while (tt > 0) {
             StringTokenizer st = new StringTokenizer(in.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            int s = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            
-            t--;
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            s = Integer.parseInt(st.nextToken()) - 1;
+            t = Integer.parseInt(st.nextToken()) - 1;
+            os = new int[m];
+            st = new StringTokenizer(in.readLine());
+            for (int i = 0; i < m; i++) {
+                os[i] = Integer.parseInt(st.nextToken());
+            }
+            grid = new boolean[n][n];
+            for (int i = 0; i < n - 1; i++) {
+                st = new StringTokenizer(in.readLine());
+                int u = Integer.parseInt(st.nextToken()) - 1;
+                int v = Integer.parseInt(st.nextToken()) - 1;
+                grid[u][v] = grid[v][u] = true;
+            }
+            tt--;
         }
     }
 
