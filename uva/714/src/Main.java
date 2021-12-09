46927fbea2dfec6e142ab2933429256c15f98bd4
lzwjava <lzwjava@gmail.com>
2021-12-09 13:40:03 +0800

    a

--- uva/714/src/Main.java
+++ uva/714/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -13,8 +14,15 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
-    void solve() {
-
+    void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            
+            t--;
+        }
     }
 
     void close() throws IOException {
