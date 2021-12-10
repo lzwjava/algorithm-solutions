3a2f29eeff08e006af4c89f7590d1508b5361e1b
lzwjava <lzwjava@gmail.com>
2021-12-10 21:45:30 +0800

    a

--- codeforces/1352C/src/Main.java
+++ codeforces/1352C/src/Main.java
@@ -1,13 +1,50 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    void solve() {
-        Scanner in = new Scanner(System.in);
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
     }
 
-    public static void main(String[] args) {
-        new Main().solve();
+    void close() throws IOException {
+        in.close();
+        out.flush();
+        out.close();
+    }
+
+    void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            // x - x/n = k
+            int x = (int) Math.floor(k * 1.0 / (1 - 1.0 / n));
+            x -= 1;
+            while (true) {
+                if (x - x / n == k) {
+                    break;
+                }
+                x++;
+            }
+            out.append(String.format("%d\n", x));
+            t--;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
     }
 
 }
