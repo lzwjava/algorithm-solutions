011ce68bbf1eedded41c39af5eb2fbdba48cb6c5
lzwjava <lzwjava@gmail.com>
2021-12-10 16:02:03 +0800

    a

--- uva/1606/src/Main.java
+++ uva/1606/src/Main.java
@@ -38,6 +38,44 @@ public class Main {
                 int r = Integer.parseInt(st.nextToken());
                 ps[i] = new Particle(x, y, r);
             }
+
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    Particle pi = ps[i];
+                    Particle pj = ps[j];
+                    double k = (pi.y - pj.y) * 1.0 / (pi.x - pj.x);
+                    double b = pi.y - pi.x * k;
+
+                    int[][] rs = new int[2][3];
+
+                    for (int u = 0; u < n; u++) {
+                        Particle pu = ps[u];
+                        double y1 = k * pu.x + b;
+                        if (Math.abs(y1 - pu.y) < 1e-8) {
+                            rs[pu.r][0]++;
+                        } else if (pu.y < y1) {
+                            rs[pu.r][1]++;
+                        } else {
+                            rs[pu.r][2]++;
+                        }
+                    }
+
+                    int white = 0, black = 0;
+                    white = rs[0][1] + rs[0][0];
+                    black = rs[1][2] + rs[1][0];
+
+                    int total = white + black;
+                    white = rs[0][2] + rs[0][0];
+                    black = rs[1][1] + rs[1][0];
+
+                    total = Integer.max(total, white + black);
+                    if (total > max) {
+                        max = total;
+                    }
+                }
+            }
+            out.append(String.format("%d\n", max));
         }
     }
 
