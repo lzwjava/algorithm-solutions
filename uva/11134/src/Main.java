275a852d7c3f6c5db4479146b760625110cf9ed5
lzwjava <lzwjava@gmail.com>
2021-12-10 10:56:06 +0800

    a

--- uva/11134/src/Main.java
+++ uva/11134/src/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -14,50 +14,38 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
-    int[] xl, yl, xr, yr;
     int n;
     boolean found;
 
-    class Point {
-        int x, y;
+    class Edge implements Comparable<Edge> {
+        int l, r, id;
 
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+        Edge(int l, int r) {
+            this.l = l;
+            this.r = r;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(r, o.r);
         }
     }
 
-    void permutation(Point[] ps, int i) {
-        if (found) {
-            return;
-        }
-        if (i == n) {
-            found = true;
-            for (int j = 0; j < n; j++) {
-                Point p = ps[j];
-                out.append(String.format("%d %d\n", p.x + 1, p.y + 1));
-            }
-            return;
-        }
-        for (int x = xl[i]; x <= xr[i]; x++) {
-            for (int y = yl[i]; y <= yr[i]; y++) {
-                ps[i] = new Point(x, y);
-                if (check(ps, i + 1)) {
-                    permutation(ps, i + 1);
-                }
-            }
-        }
-    }
+    Edge[] xs, ys;
 
-    boolean check(Point[] ps, int m) {
-        Point last = ps[m - 1];
-        for (int i = 0; i < m - 1; i++) {
-            Point p = ps[i];
-            if (p.x == last.x || p.y == last.y) {
-                return false;
+    int lowerBound(List<Integer> list, int key) {
+        int idx = Collections.binarySearch(list, key);
+        if (idx < 0) {
+            return Math.abs(idx) - 1;
+        } else {
+            while (idx > 0) {
+                if (list.get(idx - 1) == key)
+                    idx--;
+                else
+                    return idx;
             }
+            return idx;
         }
-        return true;
     }
 
     void solve() throws IOException {
@@ -66,22 +54,61 @@ public class Main {
             if (n == 0) {
                 break;
             }
-            xl = new int[n];
-            yl = new int[n];
-            xr = new int[n];
-            yr = new int[n];
+            xs = new Edge[n];
+            ys = new Edge[n];
+
+            List<Integer> xlist = new ArrayList<>();
+            List<Integer> ylist = new ArrayList<>();
+
             for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(in.readLine());
-                xl[i] = Integer.parseInt(st.nextToken()) - 1;
-                yl[i] = Integer.parseInt(st.nextToken()) - 1;
-                xr[i] = Integer.parseInt(st.nextToken()) - 1;
-                yr[i] = Integer.parseInt(st.nextToken()) - 1;
+                int xl = Integer.parseInt(st.nextToken()) - 1;
+                int yl = Integer.parseInt(st.nextToken()) - 1;
+                int xr = Integer.parseInt(st.nextToken()) - 1;
+                int yr = Integer.parseInt(st.nextToken()) - 1;
+                xs[i] = new Edge(xl, xr);
+                ys[i] = new Edge(yl, yr);
+                xs[i].id = i;
+                ys[i].id = i;
+                xlist.add(i);
+                ylist.add(i);
             }
 
-            Point[] ps = new Point[n];
-            found = false;
-            permutation(ps, 0);
-            if (!found) {
+            int maxn = 5005;
+
+            xlist.add(maxn);
+            ylist.add(maxn);
+
+            Arrays.sort(xs);
+            Arrays.sort(ys);
+
+            boolean ok = true;
+            int[] idx = new int[n];
+            int[] idy = new int[n];
+            for (int i = 0; i < n; i++) {
+                int ix = lowerBound(xlist, xs[i].l);
+                int tx = ix;
+                if (xlist.get(tx) > xs[i].r) {
+                    ok = false;
+                    break;
+                }
+                idx[xs[i].id] = tx;
+                xlist.remove((Integer) tx);
+
+                int iy = lowerBound(ylist, ys[i].l);
+                int ty = iy;
+                if (ty > ys[i].r) {
+                    ok = false;
+                    break;
+                }
+                idy[ys[i].id] = ty;
+                ylist.remove((Integer) ty);
+            }
+            if (ok) {
+                for (int i = 0; i < n; i++) {
+                    out.append(String.format("%d %d\n", idx[i] + 1, idy[i] + 1));
+                }
+            } else {
                 out.append("IMPOSSIBLE\n");
             }
         }
