50b21d3d80cdbfd60065089f9c256dc72b9f00f2
lzwjava <lzwjava@gmail.com>
2021-12-10 20:08:16 +0800

    a

--- codeforces/1462A/src/Main.java
+++ codeforces/1462A/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,10 +9,29 @@ public class Main {
         int t = in.nextInt();
         while (t > 0) {
             int n = in.nextInt();
-            int[] b = new int[n];
+            LinkedList<Integer> list = new LinkedList<>();
             for (int i = 0; i < n; i++) {
-                b[i] = in.nextInt();
+                list.add(in.nextInt());
             }
+            ArrayList<Integer> ans = new ArrayList<>();
+            boolean left = true;
+            while (!list.isEmpty()) {
+                int v;
+                if (left) {
+                    v = list.pollFirst();
+                } else {
+                    v = list.pollLast();
+                }
+                ans.add(v);
+                left = !left;
+            }
+            for (int i = 0; i < n; i++) {
+                if (i != 0) {
+                    System.out.print(' ');
+                }
+                System.out.print(ans.get(i));
+            }
+            System.out.println();
             t--;
         }
     }
