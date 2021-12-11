933cf0ffe41754619c6da75e91bb371c5ecc0c62
lzwjava <lzwjava@gmail.com>
2021-12-11 16:04:43 +0800

    a

--- codeforces/313B/src/Main.java
+++ codeforces/313B/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -19,7 +20,38 @@ public class Main {
         out.close();
     }
 
+    int[] sums;
+
     void solve() throws IOException {
+        String s = in.readLine();
+        int n = s.length();
+        int[] v = new int[n];
+        sums = new int[n];
+        int d = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                v[i] = 1;
+            }
+            d += v[i];
+            sums[i] = d;
+        }
+        int m = Integer.parseInt(in.readLine());
+        while (m > 0) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int l = Integer.parseInt(st.nextToken()) - 1;
+            int r = Integer.parseInt(st.nextToken()) - 1;
+            int cnt = sum(l, r);
+            out.append(String.format("%d\n", cnt));
+            m--;
+        }
+    }
+
+    int sum(int l, int r) {
+        if (l == 0) {
+            return sums[r - 1];
+        } else {
+            return sums[r - 1] - sums[l - 1];
+        }
     }
 
     public static void main(String[] args) throws IOException {
