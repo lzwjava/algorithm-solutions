83deb2375985857ad5c07eafdb552d499119a109
lzwjava <lzwjava@gmail.com>
2021-12-11 12:10:53 +0800

    a

--- codeforces/189A/src/Main.java
+++ codeforces/189A/src/Main.java
@@ -1,18 +1,68 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
 
 public class Main {
 
-    void solve() {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
-        
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
     }
 
-    public static void main(String[] args) {
-        new Main().solve();
+    void close() throws IOException {
+        in.close();
+        out.flush();
+        out.close();
+    }
+
+    int n;
+    List<Integer> a;
+    int max;
+    int total;
+
+    void cal(int st, int cur, int s) {
+        if (total > 10) {
+            return;
+        }
+        if (s == 0) {
+            total++;
+            if (cur > max) {
+                max = cur;
+            }
+            return;
+        }
+        for (int i = st; i < a.size(); i++) {
+            if (s >= a.get(i)) {
+                cal(i, cur + 1, s - a.get(i));
+            }
+        }
+    }
+
+    void solve() throws IOException {
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        n = Integer.parseInt(st.nextToken());
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < 3; i++) {
+            int v = Integer.parseInt(st.nextToken());
+            set.add(v);
+        }
+        a = new ArrayList<>(set);
+        Collections.sort(a);
+        max = -1;
+        total = 0;
+        cal(0, 0, n);
+        out.append(String.format("%d\n", max));
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
     }
 
 }
