30a685ecf836e3c03acc34f84e643c50163bd875
lzwjava <lzwjava@gmail.com>
2021-12-11 10:33:57 +0800

    a

--- codeforces/1369A/src/Main.java
+++ codeforces/1369A/src/Main.java
@@ -1,13 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class Main {
 
-    void solve() {
-        Scanner in = new Scanner(System.in);
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
     }
 
-    public static void main(String[] args) {
-        new Main().solve();
+    void close() throws IOException {
+        in.close();
+        out.flush();
+        out.close();
+    }
+
+    void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            int n = Integer.parseInt(in.readLine());
+            if (n % 4 == 0) {
+                out.append("YES\n");
+            } else {
+                out.append("NO\n");
+            }
+            t--;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
     }
 
 }
--- codeforces/519B/src/Main.java
+++ codeforces/519B/src/Main.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,21 +23,42 @@ public class Main {
         out.close();
     }
 
+    int diff(List<Integer> a, List<Integer> b) {
+        for (int i = 0; i < b.size(); i++) {
+            if (!a.get(i).equals(b.get(i))) {
+                return a.get(i);
+            }
+        }
+        return a.get(a.size() - 1);
+    }
+
     void solve() throws IOException {
         int n = Integer.parseInt(in.readLine());
-        int[] a = new int[n];
-        int[] b = new int[n - 1];
-        int[] c = new int[n - 2];
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+        List<Integer> c = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(in.readLine());
         for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            a.add(v);
         }
+        st = new StringTokenizer(in.readLine());
         for (int i = 0; i < n - 1; i++) {
-            b[i] = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            b.add(v);
         }
+        st = new StringTokenizer(in.readLine());
         for (int i = 0; i < n - 2; i++) {
-            c[i] = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            c.add(v);
         }
+        Collections.sort(a);
+        Collections.sort(b);
+        Collections.sort(c);
+        int d1 = diff(a, b);
+        int d2 = diff(b, c);
+        out.append(String.format("%d\n", d1));
+        out.append(String.format("%d\n", d2));
     }
 
     public static void main(String[] args) throws IOException {
