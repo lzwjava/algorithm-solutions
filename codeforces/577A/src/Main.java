8d6da6f1efda84ee5bb6f1fcc1b67a6388a0080a
lzwjava <lzwjava@gmail.com>
2021-12-12 15:18:20 +0800

    a

--- codeforces/577A/src/Main.java
+++ codeforces/577A/src/Main.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -19,7 +22,62 @@ public class Main {
         out.close();
     }
 
+    List<Integer> list;
+
+    void calPrimes() {
+        int maxn = 31625;
+        boolean[] prime = new boolean[maxn];
+        prime[2] = true;
+        for (int i = 2; i < maxn; i += 2) {
+            prime[i] = false;
+        }
+        list = new ArrayList<>();
+        list.add(2);
+        for (int i = 3; i < maxn; i += 2) {
+            if (prime[i]) {
+                list.add(i);
+                if (i * i > 0) {
+                    for (int j = i * i; j < maxn; j += i) {
+                        prime[j] = false;
+                    }
+                }
+            }
+        }
+    }
+
+    class Factor {
+        int p, c;
+
+        Factor(int p, int c) {
+            this.p = p;
+            this.c = c;
+        }
+    }
+
+    List<Factor> calFactors(int x) {
+        List<Factor> fs = new ArrayList<>();
+        int sx = (int) Math.sqrt(x);
+        for (int p : list) {
+            if (p > sx) {
+                break;
+            }
+            if (x % p == 0) {
+                int c = 0;
+                while (x % p == 0) {
+                    x /= p;
+                    c++;
+                }
+                fs.add(new Factor(p, c));
+            }
+        }
+        return fs;
+    }
+    
     void solve() throws IOException {
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
     }
 
     public static void main(String[] args) throws IOException {
