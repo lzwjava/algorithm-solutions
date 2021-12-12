66f6b7bf92acf84422520c10654387682fe17bdb
lzwjava <lzwjava@gmail.com>
2021-12-12 14:31:27 +0800

    a

--- codeforces/1342A/src/Main.java
+++ codeforces/1342A/src/Main.java
@@ -20,15 +20,31 @@ public class Main {
         out.close();
     }
 
+    int[] dx = new int[]{-1, 1, 0, 0, 1, -1};
+    int[] dy = new int[]{0, 0, -1, 1, 1, -1};
+
     void solve() throws IOException {
-        int t = Integer.parseInt(in.readLine());
-        while (t > 0) {
+        int tt = Integer.parseInt(in.readLine());
+        while (tt > 0) {
             StringTokenizer st = new StringTokenizer(in.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(in.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+            if (x > y) {
+                int t = x;
+                x = y;
+                y = t;
+            }
+            long ans;
+            if (2 * a <= b) {
+                ans = (long) a * (x + y);
+            } else {
+                ans = (long) x * b + (long) (y - x) * a;
+            }
+            out.append(String.format("%d\n", ans));
+            tt--;
         }
     }
 
