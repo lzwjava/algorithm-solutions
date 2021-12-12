747172ff63357c44e22716e8bdf201fb0a3e70cb
lzwjava <lzwjava@gmail.com>
2021-12-12 10:10:45 +0800

    a

--- codeforces/474B/src/Main.java
+++ codeforces/474B/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -24,13 +25,22 @@ public class Main {
         int n = Integer.parseInt(in.readLine());
         int[] a = new int[n];
         StringTokenizer st = new StringTokenizer(in.readLine());
+        int[] s = new int[n];
+        int p = 0;
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
+            p += a[i];
+            s[i] = p;
         }
         int m = Integer.parseInt(in.readLine());
         st = new StringTokenizer(in.readLine());
         while (m > 0) {
             int q = Integer.parseInt(st.nextToken());
+            int i = Arrays.binarySearch(s, q);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            out.append(String.format("%d\n", i + 1));
             m--;
         }
     }
