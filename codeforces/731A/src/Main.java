064ded6b4eb723c9f9f2f430f2443e0145410bdc
lzwjava <lzwjava@gmail.com>
2021-12-12 11:02:38 +0800

    a

--- codeforces/731A/src/Main.java
+++ codeforces/731A/src/Main.java
@@ -19,7 +19,27 @@ public class Main {
         out.close();
     }
 
+    int charToInt(char ch) {
+        return ch - 'a';
+    }
+
     void solve() throws IOException {
+        String s = in.readLine();
+        int a = 0;
+        int p = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int b = charToInt(c);
+            int d1 = (b - a + 26) % 26;
+            int d2 = (a + 26 - b) % 26;
+            if (d1 <= d2) {
+                p += d1;
+            } else {
+                p += d2;
+            }
+            a = b;
+        }
+        out.append(String.format("%d\n", p));
     }
 
     public static void main(String[] args) throws IOException {
