1790bba6b09ac35f8aa5656d5d2cc153a6caa514
lzwjava <lzwjava@gmail.com>
2021-12-12 22:21:57 +0800

    a

---
+++ codeforces/1372A/src/Main.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    void close() throws IOException {
+        in.close();
+        out.flush();
+        out.close();
+    }
+
+    void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            int n = Integer.parseInt(in.readLine());
+            int d = n / 2;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                if (i < d) {
+                    a[i] = 1;
+                } else {
+                    a[i] = 3;
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                if (i != 0) {
+                    out.append(' ');
+                }
+                out.append(String.format("%d", a[i]));
+            }
+            out.append('\n');
+            t--;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
+    }
+
+}
