5547b3767703dd5be468fbb95fa88ae40bfcdd40
lzwjava <lzwjava@gmail.com>
2021-12-13 15:47:29 +0800

    a

--- codeforces/1337A/src/Main.java
+++ codeforces/1337A/src/Main.java
@@ -55,6 +55,15 @@ public class Main {
         }
     }
 
+    void print(Range[] rs, int x, int y, int z) {
+        Item[] is = new Item[3];
+        is[0] = new Item(x, rs[0].id);
+        is[1] = new Item(y, rs[1].id);
+        is[2] = new Item(z, rs[2].id);
+        Arrays.sort(is);
+        out.append(String.format("%d %d %d\n", is[0].x, is[1].x, is[2].x));
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
@@ -67,25 +76,18 @@ public class Main {
             rs[0] = new Range(a, b, 0);
             rs[1] = new Range(b, c, 1);
             rs[2] = new Range(c, d, 2);
-
             Arrays.sort(rs);
-
             boolean found = false;
             for (int x = rs[0].a; x <= rs[0].b && !found; x++) {
                 for (int y = rs[1].a; y <= rs[1].b && !found; y++) {
                     int u = Math.abs(x - y);
                     int v = x + y;
-                    for (int z = u + 1; z < v; z++) {
-                        if (z >= rs[2].a && z <= rs[2].b) {
-                            Item[] is = new Item[3];
-                            is[0] = new Item(x, rs[0].id);
-                            is[1] = new Item(y, rs[1].id);
-                            is[2] = new Item(z, rs[2].id);
-                            Arrays.sort(is);
-                            out.append(String.format("%d %d %d\n", is[0].x, is[1].x, is[2].x));
-                            found = true;
-                            break;
-                        }
+                    int m1 = Math.max(u + 1, rs[2].a);
+                    int m2 = Math.min(v - 1, rs[2].b);
+                    for (int z = m1; z <= m2; z++) {
+                        found = true;
+                        print(rs, x, y, z);
+                        break;
                     }
                 }
             }
