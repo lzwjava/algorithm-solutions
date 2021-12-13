a959039fe484b5f42b48cbbd4eb59275df86cdb2
lzwjava <lzwjava@gmail.com>
2021-12-13 14:12:49 +0800

    a

--- codeforces/476A/src/Main.java
+++ codeforces/476A/src/Main.java
@@ -24,6 +24,31 @@ public class Main {
         StringTokenizer st = new StringTokenizer(in.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        int a, b;
+        if (n % 2 == 1) {
+            b = (n - 1) / 2;
+            a = b + 1;
+        } else {
+            b = n / 2;
+            a = b;
+        }
+        if (m <= a + b) {
+            if (a % m != 0) {
+                for (int i = 0; i < b; i++) {
+                    a++;
+                    if (a % m == 0) {
+                        break;
+                    }
+                }
+            }
+            if (a % m != 0) {
+                out.append("-1\n");
+            } else {
+                out.append(String.format("%d\n", a));
+            }
+        } else {
+            out.append("-1\n");
+        }
     }
 
     public static void main(String[] args) throws IOException {
