998925f82b1b3b9bf3e0567299f8d4cd75c94cea
lzwjava <lzwjava@gmail.com>
2021-12-13 08:48:54 +0800

    a

--- codeforces/279B/src/Main.java
+++ codeforces/279B/src/Main.java
@@ -34,28 +34,29 @@ public class Main {
             sums[i] = sum;
         }
         int max = 0;
+        int p = -1, s = 0;
         for (int i = 0; i < n; i++) {
-            int j = i;
-            if (a[j] > t) {
-                continue;
-            }
-            while (j + 1 < n && sum(sums, i, j + 1) <= t) {
-                j++;
-            }
-            int len = j - i + 1;
-            if (len > max) {
-                max = len;
+            if (a[i] > t) {
+                p = -1;
+                s = 0;
+            } else {
+                if (p == -1) {
+                    p = i;
+                    s = a[i];
+                } else {
+                    while (s + a[i] > t) {
+                        s -= a[p];
+                        p++;
+                    }
+                    s += a[i];
+                }
+                int len = i - p + 1;
+                if (len > max) {
+                    max = len;
+                }
             }
         }
         out.append(String.format("%d\n", max));
-    }
-
-    int sum(int[] sums, int i, int j) {
-        if (i == 0) {
-            return sums[j];
-        } else {
-            return sums[j] - sums[i - 1];
-        }
     }
 
     public static void main(String[] args) throws IOException {
