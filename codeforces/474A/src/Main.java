ae9fd54fa4c6bca8e40ccb1cc94ff6f175557972
lzwjava <lzwjava@gmail.com>
2021-12-13 14:26:57 +0800

    a

--- codeforces/474A/src/Main.java
+++ codeforces/474A/src/Main.java
@@ -20,7 +20,35 @@ public class Main {
     }
 
     void solve() throws IOException {
-        
+        String[] strs = new String[]{"qwertyuiop", "asdfghjkl;", "zxcvbnm,./"};
+        char ch = in.readLine().charAt(0);
+        int d = 0;
+        if (ch == 'R') {
+            d = -1;
+        } else {
+            d = 1;
+        }
+        String s = in.readLine();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            char nc = ' ';
+            for (int j = 0; j < strs.length; j++) {
+                String si = strs[j];
+                for (int k = 0; k < si.length(); k++) {
+                    if (si.charAt(k) == c) {
+                        int nk = k + d;
+                        nc = si.charAt(nk);
+                        break;
+                    }
+                }
+                if (nc != ' ') {
+                    break;
+                }
+            }
+            sb.append(nc);
+        }
+        out.append(String.format("%s\n", sb.toString()));
     }
 
     public static void main(String[] args) throws IOException {
