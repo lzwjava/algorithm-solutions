b2e25b4879aef86fd847069101ca5a5a132bdd40
lzwjava <lzwjava@gmail.com>
2021-12-15 00:53:14 +0800

    a

--- codeforces/1618D/src/Main.java
+++ codeforces/1618D/src/Main.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -26,12 +29,26 @@ public class Main {
             StringTokenizer st = new StringTokenizer(in.readLine());
             int n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
-            int[] a = new int[n];
+            List<Integer> list = new ArrayList<>();
             st = new StringTokenizer(in.readLine());
             for (int i = 0; i < n; i++) {
-                a[i] = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                list.add(v);
             }
-            
+            Collections.sort(list);
+            int s = 0;
+            for (int i = 0; i < k; i++) {
+                int m = list.size();
+                int b = list.get(m - 1);
+                int a = list.get(m - 2);
+                s += Math.floor(a * 1.0 / b);
+                list.remove(m - 1);
+                list.remove(m - 2);
+            }
+            for (int i = 0; i < list.size(); i++) {
+                s += list.get(i);
+            }
+            out.append(String.format("%d\n", s));
             t--;
         }
     }
