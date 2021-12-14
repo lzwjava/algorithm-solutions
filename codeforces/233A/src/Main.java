9ad2cf0231fec25ddad8ddde5a8f79056abe71ea
lzwjava <lzwjava@gmail.com>
2021-12-14 19:00:45 +0800

    a

--- codeforces/233A/src/Main.java
+++ codeforces/233A/src/Main.java
@@ -19,8 +19,59 @@ public class Main {
         out.close();
     }
 
+    boolean found;
+
+    boolean check(int[] nums, int n) {
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < n) {
+                if (nums[nums[i]] == i && nums[i] != i) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void permutation(int[] nums, boolean[] vis, int cur, int n) {
+        if (!check(nums, cur)) {
+            return;
+        }
+        if (found) {
+            return;
+        }
+        if (cur == n) {
+            found = true;
+            for (int i = 0; i < n; i++) {
+                if (i != 0) {
+                    out.append(' ');
+                }
+                out.append(String.valueOf(nums[i] + 1));
+            }
+            out.append('\n');
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                vis[i] = true;
+                nums[cur] = i;
+                permutation(nums, vis, cur + 1, n);
+                vis[i] = false;
+            }
+        }
+    }
+
     void solve() throws IOException {
         int n = Integer.parseInt(in.readLine());
+        if (n % 2 != 0) {
+            out.append("-1\n");
+        } else {
+            int[] nums = new int[n];
+            boolean[] vis = new boolean[n];
+            found = false;
+            permutation(nums, vis, 0, n);
+        }
     }
 
     public static void main(String[] args) throws IOException {
