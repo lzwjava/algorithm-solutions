917a38bc9eb1a84dcae3807b8d1f64e1724262aa
lzwjava <lzwjava@gmail.com>
2021-12-14 10:59:47 +0800

    a

--- codeforces/467B/src/Main.java
+++ codeforces/467B/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -20,6 +21,38 @@ public class Main {
     }
 
     void solve() throws IOException {
+        int n, m, k;
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        int[] x = new int[m];
+        for (int i = 0; i < m; i++) {
+            x[i] = Integer.parseInt(in.readLine());
+        }
+        int p = Integer.parseInt(in.readLine());
+        String pb = Integer.toBinaryString(p);
+        int plen = pb.length();
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            String xb = Integer.toBinaryString(x[i]);
+            int xlen = xb.length();
+            int min = Integer.min(xlen, plen);
+            int c = 0;
+            for (int j = 0; j < min; j++) {
+                int xj = xlen - 1 - j;
+                int pj = plen - 1 - j;
+                if (xb.charAt(xj) != pb.charAt(pj)) {
+                    c++;
+                }
+            }
+            int max = Integer.max(xlen, plen);
+            c += (max - min);
+            if (c <= k) {
+                ans++;
+            }
+        }
+        out.append(String.format("%d\n", ans));
     }
 
     public static void main(String[] args) throws IOException {
