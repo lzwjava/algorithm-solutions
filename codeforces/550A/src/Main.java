7b5048e77ad709f0905d432deac93aed0412c6a5
lzwjava <lzwjava@gmail.com>
2021-12-14 10:03:47 +0800

    a

--- codeforces/550A/src/Main.java
+++ codeforces/550A/src/Main.java
@@ -20,7 +20,25 @@ public class Main {
     }
 
     void solve() throws IOException {
-        
+        String s = in.readLine();
+        boolean ok = false;
+        int i1 = s.indexOf("AB");
+        int i2 = s.lastIndexOf("BA");
+        if (i1 >= 0 && i2 >= 0 && i1 + 1 != i2) {
+            ok = true;
+        }
+        if (!ok) {
+            i1 = s.lastIndexOf("AB");
+            i2 = s.indexOf("BA");
+            if (i1 >= 0 && i2 >= 0 && i2 != i1 + 1) {
+                ok = true;
+            }
+        }
+        if (ok) {
+            out.append("YES\n");
+        } else {
+            out.append("NO\n");
+        }
     }
 
     public static void main(String[] args) throws IOException {
