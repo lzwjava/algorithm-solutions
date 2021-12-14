942d0905d8861d23ce5e885515a42cb8f889c54d
lzwjava <lzwjava@gmail.com>
2021-12-14 20:37:28 +0800

    a

--- codeforces/1368A/src/Main.java
+++ codeforces/1368A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -20,7 +21,25 @@ public class Main {
     }
 
     void solve() throws IOException {
-        
+        int tt = Integer.parseInt(in.readLine());
+        while (tt > 0) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int c = 0;
+            while (a <= n && b <= n) {
+                if (a > b) {
+                    int t = a;
+                    a = b;
+                    b = t;
+                }
+                a += b;
+                c++;
+            }
+            out.append(String.format("%d\n", c));
+            tt--;
+        }
     }
 
     public static void main(String[] args) throws IOException {
