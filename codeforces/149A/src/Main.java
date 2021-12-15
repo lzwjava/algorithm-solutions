b9fc7be33dd6976ed7c3ba04c86ea4c66b04ae50
lzwjava <lzwjava@gmail.com>
2021-12-15 10:47:55 +0800

    a

--- codeforces/149A/src/Main.java
+++ codeforces/149A/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -21,6 +23,27 @@ public class Main {
 
     void solve() throws IOException {
         int k = Integer.parseInt(in.readLine());
+        int n = 12;
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a);
+        int s = 0;
+        int p = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s >= k) {
+                break;
+            }
+            s += a[i];
+            p++;
+        }
+        if (s >= k) {
+            out.append(String.format("%d\n", p));
+        } else {
+            out.append("-1\n");
+        }
     }
 
     public static void main(String[] args) throws IOException {
