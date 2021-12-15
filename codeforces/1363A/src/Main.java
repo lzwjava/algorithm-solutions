51c1eae764837c6a7932faaa460180134629db65
lzwjava <lzwjava@gmail.com>
2021-12-15 21:57:14 +0800

    a

--- codeforces/1363A/src/Main.java
+++ codeforces/1363A/src/Main.java
@@ -28,8 +28,31 @@ public class Main {
             int x = Integer.parseInt(st.nextToken());
             int[] a = new int[n];
             st = new StringTokenizer(in.readLine());
+            int c0 = 0, c1 = 0;
             for (int i = 0; i < n; i++) {
                 a[i] = Integer.parseInt(st.nextToken());
+                if (a[i] % 2 == 0) {
+                    c0++;
+                } else {
+                    c1++;
+                }
+            }
+            boolean ok;
+            if (x == n) {
+                ok = c1 % 2 == 1;
+            } else {
+                int d = 0;
+                if (c1 % 2 == 1) {
+                    d = c1;
+                } else {
+                    d = c1 - 1;
+                }
+                ok = d + c0 >= x;
+            }
+            if (ok) {
+                out.append("Yes\n");
+            } else {
+                out.append("No\n");
             }
             t--;
         }
