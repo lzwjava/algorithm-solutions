43695acad8d947c9a05813d30dd7d8f02c444a1b
lzwjava <lzwjava@gmail.com>
2021-12-15 22:47:58 +0800

    a

--- codeforces/82A/src/Main.java
+++ codeforces/82A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Main {
 
@@ -21,7 +22,13 @@ public class Main {
 
     void solve() throws IOException {
         String[] names = new String[]{"Sheldon", "Leonard", "Penny", "Rajesh", "Howard"};
-        
+        int n = Integer.parseInt(in.readLine());
+        ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(1000);
+        for (int i = 0; i < 5; i++) {
+            queue.add(i);
+        }
+        for (int i = 0; i < n; i++) {
+        }
     }
 
     public static void main(String[] args) throws IOException {
