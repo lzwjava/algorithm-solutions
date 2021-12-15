f4c924d50453f84a9c69165f51177c65b8d20145
lzwjava <lzwjava@gmail.com>
2021-12-15 15:49:39 +0800

    a

---
+++ codeforces/742A/src/Main.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    void close() throws IOException {
+        in.close();
+        out.flush();
+        out.close();
+    }
+
+    void solve() throws IOException {
+        int[] nums = new int[]{8, 4, 2, 6};
+        int n = Integer.parseInt(in.readLine());
+        int ans = nums[(n - 1) % 4];
+        out.append(String.format("%d\n", ans));
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
+    }
+
+}
