10f5e3fb0b34455f52507de17236a2fbe13c1a2f
lzwjava <lzwjava@gmail.com>
2021-12-15 11:44:09 +0800

    a

--- codeforces/478B/src/Main.java
+++ codeforces/478B/src/Main.java
@@ -20,10 +20,33 @@ public class Main {
         out.close();
     }
 
+    long max(int n, int m) {
+        int d = m - 1;
+        int a = n - d;
+        return pair(a);
+    }
+
+    long pair(long a) {
+        return a * (a - 1) / 2;
+    }
+
+    long min(int n, int m) {
+        int d = n / m;
+        if (n % m == 0) {
+            return m * pair(d);
+        } else {
+            int a = n % m;
+            return (m - 1) * pair(d) + pair(d + a);
+        }
+    }
+
     void solve() throws IOException {
         StringTokenizer st = new StringTokenizer(in.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        long min = min(n, m);
+        long max = max(n, m);
+        out.append(String.format("%d %d\n", min, max));
     }
 
     public static void main(String[] args) throws IOException {
