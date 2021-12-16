e75e0812f20a46b5db87d768089ebde28cca3ba0
lzwjava <lzwjava@gmail.com>
2021-12-16 23:24:52 +0800

    a

--- codeforces/1617C/src/Main.java
+++ codeforces/1617C/src/Main.java
@@ -20,18 +20,17 @@ public class Main {
         out.close();
     }
 
-    List<Integer> mods(int x, int n) {
+    List<Integer> mods(int x) {
         Set<Integer> set = new HashSet<>();
         for (int i = 1; i <= x + 1; i++) {
             int m = x % i;
-            if (m >= 1 && m <= n) {
-                set.add(m);
-                if (set.size() == n) {
-                    break;
-                }
-            }
+            set.add(m);
         }
         return new ArrayList<>(set);
+    }
+
+    boolean haveMod(int x, int y) {
+        return y * 2 < x;
     }
 
     void solve() throws IOException {
@@ -40,24 +39,27 @@ public class Main {
             int n = Integer.parseInt(in.readLine());
             List<Integer> list = new ArrayList<>();
             StringTokenizer st = new StringTokenizer(in.readLine());
+            boolean[] vis = new boolean[n + 1];
             for (int i = 0; i < n; i++) {
-                list.add(Integer.parseInt(st.nextToken()));
-            }
-            List<Integer> nos = new ArrayList<>();
-            for (int i = 1; i <= n; i++) {
-                int idx = list.indexOf(i);
-                if (idx >= 0) {
-                    list.remove(idx);
+                int v = Integer.parseInt(st.nextToken());
+                if (v >= 1 && v <= n && !vis[v]) {
+                    vis[v] = true;
                 } else {
-                    nos.add(i);
+                    list.add(v);
                 }
             }
             Collections.sort(list);
+            List<Integer> nos = new ArrayList<>();
+            for (int i = 1; i <= n; i++) {
+                if (!vis[i]) {
+                    nos.add(i);
+                }
+            }
             boolean ok = true;
             int c = 0;
             for (int i = 0; i < list.size(); i++) {
                 int v = list.get(i);
-                if (mods(v, n).contains(nos.get(i))) {
+                if (haveMod(v, nos.get(i))) {
                     c++;
                 } else {
                     ok = false;
@@ -75,6 +77,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main m = new Main();
+//        m.test();
         m.solve();
         m.close();
     }
