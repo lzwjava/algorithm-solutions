6b2659d538328f1cba916644b9abeb9826d1577d
lzwjava <lzwjava@gmail.com>
2021-12-16 19:38:26 +0800

    a

--- codeforces/1400A/src/Main.java
+++ codeforces/1400A/src/Main.java
@@ -19,7 +19,56 @@ public class Main {
         out.close();
     }
 
+    boolean similar(String a, String b) {
+        int len = a.length();
+        for (int i = 0; i < len; i++) {
+            if (a.charAt(i) == b.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean found;
+
+    void permutation(char[] chs, int cur, int n) {
+        if (found) {
+            return;
+        }
+        if (cur == n) {
+            String ns = new String(chs);
+            boolean ok = true;
+            for (int i = 0; i < n; i++) {
+                String sub = s.substring(i, i + n);
+                if (!similar(sub, ns)) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                found = true;
+                out.append(String.format("%s\n", ns));
+            }
+            return;
+        }
+        for (int i = 0; i < 2; i++) {
+            chs[cur] = (char) ('0' + i);
+            permutation(chs, cur + 1, n);
+        }
+    }
+
+    String s;
+
     void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            int n = Integer.parseInt(in.readLine());
+            s = in.readLine();
+            char[] chs = new char[n];
+            found = false;
+            permutation(chs, 0, n);
+            t--;
+        }
     }
 
     public static void main(String[] args) throws IOException {
