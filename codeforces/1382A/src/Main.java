983a6a4c9126af99d3262722d4115774ed7b784f
lzwjava <lzwjava@gmail.com>
2021-12-16 20:39:54 +0800

    a

---
+++ codeforces/1382A/src/Main.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    void close() throws IOException {
+        in.close();
+        out.flush();
+        out.close();
+    }
+
+    void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int[] a = new int[n];
+            int[] b = new int[m];
+            st = new StringTokenizer(in.readLine());
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(st.nextToken());
+            }
+            st = new StringTokenizer(in.readLine());
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(a);
+            boolean ok = false;
+            int v = 0;
+            for (int i = 0; i < m; i++) {
+                int p = Arrays.binarySearch(a, b[i]);
+                if (p >= 0) {
+                    ok = true;
+                    v = b[i];
+                    break;
+                }
+            }
+            if (ok) {
+                out.append("YES\n");
+                out.append(String.format("%d %d\n", 1, v));
+            } else {
+                out.append("NO\n");
+            }
+            t--;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
+    }
+
+}
