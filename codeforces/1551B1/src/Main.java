1f76ce73ba77d9b29bb38d2fef42a5594faeae0d
lzwjava <lzwjava@gmail.com>
2021-12-17 17:52:10 +0800

    a

--- codeforces/1551B1/src/Main.java
+++ codeforces/1551B1/src/Main.java
@@ -20,6 +20,11 @@ public class Main {
     }
 
     void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            String s = in.readLine();
+            t--;
+        }
     }
 
     public static void main(String[] args) throws IOException {
