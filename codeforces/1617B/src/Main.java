6546f688714d492529d9a30ca9a08e83d8dc4e7e
lzwjava <lzwjava@gmail.com>
2021-12-17 11:08:01 +0800

    a

--- codeforces/1617B/src/Main.java
+++ codeforces/1617B/src/Main.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
@@ -14,64 +11,6 @@ public class Main {
     Main() {
         in = new BufferedReader(new InputStreamReader(System.in));
         out = new PrintWriter(System.out);
-    }
-
-    List<Integer> list;
-
-    void calPrimes() {
-        int maxn = 31625;
-        boolean[] prime = new boolean[maxn];
-        Arrays.fill(prime, true);
-        prime[0] = prime[1] = false;
-        for (int i = 4; i < maxn; i += 2) {
-            prime[i] = false;
-        }
-        list = new ArrayList<>();
-        list.add(2);
-        for (int i = 3; i < maxn; i += 2) {
-            if (prime[i]) {
-                list.add(i);
-                if (i * i > 0) {
-                    for (int j = i * i; j < maxn; j += i) {
-                        prime[j] = false;
-                    }
-                }
-            }
-        }
-    }
-
-    class Factor {
-        int p, c;
-
-        Factor(int p, int c) {
-            this.p = p;
-            this.c = c;
-        }
-    }
-
-    List<Factor> calFactors(int x) {
-        List<Factor> fs = new ArrayList<>();
-        int sx = (int) Math.sqrt(x);
-        for (int p : list) {
-            if (p > sx) {
-                break;
-            }
-            if (x % p == 0) {
-                int c = 0;
-                while (x % p == 0) {
-                    x /= p;
-                    c++;
-                }
-                fs.add(new Factor(p, c));
-            }
-            if (x == 1) {
-                break;
-            }
-        }
-        if (x != 1) {
-            fs.add(new Factor(x, 1));
-        }
-        return fs;
     }
 
     void close() throws IOException {
@@ -93,7 +32,6 @@ public class Main {
     }
 
     void solve() throws IOException {
-        calPrimes();
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             int n = Integer.parseInt(in.readLine());
