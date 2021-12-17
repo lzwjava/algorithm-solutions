2999227c1dbda5912ad16041cf6e6638d2f3a6b9
lzwjava <lzwjava@gmail.com>
2021-12-17 16:33:10 +0800

    a

--- codeforces/1541A/src/Main.java
+++ codeforces/1541A/src/Main.java
@@ -19,11 +19,40 @@ public class Main {
         out.close();
     }
 
+    void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
     void solve() throws IOException {
-        int t = Integer.parseInt(in.readLine());
-        while (t > 0) {
+        int tt = Integer.parseInt(in.readLine());
+        while (tt > 0) {
             int n = Integer.parseInt(in.readLine());
-            t--;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = i;
+            }
+            if (n % 2 == 0) {
+                for (int i = 0; i < n; i += 2) {
+                    swap(a, i, i + 1);
+                }
+            } else {
+                a[0] = 2;
+                a[1] = 0;
+                a[2] = 1;
+                for (int i = 3; i < n; i += 2) {
+                    swap(a, i, i + 1);
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                if (i != 0) {
+                    out.append(' ');
+                }
+                out.append(String.format("%d", a[i] + 1));
+            }
+            out.append('\n');
+            tt--;
         }
     }
 
