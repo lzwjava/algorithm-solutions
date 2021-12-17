9936db99468c08d178e9652b6167c88e02985348
lzwjava <lzwjava@gmail.com>
2021-12-17 17:46:20 +0800

    a

--- codeforces/1454B/src/Main.java
+++ codeforces/1454B/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.*;
 
 public class Main {
 
@@ -19,7 +20,48 @@ public class Main {
         out.close();
     }
 
+    Map<Integer, Integer> map;
+    Map<Integer, Integer> ps;
+
+    void count(int v, int pos) {
+        Integer c = map.get(v);
+        if (c == null) {
+            c = 0;
+        }
+        c++;
+        map.put(v, c);
+        ps.put(v, pos);
+    }
+
     void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            int n = Integer.parseInt(in.readLine());
+            map = new HashMap<>();
+            ps = new HashMap<>();
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            for (int i = 0; i < n; i++) {
+                int v = Integer.parseInt(st.nextToken());
+                count(v, i);
+            }
+            List<Integer> list = new ArrayList<>();
+            for (int v : map.keySet()) {
+                int c = map.get(v);
+                if (c == 1) {
+                    list.add(v);
+                }
+            }
+            int ans;
+            if (list.size() == 0) {
+                ans = -1;
+            } else {
+                Collections.sort(list);
+                Integer v = list.get(0);
+                ans = ps.get(v) + 1;
+            }
+            out.append(String.format("%d\n", ans));
+            t--;
+        }
     }
 
     public static void main(String[] args) throws IOException {
