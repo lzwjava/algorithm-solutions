a87302112d1aca85e7330f214d24f16eb85d1b30
lzwjava <lzwjava@gmail.com>
2021-12-17 12:21:59 +0800

    a

--- codeforces/1398A/src/Main.java
+++ codeforces/1398A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,14 +21,42 @@ public class Main {
         out.close();
     }
 
+    class Item implements Comparable<Item> {
+        int v;
+        int i;
+
+        Item(int v, int i) {
+            this.v = v;
+            this.i = i;
+        }
+
+        @Override
+        public int compareTo(Item o) {
+            return Integer.compare(v, o.v);
+        }
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             int n = Integer.parseInt(in.readLine());
-            int[] a = new int[n];
+//            int[] a = new int[n];
+            Item[] is = new Item[n];
             StringTokenizer st = new StringTokenizer(in.readLine());
             for (int i = 0; i < n; i++) {
-                a[i] = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                is[i] = new Item(v, i);
+            }
+            Arrays.sort(is);
+            if (is[0].v + is[1].v <= is[n - 1].v) {
+                int[] ids = new int[3];
+                ids[0] = is[0].i;
+                ids[1] = is[1].i;
+                ids[2] = is[n - 1].i;
+                Arrays.sort(ids);
+                out.append(String.format("%d %d %d\n", ids[0] + 1, ids[1] + 1, ids[2] + 1));
+            } else {
+                out.append("-1\n");
             }
             t--;
         }
