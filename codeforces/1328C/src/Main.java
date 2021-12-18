b804e62d87ddaf6db61c9a46fb70d94af6fd97d2
lzwjava <lzwjava@gmail.com>
2021-12-18 16:14:12 +0800

    a

--- codeforces/1328C/src/Main.java
+++ codeforces/1328C/src/Main.java
@@ -24,7 +24,32 @@ public class Main {
         while (t > 0) {
             int n = Integer.parseInt(in.readLine());
             String x = in.readLine();
-            
+            StringBuilder sb = new StringBuilder();
+            int i;
+            boolean one = false;
+            for (i = 0; i < x.length(); i++) {
+                if (one) {
+                    sb.append('0');
+                    continue;
+                }
+                char c = x.charAt(i);
+                if (c == '2') {
+                    sb.append('1');
+                } else if (c == '0') {
+                    sb.append('0');
+                } else if (c == '1') {
+                    sb.append('1');
+                    one = true;
+                }
+            }
+            String a = sb.toString();
+            StringBuilder sb2 = new StringBuilder();
+            for (int j = 0; j < x.length(); j++) {
+                char c = (char) (x.charAt(j) - a.charAt(j) + '0');
+                sb2.append(c);
+            }
+            String b = sb2.toString();
+            out.append(String.format("%s\n%s\n", a, b));
             t--;
         }
     }
