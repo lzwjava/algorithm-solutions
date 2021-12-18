93d7d1bd40f3d96718b985b2a967b77d92ce4487
lzwjava <lzwjava@gmail.com>
2021-12-18 18:52:51 +0800

    a

--- codeforces/1567A/src/Main.java
+++ codeforces/1567A/src/Main.java
@@ -20,6 +20,25 @@ public class Main {
     }
 
     void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            int n = Integer.parseInt(in.readLine());
+            String s = in.readLine();
+            StringBuilder sb = new StringBuilder();
+            for (char c : s.toCharArray()) {
+                char nc = ' ';
+                if (c == 'U') {
+                    nc = 'D';
+                } else if (c == 'D') {
+                    nc = 'U';
+                } else if (c == 'L' || c == 'R') {
+                    nc = c;
+                }
+                sb.append(nc);
+            }
+            out.append(String.format("%s\n", sb));
+            t--;
+        }
     }
 
     public static void main(String[] args) throws IOException {
