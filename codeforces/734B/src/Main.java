0d1ab883907e9c5a8ac044d9ded6543d2a5496e3
lzwjava <lzwjava@gmail.com>
2021-12-18 21:46:59 +0800

    a

--- codeforces/734B/src/Main.java
+++ codeforces/734B/src/Main.java
@@ -21,24 +21,11 @@ public class Main {
     }
 
     void solve() throws IOException {
-        int q = Integer.parseInt(in.readLine());
-        while (q > 0) {
-            int n = Integer.parseInt(in.readLine());
-            StringTokenizer st = new StringTokenizer(in.readLine());
-            int s = 0;
-            for (int i = 0; i < n; i++) {
-                int v = Integer.parseInt(st.nextToken());
-                s += v;
-            }
-            int ans;
-            if (s % n == 0) {
-                ans = s / n;
-            } else {
-                ans = (int) Math.ceil(s * 1.0 / n);
-            }
-            out.append(String.format("%d\n", ans));
-            q--;
-        }
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int k2 = Integer.parseInt(st.nextToken());
+        int k3 = Integer.parseInt(st.nextToken());
+        int k5 = Integer.parseInt(st.nextToken());
+        int k6 = Integer.parseInt(st.nextToken());
     }
 
     public static void main(String[] args) throws IOException {
