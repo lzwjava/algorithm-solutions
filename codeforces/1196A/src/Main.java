6190c93af6f25ce2d696ed578e73c280b0b0bc8d
lzwjava <lzwjava@gmail.com>
2021-12-18 14:31:04 +0800

    a

--- codeforces/1196A/src/Main.java
+++ codeforces/1196A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -20,7 +21,15 @@ public class Main {
     }
 
     void solve() throws IOException {
-        
+        int q = Integer.parseInt(in.readLine());
+        while (q > 0) {
+            long a, b, c;
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            a = Long.parseLong(st.nextToken());
+            b = Long.parseLong(st.nextToken());
+            c = Long.parseLong(st.nextToken());
+            q--;
+        }
     }
 
     public static void main(String[] args) throws IOException {
