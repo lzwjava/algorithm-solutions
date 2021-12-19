8a03632c9a1db0637cc712c5698d4d2d6bf142e9
lzwjava <lzwjava@gmail.com>
2021-12-19 23:01:50 +0800

    a

--- codeforces/1519B/src/Main.java
+++ codeforces/1519B/src/Main.java
@@ -20,14 +20,43 @@ public class Main {
         out.close();
     }
 
+
+    void dp(int x, int y, int cost) {
+        if (found != -1) {
+            return;
+        }
+        if (cost < 0) {
+            return;
+        }
+        if (x == n && y == m) {
+            found = cost == 0 ? 1 : 0;
+            return;
+        }
+        if (x + 1 <= n) {
+            dp(x + 1, y, cost - y);
+        }
+        if (y + 1 <= m) {
+            dp(x, y + 1, cost - x);
+        }
+    }
+
+    int n, m, k;
+    int found;
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             StringTokenizer st = new StringTokenizer(in.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-            
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+            found = -1;
+            dp(1, 1, k);
+            if (found == 1) {
+                out.append("YES\n");
+            } else {
+                out.append("NO\n");
+            }
             t--;
         }
     }
