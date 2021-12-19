dd2e9d0d52feeb0aab0c885699d9d3e049a80189
lzwjava <lzwjava@gmail.com>
2021-12-19 15:50:52 +0800

    a

--- codeforces/686A/src/Main.java
+++ codeforces/686A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -20,6 +21,26 @@ public class Main {
     }
 
     void solve() throws IOException {
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        long x = Integer.parseInt(st.nextToken());
+        int d = 0;
+        while (n > 0) {
+            st = new StringTokenizer(in.readLine());
+            char sign = st.nextToken().charAt(0);
+            long v = Integer.parseInt(st.nextToken());
+            if (sign == '+') {
+                x += v;
+            } else if (sign == '-') {
+                if (x >= v) {
+                    x -= v;
+                } else {
+                    d++;
+                }
+            }
+            n--;
+        }
+        out.append(String.format("%d %d\n", x, d));
     }
 
     public static void main(String[] args) throws IOException {
