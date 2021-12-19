3cfc3eeb2e0b5a30b589fd76d8fb5ec0e23ea414
lzwjava <lzwjava@gmail.com>
2021-12-19 20:08:31 +0800

    a

--- codeforces/1359A/src/Main.java
+++ codeforces/1359A/src/Main.java
@@ -27,6 +27,23 @@ public class Main {
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
+            int d = n / k;
+            int[] as = new int[k];
+            int min = Integer.min(m, d);
+            as[0] = min;
+            m -= min;
+            if (m > 0) {
+                while (m > 0) {
+                    for (int i = 1; i < k; i++) {
+                        as[i]++;
+                        m--;
+                        if (m == 0) {
+                            break;
+                        }
+                    }
+                }
+            }
+            out.append(String.format("%d\n", as[0] - as[1]));
             t--;
         }
     }
