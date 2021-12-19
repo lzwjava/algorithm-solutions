d2e4c77dec24130cf5049c86299c0b5b90220a12
lzwjava <lzwjava@gmail.com>
2021-12-19 20:59:31 +0800

    a

--- codeforces/894A/src/Main.java
+++ codeforces/894A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 public class Main {
 
@@ -19,8 +20,51 @@ public class Main {
         out.close();
     }
 
+    char ch(int p) {
+        return "QAQ".charAt(p);
+    }
+
+    int dp(String s, int i, int p) {
+        int n = s.length();
+        if (p == 3) {
+            return 1;
+        }
+        if (i == n) {
+            return 0;
+        }
+        int cache = map[i][p];
+        if (cache != -1) {
+            return cache;
+        }
+        char ch = ch(p);
+        int c = 0;
+        for (int j = i; j < n; j++) {
+            if (s.charAt(j) == ch) {
+                c += dp(s, j + 1, p + 1);
+            }
+        }
+        map[i][p] = c;
+        return c;
+    }
+
+    int maxn = 101;
+    int[][] map;
+
     void solve() throws IOException {
-        
+        String s = in.readLine();
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == 'Q' || c == 'A') {
+                sb.append(c);
+            }
+        }
+        String ns = sb.toString();
+        map = new int[maxn][3];
+        for (int i = 0; i < maxn; i++) {
+            Arrays.fill(map[i], -1);
+        }
+        int cnt = dp(ns, 0, 0);
+        out.append(String.format("%d\n", cnt));
     }
 
     public static void main(String[] args) throws IOException {
