ff01c8e6e85f3d43b3cec79ed79c31c85c494ae9
lzwjava <lzwjava@gmail.com>
2021-12-19 15:31:44 +0800

    a

--- codeforces/977B/src/Main.java
+++ codeforces/977B/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -19,8 +21,35 @@ public class Main {
         out.close();
     }
 
+    int n;
+    String s;
+    Map<String, Integer> map;
+
+    int count(String sub) {
+        Integer c = map.get(sub);
+        if (c == null) {
+            c = 0;
+        }
+        c++;
+        map.put(sub, c);
+        return c;
+    }
+
     void solve() throws IOException {
-        
+        n = Integer.parseInt(in.readLine());
+        s = in.readLine();
+        map = new HashMap<>();
+        int max = 0;
+        String maxs = "";
+        for (int i = 0; i < n - 1; i++) {
+            String sub = s.substring(i, i + 2);
+            int c = count(sub);
+            if (c > max) {
+                max = c;
+                maxs = sub;
+            }
+        }
+        out.append(String.format("%s\n", maxs));
     }
 
     public static void main(String[] args) throws IOException {
