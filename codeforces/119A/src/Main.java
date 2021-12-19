9a154da944ccfa4737ae2573ba9bb45dc612c837
lzwjava <lzwjava@gmail.com>
2021-12-19 22:33:18 +0800

    a

--- codeforces/119A/src/Main.java
+++ codeforces/119A/src/Main.java
@@ -20,12 +20,46 @@ public class Main {
         out.close();
     }
 
+    int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
     void solve() throws IOException {
         StringTokenizer st = new StringTokenizer(in.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
-        
+        boolean simon = true;
+        boolean simonWin;
+        while (true) {
+            if (simon) {
+                int v = gcd(a, n);
+                if (v <= n) {
+                    n -= v;
+                } else {
+                    simonWin = false;
+                    break;
+                }
+            } else {
+                int v = gcd(b, n);
+                if (v <= n) {
+                    n -= v;
+                } else {
+                    simonWin = true;
+                    break;
+                }
+            }
+            simon = !simon;
+        }
+        if (simonWin) {
+            out.append("0\n");
+        } else {
+            out.append("1\n");
+        }
     }
 
     public static void main(String[] args) throws IOException {
