31c10562d6a8f8f33fd65cbe030abc4ae81edc50
lzwjava <lzwjava@gmail.com>
2021-12-20 22:48:05 +0800

    a

---
+++ codeforces/1619B/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    void close() throws IOException {
+        in.close();
+        out.flush();
+        out.close();
+    }
+
+    void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            int n = Integer.parseInt(in.readLine());
+            int si = (int) (Math.sqrt(n) + 1e-8);
+            int sj = (int) (Math.pow(n, 1.0 / 3) + 1e-8);
+            int sk = (int) (Math.pow(n, 1.0 / 6) + 1e-8);
+            out.append(String.format("%d\n", si + sj - sk));
+            t--;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
+    }
+
+}
