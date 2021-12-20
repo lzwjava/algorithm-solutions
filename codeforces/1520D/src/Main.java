5e63b200866d0292ac87c90a02e03b936fdc5ab6
lzwjava <lzwjava@gmail.com>
2021-12-20 19:28:58 +0800

    a

--- codeforces/1520D/src/Main.java
+++ codeforces/1520D/src/Main.java
@@ -20,6 +20,31 @@ public class Main {
         out.close();
     }
 
+    int mergeCount(int[] a, int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        if (l + 1 == r) {
+            if (a[r] - a[l] == r - l) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        int mid = (l + r) / 2;
+        int c1 = mergeCount(a, l, mid);
+        int c2 = mergeCount(a, mid + 1, r);
+        int mc = 0;
+        for (int i = l; i <= mid; i++) {
+            for (int j = mid + 1; j <= r; j++) {
+                if (a[j] - a[i] == j - i) {
+                    mc++;
+                }
+            }
+        }
+        return c1 + c2 + mc;
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
@@ -29,17 +54,8 @@ public class Main {
             for (int i = 0; i < n; i++) {
                 a[i] = Integer.parseInt(st.nextToken());
             }
-            int c = 0;
-
-            for (int i = 0; i < n; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    if (a[j] - a[i] == j - i) {
-                        out.append(String.format("%d %d\n", a[i], a[j]));
-                        c++;
-                    }
-                }
-            }
-            out.append(String.format("%d\n", c));
+            int ans = mergeCount(a, 0, n - 1);
+            out.append(String.format("%d\n", ans));
             t--;
         }
     }
