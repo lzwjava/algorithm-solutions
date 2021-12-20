a1daa875b0e71afd0e82495039e55fdef62d5217
lzwjava <lzwjava@gmail.com>
2021-12-20 20:34:12 +0800

    a

--- codeforces/1462C/src/Main.java
+++ codeforces/1462C/src/Main.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
@@ -20,7 +23,33 @@ public class Main {
     }
 
     void solve() throws IOException {
-        
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            int x = Integer.parseInt(in.readLine());
+            if (x > 45) {
+                out.append("-1\n");
+            } else {
+                int p = 9;
+                List<Integer> list = new ArrayList<>();
+                while (x > 0) {
+                    if (x >= p) {
+                        list.add(p);
+                        x -= p;
+                        p--;
+                    } else {
+                        list.add(x);
+                        x -= x;
+                    }
+                }
+                Collections.sort(list);
+                StringBuilder sb = new StringBuilder();
+                for (int v : list) {
+                    sb.append(String.format("%d", v));
+                }
+                out.append(String.format("%s\n", sb));
+            }
+            t--;
+        }
     }
 
     public static void main(String[] args) throws IOException {
