6e826b6816c0ef43f08e2a112899f496a550080e
lzwjava <lzwjava@gmail.com>
2021-12-20 14:07:55 +0800

    a

--- codeforces/1557A/src/Main.java
+++ codeforces/1557A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -29,7 +30,26 @@ public class Main {
             for (int i = 0; i < n; i++) {
                 a[i] = Integer.parseInt(st.nextToken());
             }
+            Arrays.sort(a);
+            long s = 0;
+            long[] sums = new long[n];
+            for (int i = 0; i < n; i++) {
+                s += a[i];
+                sums[i] = s;
+            }
+            long s1 = sum(sums, 0, n - 2);
+            long s2 = sum(sums, n - 1, n - 1);
+            double ans = s1 * 1.0 / (n - 1) + s2;
+            out.append(String.format("%f\n", ans));
             t--;
+        }
+    }
+
+    long sum(long[] sums, int i, int j) {
+        if (i == 0) {
+            return sums[j];
+        } else {
+            return sums[j] - sums[i - 1];
         }
     }
 
