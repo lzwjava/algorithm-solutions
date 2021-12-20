16823bb01cd7d7e0a08255ddcc56f56143d777e7
lzwjava <lzwjava@gmail.com>
2021-12-20 10:34:53 +0800

    a

--- codeforces/599A/src/Main.java
+++ codeforces/599A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -20,6 +21,15 @@ public class Main {
     }
 
     void solve() throws IOException {
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int[] d = new int[3];
+        for (int i = 0; i < 3; i++) {
+            d[i] = Integer.parseInt(st.nextToken());
+        }
+        int min = Integer.min(2 * d[0] + 2 * d[1], d[0] + d[1] + d[2]);
+        min = Integer.min(min, ((d[0] + d[2]) * 2));
+        min = Integer.min(min, ((d[1] + d[2]) * 2));
+        out.append(String.format("%d\n", min));
     }
 
     public static void main(String[] args) throws IOException {
