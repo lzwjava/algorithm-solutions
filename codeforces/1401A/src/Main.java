fc14bb1fedbeb66fc212a1916ed4a2b418634ae0
lzwjava <lzwjava@gmail.com>
2021-12-20 15:11:20 +0800

    a

--- codeforces/1401A/src/Main.java
+++ codeforces/1401A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -20,6 +21,13 @@ public class Main {
     }
 
     void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            t--;
+        }
     }
 
     public static void main(String[] args) throws IOException {
