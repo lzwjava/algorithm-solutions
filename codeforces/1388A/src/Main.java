9cb8bd59a9d5c3d3c83fd6baab6b4f6f75e904db
lzwjava <lzwjava@gmail.com>
2021-12-20 15:05:03 +0800

    a

--- codeforces/1388A/src/Main.java
+++ codeforces/1388A/src/Main.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -19,10 +22,87 @@ public class Main {
         out.close();
     }
 
+    List<Integer> primes;
+    int maxn = 200000;
+
+    void calPrimes() {
+        int smax = (int) Math.sqrt(Integer.MAX_VALUE);
+        boolean[] isp = new boolean[maxn];
+        Arrays.fill(isp, true);
+        isp[0] = isp[1] = false;
+        for (int i = 4; i < maxn; i += 2) {
+            isp[i] = false;
+        }
+        primes = new ArrayList<>();
+        primes.add(2);
+        for (int i = 3; i < maxn; i += 2) {
+            if (isp[i]) {
+                primes.add(i);
+                if (i > smax) {
+                    continue;
+                }
+                for (int j = i * i; j < maxn; j += i) {
+                    isp[j] = false;
+                }
+            }
+        }
+    }
+
+    List<Integer> nearlys;
+
+    void calNearlyPrimes() {
+        nearlys = new ArrayList<>();
+        int m = primes.size();
+        for (int i = 0; i < m; i++) {
+            for (int j = i + 1; j < m; j++) {
+                int pi = primes.get(i);
+                int pj = primes.get(j);
+                long product = (long) pi * pj;
+                if (product < maxn) {
+                    nearlys.add((int) product);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
     void solve() throws IOException {
+        calPrimes();
+        calNearlyPrimes();
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             int n = Integer.parseInt(in.readLine());
+            int m = nearlys.size();
+            boolean ok = false;
+            for (int i = 0; i < m; i++) {
+                int ni = nearlys.get(i);
+                if (ni >= n || ok) {
+                    break;
+                }
+                for (int j = i + 1; j < m; j++) {
+                    int nj = nearlys.get(j);
+                    if (nj >= n || ni + nj >= n || ok) {
+                        break;
+                    }
+                    for (int k = j + 1; k < m; k++) {
+                        int nk = nearlys.get(k);
+                        if (nk >= n || ni + nj + nk >= n) {
+                            break;
+                        }
+                        int nl = n - ni - nj - nk;
+                        if (nl > 0 && nl != ni && nl != nj && nl != nk) {
+                            ok = true;
+                            out.append("YES\n");
+                            out.append(String.format("%d %d %d %d\n", ni, nj, nk, nl));
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!ok) {
+                out.append("NO\n");
+            }
             t--;
         }
     }
