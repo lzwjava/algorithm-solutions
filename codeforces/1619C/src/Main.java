675f02363d3184ad36bcf0b6384d9b9523be645b
lzwjava <lzwjava@gmail.com>
2021-12-20 23:30:26 +0800

    a

--- codeforces/1619C/src/Main.java
+++ codeforces/1619C/src/Main.java
@@ -37,30 +37,115 @@ public class Main {
         return Long.parseLong(fs);
     }
 
+    boolean isValid(String s) {
+        if (s.length() > 2) {
+            return false;
+        }
+        if (s.length() == 2 && s.charAt(0) == '0') {
+            return false;
+        }
+        return true;
+    }
+
+    int calDigit(String s, int c) {
+        int v = Integer.parseInt(s);
+        if (v < c) {
+            return -1;
+        } else {
+            return v - c;
+        }
+    }
+
+    long toInt(int[] bs) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bs.length; i++) {
+            sb.append(String.format("%d", bs[i]));
+        }
+        return Long.parseLong(sb.toString());
+    }
+
+    void permutation(int[] idx, int i, int n) {
+        if (found) {
+            return;
+        }
+        if (i == n) {
+            int m = n + 1;
+            String[] ns = new String[m];
+            for (int j = 0; j < m; j++) {
+                int start;
+                if (j == 0) {
+                    start = 0;
+                } else {
+                    start = idx[j - 1];
+                }
+                int end;
+                if (j == n) {
+                    end = sn;
+                } else {
+                    end = idx[j];
+                }
+                String sub = ss.substring(start, end);
+                if (!isValid(sub)) {
+                    return;
+                }
+                ns[j] = sub;
+            }
+            int[] bs = new int[m];
+            for (int j = 0; j < m; j++) {
+                int d = calDigit(ns[j], as.charAt(j) - '0');
+                if (d == -1) {
+                    return;
+                }
+                bs[j] = d;
+            }
+            long b = toInt(bs);
+            if (cal(a, b) == s) {
+                found = true;
+                out.append(String.format("%d\n", b));
+            }
+            return;
+        }
+        int st;
+        if (i == 0) {
+            st = 1;
+        } else {
+            st = idx[i - 1] + 1;
+        }
+        for (int j = st; j < sn; j++) {
+            idx[i] = j;
+            permutation(idx, i + 1, n);
+        }
+    }
+
+    long a, s;
+    String as, ss;
+    int an, sn;
+    boolean found;
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
-            long a, s;
+
             StringTokenizer st = new StringTokenizer(in.readLine());
             a = Long.parseLong(st.nextToken());
             s = Long.parseLong(st.nextToken());
-            long left = 0, right = s;
-            while (right - left > 0) {
-                long mid = (left + right) / 2;
-                long v = cal(a, mid);
-                if (v < s) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
-                }
-                out.append(String.format("%d %d\n", left, right));
-            }
-            long fs = cal(a, left);
-            if (fs == s) {
-                out.append(String.format("%d\n", left));
-            } else {
+            as = String.format("%s", a);
+            ss = String.format("%s", s);
+            an = as.length();
+            sn = ss.length();
+            int[] idx = new int[an];
+            found = false;
+            permutation(idx, 0, an - 1);
+            if (!found) {
                 out.append("-1\n");
             }
+
+//            long fs = cal(a, left);
+//            if (fs == s) {
+//                out.append(String.format("%d\n", left));
+//            } else {
+//                out.append("-1\n");
+//            }
             t--;
         }
     }
