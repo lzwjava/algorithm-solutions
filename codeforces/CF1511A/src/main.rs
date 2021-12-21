8e4823f3236cc82f93acb59b4c5b2646bf7bb072
lzwjava <lzwjava@gmail.com>
2021-12-21 23:06:28 +0800

    a

--- codeforces/CF1511A/src/main.rs
+++ codeforces/CF1511A/src/main.rs
@@ -9,6 +9,20 @@ fn main() -> std::io::Result<()> {
     let mut reader = BufReader::new(f);
     let mut line = String::new();
     reader.read_line(&mut line)?;
-    println!("{}", line);
+    let t: u32 = line.trim().parse().expect("");
+
+    for i in 0..t {
+        let mut input = String::new();
+        reader.read_line(&mut input)?;
+        let n: u32 = input.trim().parse().expect("");
+        input = String::new();
+        reader.read_line(&mut input)?;
+        let numbers: Vec<i32> = input
+            .trim()
+            .split(' ')
+            .map(|s| s.parse().unwrap())
+            .collect();
+        println!("{}", n);
+    }
     Ok(())
 }
