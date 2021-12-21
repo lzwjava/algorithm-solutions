c756528c2d924420b9c8e8b3beb850ededd7542d
lzwjava <lzwjava@gmail.com>
2021-12-21 22:46:07 +0800

    a

--- codeforces/CF1511A/src/main.rs
+++ codeforces/CF1511A/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() -> std::io::Result<()> {
+    let f = File::open("1.in")?;
+    // let f = io::stdin();
+    let mut reader = BufReader::new(f);
+    let mut line = String::new();
+    reader.read_line(&mut line)?;
+    println!("{}", line);
+    Ok(())
 }
