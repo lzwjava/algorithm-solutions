370ec65b54515d0e8fb9e47051148618350a366f
lzwjava <lzwjava@gmail.com>
2021-12-21 20:28:18 +0800

    a

---
+++ codeforces/1220A/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<cassert>
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE    
+    freopen("1.in", "r", stdin);
+#endif
+    return 0;
+}
