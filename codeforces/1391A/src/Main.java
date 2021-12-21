d95400c61cce7b98687d290ca3a92c2ba5f6955b
lzwjava <lzwjava@gmail.com>
2021-12-21 18:25:09 +0800

    a

--- codeforces/1391A/src/Main.java
+++ codeforces/1391A/src/Main.java
@@ -19,14 +19,46 @@ public class Main {
         out.close();
     }
 
+    boolean found;
+
     void permutation(int[] nums, boolean[] vis, int cur, int n) {
+        if (found) {
+            return;
+        }
         if (cur == n) {
-            
+            boolean ok = true;
+            for (int i = 0; i < n && ok; i++) {
+                for (int j = i; j < n; j++) {
+                    int v = -1;
+                    for (int k = i; k <= j; k++) {
+                        if (v == -1) {
+                            v = nums[k] + 1;
+                        } else {
+                            v = v | (nums[k] + 1);
+                        }
+                    }
+                    if (v < j - i + 1) {
+                        ok = false;
+                        break;
+                    }
+                }
+            }
+            if (ok) {
+                found = true;
+                for (int i = 0; i < n; i++) {
+                    if (i != 0) {
+                        out.append(' ');
+                    }
+                    out.append(String.format("%d", nums[i] + 1));
+                }
+                out.append('\n');
+            }
             return;
         }
         for (int i = 0; i < n; i++) {
             if (!vis[i]) {
                 vis[i] = true;
+                nums[cur] = i;
                 permutation(nums, vis, cur + 1, n);
                 vis[i] = false;
             }
@@ -38,6 +70,9 @@ public class Main {
         while (t > 0) {
             int n = Integer.parseInt(in.readLine());
             int[] nums = new int[n];
+            boolean[] vis = new boolean[n];
+            found = false;
+            permutation(nums, vis, 0, n);
             t--;
         }
     }
