9e13f8539df4aa2a76fd834be5be8917c38ab26e
lzwjava <lzwjava@gmail.com>
2021-12-21 18:15:48 +0800

    a

--- codeforces/1391A/src/Main.java
+++ codeforces/1391A/src/Main.java
@@ -19,11 +19,25 @@ public class Main {
         out.close();
     }
 
+    void permutation(int[] nums, boolean[] vis, int cur, int n) {
+        if (cur == n) {
+            
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                vis[i] = true;
+                permutation(nums, vis, cur + 1, n);
+                vis[i] = false;
+            }
+        }
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             int n = Integer.parseInt(in.readLine());
-            
+            int[] nums = new int[n];
             t--;
         }
     }
