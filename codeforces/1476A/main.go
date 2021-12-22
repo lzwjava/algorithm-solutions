fc46e269bcbc97cfcf647b4e18e5aa2821a0a239
lzwjava <lzwjava@gmail.com>
2021-12-22 13:01:18 +0800

    a

--- codeforces/1476A/main.go
+++ codeforces/1476A/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main() {
+func solve() {
+
 	var t int
 	fmt.Scan(&t)
 	for t > 0 {
@@ -10,5 +14,30 @@ func main() {
 		var n, k int
 		fmt.Scan(&n)
 		fmt.Scan(&k)
+		var s int
+		if n <= k {
+			s = k
+		} else {
+			if n%k == 0 {
+				s = n
+			} else {
+				s = (n/k + 1) * k
+			}
+		}
+		var ans int
+		if s%n == 0 {
+			ans = s / n
+		} else {
+			ans = s/n + 1
+		}
+		fmt.Println(ans)
 	}
+}
+
+func main() {
+	if _, ok := os.LookupEnv("ONLINE_JUDGE"); !ok {
+		os.Stdin, _ = os.OpenFile("1.in",
+			os.O_RDONLY, 0666)
+	}
+	solve()
 }
