7cb1889a9a1b9ba38c67a153d2ee91c6057e05af
lzwjava <lzwjava@gmail.com>
2021-12-22 19:51:12 +0800

    a

---
+++ codeforces/1481A/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    void close() throws IOException {
+        in.close();
+        out.flush();
+        out.close();
+    }
+
+    void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int px = Integer.parseInt(st.nextToken());
+            int py = Integer.parseInt(st.nextToken());
+            String s = in.readLine();
+            t--;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
+    }
+
+}
