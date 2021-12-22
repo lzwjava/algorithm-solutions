9ca567e66f50694ba7e789705b40f047c8164f09
lzwjava <lzwjava@gmail.com>
2021-12-22 13:50:27 +0800

    a

--- codeforces/1554A/main.c
+++ codeforces/1554A/main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 int main() {
-  int a, b;
-  scanf("%d%d", &a, &b);
-  printf("%d\n", a + b);
+#ifndef ONLINE_JUDGE
+  freopen("1.in", "r", stdin);
+#endif
+  int t;
+  scanf("%d", &t);
+  for (int i = 0; i < t; i++) {
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++) {
+      scanf("%d", &a[i]);
+    }
+    long mm = 0;
+    for (int l = 0; l < n; l++) {
+      for (int r = l + 1; r < n; r++) {
+        int min = a[l];
+        int max = a[l];
+        for (int k = l + 1; k <= r; k++) {
+          if (a[k] < min) {
+            min = a[k];
+          }
+          if (a[k] > max) {
+            max = a[k];
+          }
+        }
+        long p = (long)min * max;
+        if (p > mm) {
+          mm = p;
+        }
+      }
+    }
+    printf("%ld\n", mm);
+  }
   return 0;
 }
