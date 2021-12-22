a13b7d07ecb7ab8c5278504c5be097ec6c8d1167
lzwjava <lzwjava@gmail.com>
2021-12-22 20:09:37 +0800

    a

--- codeforces/1481A/src/Main.java
+++ codeforces/1481A/src/Main.java
@@ -23,11 +23,51 @@ public class Main {
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
+            t--;
             StringTokenizer st = new StringTokenizer(in.readLine());
             int px = Integer.parseInt(st.nextToken());
             int py = Integer.parseInt(st.nextToken());
+            if (px == 0 && py == 0) {
+                out.append("YES\n");
+                continue;
+            }
             String s = in.readLine();
-            t--;
+            int up = 0, down = 0, left = 0, right = 0;
+            boolean ok = false;
+            for (char c : s.toCharArray()) {
+                if (c == 'U') {
+                    up++;
+                } else if (c == 'D') {
+                    down++;
+                } else if (c == 'L') {
+                    left++;
+                } else if (c == 'R') {
+                    right++;
+                }
+                boolean ok1 = false;
+                if (px >= 0 && right >= px) {
+                    ok1 = true;
+                } else if (px < 0 && left >= -px) {
+                    ok1 = true;
+                }
+                if (ok1) {
+                    boolean ok2 = false;
+                    if (py >= 0 && up >= py) {
+                        ok2 = true;
+                    } else if (py < 0 && down >= -py) {
+                        ok2 = true;
+                    }
+                    if (ok2) {
+                        ok = true;
+                        break;
+                    }
+                }
+            }
+            if (ok) {
+                out.append("YES\n");
+            } else {
+                out.append("NO\n");
+            }
         }
     }
 
