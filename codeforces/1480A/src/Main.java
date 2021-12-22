1917ff837c54afb149f1e1ac7bc0abf0216d5bcd
lzwjava <lzwjava@gmail.com>
2021-12-22 20:49:06 +0800

    a

--- codeforces/1480A/src/Main.java
+++ codeforces/1480A/src/Main.java
@@ -20,6 +20,42 @@ public class Main {
     }
 
     void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            t--;
+            String s = in.readLine();
+            int n = s.length();
+            boolean alice = true;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                char c = s.charAt(i);
+                char nc;
+                if (alice) {
+                    nc = small(c);
+                } else {
+                    nc = large(c);
+                }
+                sb.append(nc);
+                alice = !alice;
+            }
+            out.append(String.format("%s\n", sb));
+        }
+    }
+
+    char small(char c) {
+        if (c != 'a') {
+            return 'a';
+        } else {
+            return 'b';
+        }
+    }
+
+    char large(char c) {
+        if (c != 'z') {
+            return 'z';
+        } else {
+            return 'y';
+        }
     }
 
     public static void main(String[] args) throws IOException {
