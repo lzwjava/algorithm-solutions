81c6d12a2a393022f205e9f66c999ab462c154f1
lzwjava <lzwjava@gmail.com>
2021-12-22 14:38:22 +0800

    a

--- codeforces/1554A/main.c
+++ codeforces/1554A/main.c
@@ -1,37 +1,58 @@
 #include <stdio.h>
-int main() {
+int main()
+{
 #ifndef ONLINE_JUDGE
-  freopen("1.in", "r", stdin);
+    freopen("1.in", "r", stdin);
 #endif
-  int t;
-  scanf("%d", &t);
-  for (int i = 0; i < t; i++) {
-    int n;
-    scanf("%d", &n);
-    int a[n];
-    for (int i = 0; i < n; i++) {
-      scanf("%d", &a[i]);
-    }
-    long mm = 0;
-    for (int l = 0; l < n; l++) {
-      for (int r = l + 1; r < n; r++) {
-        int min = a[l];
-        int max = a[l];
-        for (int k = l + 1; k <= r; k++) {
-          if (a[k] < min) {
-            min = a[k];
-          }
-          if (a[k] > max) {
-            max = a[k];
-          }
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++) {
+        int n;
+        scanf("%d", &n);
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
         }
-        long p = (long)min * max;
-        if (p > mm) {
-          mm = p;
+        long long mm = 0;
+        int mins[n][n];
+        for (int l = 1; l <= n; l++) {
+            for (int i = 0; i < n - l + 1; i++) {
+                int j = i + l - 1;
+                if (i == j) {
+                    mins[i][j] = a[i];
+                } else {
+                    if (mins[i][j - 1] < mins[j][j]) {
+                        mins[i][j] = mins[i][j - 1];
+                    } else {
+                        mins[i][j] = mins[j][j];
+                    }
+                }
+            }
         }
-      }
+        int maxs[n][n];
+        for (int l = 1; l <= n; l++) {
+            for (int i = 0; i < n - l + 1; i++) {
+                int j = i + l - 1;
+                if (i == j) {
+                    maxs[i][j] = a[i];
+                } else {
+                    if (maxs[i][j - 1] > maxs[j][j]) {
+                        maxs[i][j] = maxs[i][j - 1];
+                    } else {
+                        maxs[i][j] = maxs[j][j];
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                long long p = (long long)mins[i][j] * maxs[i][j];
+                if (p > mm) {
+                    mm = p;
+                }
+            }
+        }
+        printf("%lld\n", mm);
     }
-    printf("%ld\n", mm);
-  }
-  return 0;
+    return 0;
 }
