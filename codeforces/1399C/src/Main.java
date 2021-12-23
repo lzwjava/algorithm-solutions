16b062bb3e6083e58c4161f0234bc1fef99ab8fb
lzwjava <lzwjava@gmail.com>
2021-12-23 22:00:03 +0800

    a

--- codeforces/1399C/src/Main.java
+++ codeforces/1399C/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -30,6 +32,43 @@ public class Main {
             for (int i = 0; i < n; i++) {
                 w[i] = Integer.parseInt(st.nextToken());
             }
+            Map<Integer, Integer> map = new HashMap<>();
+            int maxc = 0;
+            int maxs = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    int s = w[i] + w[j];
+                    Integer c = map.get(s);
+                    if (c == null) {
+                        c = 0;
+                    }
+                    c++;
+                    map.put(s, c);
+                    if (c > maxc) {
+                        maxc = c;
+                        maxs = s;
+                    }
+                }
+            }
+            boolean[] vis = new boolean[n];
+            int fc = 0;
+            for (int i = 0; i < n; i++) {
+                if (vis[i]) {
+                    continue;
+                }
+                for (int j = i + 1; j < n; j++) {
+                    if (vis[j]) {
+                        continue;
+                    }
+                    int s = w[i] + w[j];
+                    if (s == maxs) {
+                        fc++;
+                        vis[i] = vis[j] = true;
+                        break;
+                    }
+                }
+            }
+            out.append(String.format("%d\n", fc));
         }
     }
 
