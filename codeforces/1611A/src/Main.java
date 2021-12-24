6d1e7b8368f306ec43208151818d6718b1a7ba1b
lzwjava <lzwjava@gmail.com>
2021-12-24 18:27:47 +0800

    a

--- codeforces/1611A/src/Main.java
+++ codeforces/1611A/src/Main.java
@@ -20,6 +20,34 @@ public class Main {
     }
 
     void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            t--;
+            int n = Integer.parseInt(in.readLine());
+            int ans;
+            if (n % 2 == 0) {
+                ans = 0;
+            } else {
+                boolean hasEven = false;
+                while (n >= 10) {
+                    int d = n % 10;
+                    n /= 10;
+                    if (d % 2 == 0) {
+                        hasEven = true;
+                    }
+                }
+                if (n % 2 == 0) {
+                    ans = 1;
+                } else {
+                    if (hasEven) {
+                        ans = 2;
+                    } else {
+                        ans = -1;
+                    }
+                }
+            }
+            out.append(String.format("%d\n", ans));
+        }
     }
 
     public static void main(String[] args) throws IOException {
