835f941db85b8521a890c6cadc98959090f332ed
lzwjava <lzwjava@gmail.com>
2021-12-24 21:16:37 +0800

    a

--- codeforces/1345B/src/Main.java
+++ codeforces/1345B/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 public class Main {
 
@@ -24,6 +25,34 @@ public class Main {
         while (t > 0) {
             t--;
             int n = Integer.parseInt(in.readLine());
+            //2,5,8,11
+            int m = 25819;
+            int[] a = new int[m];
+            a[0] = 2;
+            for (int i = 1; i < m; i++) {
+                a[i] = a[i - 1] + 3;
+            }
+            int[] s = new int[m];
+            int p = 0;
+            for (int i = 0; i < m; i++) {
+                p += a[i];
+                s[i] = p;
+            }
+            int idx = Arrays.binarySearch(s, n);
+            if (idx < 0) {
+                idx = -(idx + 1);
+            }
+            int c = 0;
+            while (n >= 2) {
+                while (s[idx] > n) {
+                    idx--;
+                }
+                while (s[idx] <= n) {
+                    n -= s[idx];
+                    c++;
+                }
+            }
+            out.append(String.format("%d\n", c));
         }
     }
 
