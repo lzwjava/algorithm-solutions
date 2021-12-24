b8a8ea89f3f886e5d6f6dfcdc32bbc4add0cfb53
lzwjava <lzwjava@gmail.com>
2021-12-24 11:51:25 +0800

    a

--- codeforces/1569A/src/Main.java
+++ codeforces/1569A/src/Main.java
@@ -25,6 +25,29 @@ public class Main {
             t--;
             int n = Integer.parseInt(in.readLine());
             String s = in.readLine();
+            boolean found = false;
+            for (int l = 0; l < n && !found; l++) {
+                for (int r = l; r < n; r++) {
+                    String sub = s.substring(l, r + 1);
+                    int ca = 0, cb = 0;
+                    for (int k = 0; k < sub.length(); k++) {
+                        char c = sub.charAt(k);
+                        if (c == 'a') {
+                            ca++;
+                        } else if (c == 'b') {
+                            cb++;
+                        }
+                    }
+                    if (ca == cb) {
+                        found = true;
+                        out.append(String.format("%d %d\n", l + 1, r + 1));
+                        break;
+                    }
+                }
+            }
+            if (!found) {
+                out.append("-1 -1\n");
+            }
         }
     }
 
