25157dc85adbc2dc99447ef9aca93c6e078ec1ac
lzwjava <lzwjava@gmail.com>
2021-12-24 11:19:22 +0800

    a

--- codeforces/1433C/src/Main.java
+++ codeforces/1433C/src/Main.java
@@ -20,6 +20,16 @@ public class Main {
         out.close();
     }
 
+    boolean allEqual(int[] a) {
+        int n = a.length;
+        for (int i = 1; i < n; i++) {
+            if (a[i] != a[0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
@@ -29,6 +39,21 @@ public class Main {
             StringTokenizer st = new StringTokenizer(in.readLine());
             for (int i = 0; i < n; i++) {
                 a[i] = Integer.parseInt(st.nextToken());
+            }
+            boolean eq = allEqual(a);
+            if (eq) {
+                out.append("-1\n");
+            } else {
+                int i;
+                for (i = 0; i < n; i++) {
+                    if (i - 1 >= 0 && a[i - 1] < a[i]) {
+                        break;
+                    }
+                    if (i + 1 < n && a[i + 1] < a[i]) {
+                        break;
+                    }
+                }
+                out.append(String.format("%d\n", i));
             }
         }
     }
