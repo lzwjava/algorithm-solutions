76df1d5948e22a0723645af8aab40376f42bc4a2
lzwjava <lzwjava@gmail.com>
2021-12-25 00:21:51 +0800

    a

--- codeforces/1615C/src/Main.java
+++ codeforces/1615C/src/Main.java
@@ -32,6 +32,31 @@ public class Main {
             int n = Integer.parseInt(in.readLine());
             String a = in.readLine();
             String b = in.readLine();
+            int ans;
+            if (a.equals(b)) {
+                ans = 0;
+            } else if (a.indexOf('1') == -1 && b.indexOf('1') != -1) {
+                ans = -1;
+            } else {
+                int diff = 0;
+                int same = 0;
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < n; i++) {
+                    if (a.charAt(i) == b.charAt(i)) {
+                        sb.append('-');
+                        same++;
+                    } else {
+                        sb.append('x');
+                        diff++;
+                    }
+                }
+                int min = Math.min(same, diff);
+//                out.append(String.format("%s\n", a));
+//                out.append(String.format("%s\n", b));
+//                out.append(sb.toString());
+                ans = min;
+            }
+            out.append(String.format("%d\n", ans));
         }
     }
 
