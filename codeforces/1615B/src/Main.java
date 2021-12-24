6c59c1f38de82b2592aef88b94d79e535bd55d3a
lzwjava <lzwjava@gmail.com>
2021-12-24 22:43:13 +0800

    a

--- codeforces/1615B/src/Main.java
+++ codeforces/1615B/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -29,7 +30,14 @@ public class Main {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             t--;
-            int n = Integer.parseInt(in.readLine());
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            for (int i = l; i <= r; i++) {
+                String s = Integer.toBinaryString(i);
+                out.append(String.format("%d\n", i));
+                out.append(String.format("%s\n", s));
+            }
         }
     }
 
