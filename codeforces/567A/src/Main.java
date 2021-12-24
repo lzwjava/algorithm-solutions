da662262efea34d7e31aa0d72233eee163e68890
lzwjava <lzwjava@gmail.com>
2021-12-24 15:21:39 +0800

    a

--- codeforces/567A/src/Main.java
+++ codeforces/567A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -20,6 +21,41 @@ public class Main {
     }
 
     void solve() throws IOException {
+        int n = Integer.parseInt(in.readLine());
+        int[] x = new int[n];
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < n; i++) {
+            x[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < n; i++) {
+            int min = Integer.MAX_VALUE;
+            if (i - 1 >= 0) {
+                int d = x[i] - x[i - 1];
+                if (d < min) {
+                    min = d;
+                }
+            }
+            if (i + 1 < n) {
+                int d = x[i + 1] - x[i];
+                if (d < min) {
+                    min = d;
+                }
+            }
+            int max = -1;
+            if (i != 0) {
+                int d = x[i] - x[0];
+                if (d > max) {
+                    max = d;
+                }
+            }
+            if (i != n - 1) {
+                int d = x[n - 1] - x[i];
+                if (d > max) {
+                    max = d;
+                }
+            }
+            out.append(String.format("%d %d\n", min, max));
+        }
     }
 
     public static void main(String[] args) throws IOException {
