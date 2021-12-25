6a29cfca4205b35b12800605fbf105eb021708a0
lzwjava <lzwjava@gmail.com>
2021-12-25 16:03:06 +0800

    a

--- codeforces/349A/src/Gen.java
+++ codeforces/349A/src/Gen.java
@@ -1,13 +1,17 @@
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Random;
 
 public class Gen {
     public static void main(String[] args) throws FileNotFoundException {
         PrintWriter out = new PrintWriter("2.in");
         int n = 100000;
         out.append(String.format("%d\n", n));
+        Random random = new Random();
+        int[] arr = new int[]{25, 50, 100};
         for (int i = 0; i < n; i++) {
-            out.append(String.format("%d ", 25));
+            int idx = random.nextInt(3);
+            out.append(String.format("%d ", arr[idx]));
         }
         out.flush();
         out.close();
