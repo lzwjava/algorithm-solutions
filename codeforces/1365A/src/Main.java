e249f9b048b1b3fa2f053580882e858ca61df0d5
lzwjava <lzwjava@gmail.com>
2021-12-25 21:58:51 +0800

    a

--- codeforces/1365A/src/Main.java
+++ codeforces/1365A/src/Main.java
@@ -28,9 +28,43 @@ public class Main {
 
     int n;
     int m;
-    int[][] g;
 
-    boolean dp() {
+    int[] dx = new int[]{-1, 1, 0, 0};
+    int[] dy = new int[]{0, 0, -1, 1};
+
+    // Ashish win
+    boolean dp(int[][] g, boolean ashish) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (g[i][j] == 1) {
+                    continue;
+                }
+                // 0
+                boolean ok = true;
+                for (int d = 0; d < dx.length; d++) {
+                    int ni = i + dx[d];
+                    int nj = j + dy[d];
+                    if (ni >= 0 && ni < n && nj >= 0 && nj < m) {
+                        if (g[ni][nj] == 1) {
+                            ok = false;
+                            break;
+                        }
+                    }
+                }
+                if (ok) {
+                    g[i][j] = 1;
+                    if (dp(g, !ashish)) {
+                        return true;
+                    }
+                    g[i][j] = 0;
+                }
+            }
+        }
+        if (ashish) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     void solve() throws IOException {
@@ -40,12 +74,18 @@ public class Main {
             StringTokenizer st = new StringTokenizer(in.readLine());
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
-            g = new int[n][m];
+            int[][] g = new int[n][m];
             for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(in.readLine());
                 for (int j = 0; j < m; j++) {
                     g[i][j] = Integer.parseInt(st.nextToken());
                 }
+            }
+            boolean win = dp(g, true);
+            if (win) {
+                out.append("Ashish\n");
+            } else {
+                out.append("Vivek\n");
             }
         }
     }
