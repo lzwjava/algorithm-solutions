c6389324c9df4f1d0dceb0df68b54b4eface50c1
lzwjava <lzwjava@gmail.com>
2021-12-26 01:10:15 +0800

    a

--- codeforces/688B/src/Main.java
+++ codeforces/688B/src/Main.java
@@ -26,11 +26,7 @@ public class Main {
     }
 
     void solve() throws IOException {
-        int t = Integer.parseInt(in.readLine());
-        while (t > 0) {
-            t--;
-            int n = Integer.parseInt(in.readLine());
-        }
+        String s = in.readLine();
     }
 
 }
