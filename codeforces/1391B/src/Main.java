b19c0bbb0e5dcf6b69902ebadb23bcb342aaee43
lzwjava <lzwjava@gmail.com>
2021-12-26 01:31:38 +0800

    a

--- codeforces/1391B/src/Main.java
+++ codeforces/1391B/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -29,7 +30,16 @@ public class Main {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             t--;
-            int n = Integer.parseInt(in.readLine());
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            char[][] grid = new char[n][m];
+            for (int i = 0; i < n; i++) {
+                String s = in.readLine();
+                for (int j = 0; j < m; j++) {
+                    grid[i][j] = s.charAt(j);
+                }
+            }
         }
     }
 
