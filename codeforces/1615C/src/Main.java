b43ddea89d65815bf1caddb71785e20fc72d4976
lzwjava <lzwjava@gmail.com>
2021-12-25 13:45:03 +0800

    a

--- codeforces/1615C/src/Main.java
+++ codeforces/1615C/src/Main.java
@@ -24,8 +24,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main m = new Main();
-//        m.solve();
-        m.test();
+        m.solve();
+//        m.test();
         m.close();
     }
 
