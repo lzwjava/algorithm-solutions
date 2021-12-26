c6c112a9a71d0cc58b8b206d140c4c80cf682a67
lzwjava <lzwjava@gmail.com>
2021-12-27 02:04:22 +0800

    a

--- codeforces/1382B/src/Main.java
+++ codeforces/1382B/src/Main.java
@@ -36,12 +36,51 @@ public class Main {
         m.close();
     }
 
+    int[] a;
+    int n;
+
+    boolean dp(int i, boolean first) {
+        if (i == n) {
+            return !first;
+        }
+        if (a[i] == 1) {
+            return dp(i + 1, !first);
+        } else {
+            boolean result = dp(i + 1, !first);
+            if (first && result) {
+                return true;
+            }
+            if (!first && !result) {
+                return false;
+            }
+
+            int d = a[i] - 1;
+            a[i] -= d;
+            result = dp(i, !first);
+            a[i] += d;
+            if (first && !result) {
+                return true;
+            }
+
+            if (!first && !result) {
+                return false;
+            }
+            return !first;
+        }
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             t--;
-            int n = Integer.parseInt(in.readLine());
-            int[] a = parseArray(in.readLine());
+            n = Integer.parseInt(in.readLine());
+            a = parseArray(in.readLine());
+            boolean ok = dp(0, true);
+            if (ok) {
+                out.append("First\n");
+            } else {
+                out.append("Second\n");
+            }
         }
     }
 
