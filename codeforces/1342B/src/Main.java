2b5ec612d8ff5f57ea5d9f4f88a9f5a73b9e0787
lzwjava <lzwjava@gmail.com>
2021-12-26 14:49:53 +0800

    a

--- codeforces/1342B/src/Main.java
+++ codeforces/1342B/src/Main.java
@@ -25,11 +25,30 @@ public class Main {
         m.close();
     }
 
+    int period(String s) {
+        int n = s.length();
+        for (int k = 1; k <= n; k++) {
+            boolean ok = true;
+            for (int i = 0; i < n - k; i++) {
+                if (s.charAt(i) != s.charAt(i + k)) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                return k;
+            }
+        }
+        return -1;
+    }
+
     void solve() throws IOException {
-        int t = Integer.parseInt(in.readLine());
-        while (t > 0) {
-            t--;
-            String s = in.readLine();
+        int tt = Integer.parseInt(in.readLine());
+        while (tt > 0) {
+            tt--;
+            String t = in.readLine();
+            int p = period(t);
+            out.append(String.format("%d\n", p));
         }
     }
 
