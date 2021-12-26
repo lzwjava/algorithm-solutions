736396c97bd3af2ba8d0b8ba0ce756080a176a81
lzwjava <lzwjava@gmail.com>
2021-12-26 22:40:35 +0800

    a

--- codeforces/1339B/src/Main.java
+++ codeforces/1339B/src/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -23,6 +23,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main m = new Main();
         m.solve();
+//        m.test();
         m.close();
     }
 
@@ -48,21 +49,45 @@ public class Main {
         return true;
     }
 
+    void cal(int[] a, int n) {
+        long s = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            s += a[i];
+            list.add(a[i]);
+        }
+        double avg = s * 1.0 / n;
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                double d1 = Math.abs(o1 - avg);
+                double d2 = Math.abs(o2 - avg);
+                return Double.compare(d1, d2);
+            }
+        });
+        for (int i = 0; i < n; i++) {
+            a[i] = list.get(i);
+        }
+    }
+
+    void test() {
+        Random random = new Random();
+        int n = 100000;
+        int[] a = new int[n];
+        int maxn = (int) 2e9;
+        for (int i = 0; i < n; i++) {
+            a[i] = (int) (random.nextInt(maxn) - 1e9);
+        }
+        cal(a, n);
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             t--;
             int n = Integer.parseInt(in.readLine());
             int[] a = parseArray(in.readLine());
-            while (!check(a, n)) {
-                for (int i = 1; i < n - 1; i++) {
-                    if (Math.abs(a[i - 1] - a[i]) > Math.abs(a[i] - a[i + 1])) {
-                        int ta = a[i - 1];
-                        a[i - 1] = a[i + 1];
-                        a[i + 1] = ta;
-                    }
-                }
-            }
+            cal(a, n);
             for (int i = 0; i < n; i++) {
                 if (i != 0) {
                     out.append(' ');
