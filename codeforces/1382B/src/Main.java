0af7e83ccf8d3147299c91d49eab4cbb087cbacb
lzwjava <lzwjava@gmail.com>
2021-12-27 02:13:01 +0800

    a

--- codeforces/1382B/src/Main.java
+++ codeforces/1382B/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -39,34 +40,58 @@ public class Main {
     int[] a;
     int n;
 
+    int[][][] map;
+    int maxn = 100001;
+
+    int toInt(boolean b) {
+        return b ? 1 : 0;
+    }
+
+    boolean toBool(int b) {
+        return b == 1;
+    }
+
+    int arrInt(int v) {
+        if (v > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
     boolean dp(int i, boolean first) {
         if (i == n) {
             return !first;
         }
+        int cache = map[i][arrInt(a[i])][toInt(first)];
+        if (cache != -1) {
+            return toBool(cache);
+        }
+        boolean ans;
         if (a[i] == 1) {
-            return dp(i + 1, !first);
+            ans = dp(i + 1, !first);
         } else {
             boolean result = dp(i + 1, !first);
             if (first && result) {
-                return true;
+                ans = true;
+            } else if (!first && !result) {
+                ans = false;
+            } else {
+                int d = a[i] - 1;
+                a[i] -= d;
+                result = dp(i, !first);
+                a[i] += d;
+                if (first && !result) {
+                    ans = true;
+                } else if (!first && !result) {
+                    ans = false;
+                } else {
+                    ans = !first;
+                }
             }
-            if (!first && !result) {
-                return false;
-            }
-
-            int d = a[i] - 1;
-            a[i] -= d;
-            result = dp(i, !first);
-            a[i] += d;
-            if (first && !result) {
-                return true;
-            }
-
-            if (!first && !result) {
-                return false;
-            }
-            return !first;
         }
+        map[i][arrInt(a[i])][toInt(first)] = toInt(ans);
+        return ans;
     }
 
     void solve() throws IOException {
@@ -75,6 +100,12 @@ public class Main {
             t--;
             n = Integer.parseInt(in.readLine());
             a = parseArray(in.readLine());
+            map = new int[maxn][3][2];
+            for (int i = 0; i < maxn; i++) {
+                for (int j = 0; j < 3; j++) {
+                    Arrays.fill(map[i][j], -1);
+                }
+            }
             boolean ok = dp(0, true);
             if (ok) {
                 out.append("First\n");
