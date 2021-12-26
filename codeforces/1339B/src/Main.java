5d1288f325f76585efd8466bccddc5030df3e771
lzwjava <lzwjava@gmail.com>
2021-12-26 22:12:19 +0800

    a

--- codeforces/1339B/src/Main.java
+++ codeforces/1339B/src/Main.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.Random;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -51,7 +50,6 @@ public class Main {
 
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
-        Random random = new Random();
         while (t > 0) {
             t--;
             int n = Integer.parseInt(in.readLine());
