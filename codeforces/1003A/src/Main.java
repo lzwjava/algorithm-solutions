7da68135254246b878986b40e909f6eae31c8703
lzwjava <lzwjava@gmail.com>
2021-12-26 20:50:10 +0800

    a

--- codeforces/1003A/src/Main.java
+++ codeforces/1003A/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -39,6 +41,18 @@ public class Main {
     void solve() throws IOException {
         int n = Integer.parseInt(in.readLine());
         int[] a = parseArray(in.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        int mc = 0;
+        for (int i = 0; i < n; i++) {
+            Integer c = map.get(a[i]);
+            if (c == null) {
+                c = 0;
+            }
+            c++;
+            map.put(a[i], c);
+            mc = Integer.max(mc, c);
+        }
+        out.append(String.format("%d\n", mc));
     }
 
 }
