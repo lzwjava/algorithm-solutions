45dec2c22b98f10a2e31c2f772cc7b53a5a0a695
lzwjava <lzwjava@gmail.com>
2021-12-26 17:03:37 +0800

    a

--- codeforces/1342B/src/Main.java
+++ codeforces/1342B/src/Main.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 
@@ -23,7 +24,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main m = new Main();
-        m.solve();
+//        m.solve();
+        m.test();
         m.close();
     }
 
@@ -111,45 +113,111 @@ public class Main {
         }
     }
 
+    class Item {
+        int c0, c1;
+
+        Item(int c0, int c1) {
+            this.c0 = c0;
+            this.c1 = c1;
+        }
+    }
+
+    Item count(String s) {
+        int c0 = 0, c1 = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '0') {
+                c0++;
+            } else if (c == '1') {
+                c1++;
+            }
+        }
+        return new Item(c0, c1);
+    }
+
+    String makeLen(String p, int k, int n) {
+        String s = p;
+        for (int i = 0; i < n - k; i++) {
+            char c = s.charAt(s.length() - k);
+            s += c;
+        }
+        return s;
+    }
+
+    String solve1(String t) {
+        int n = t.length();
+        Item it = count(t);
+        boolean found = false;
+        for (int k = 1; k <= n && !found; k++) {
+            List<String> ps = new ArrayList<>();
+            permutation("", ps, 0, k);
+            for (String p : ps) {
+                String start = makeLen(p, k, n);
+                Item is = count(start);
+                if ((it.c0 > 0 && is.c0 == 0) || (it.c1 > 0 && is.c1 == 0) || (it.c0 == 0 && is.c0 > 0) || (it.c1 == 0 && is.c1 > 0)) {
+                    continue;
+                }
+                if (subsequence(start, t)) {
+                    return start;
+                }
+                int sn = start.length();
+                int rest = 2 * n - sn;
+                for (int i = 0; i < rest; i++) {
+                    char c = start.charAt(start.length() - k);
+                    start += c;
+                    if (subsequence(start, t)) {
+                        return start;
+                    }
+                }
+            }
+        }
+        return "";
+    }
+
     void solve() throws IOException {
         int tt = Integer.parseInt(in.readLine());
         while (tt > 0) {
             tt--;
             String t = in.readLine();
-            int n = t.length();
-            mink = Integer.MAX_VALUE;
-            int c0 = 0, c1 = 0;
-            for (char c : t.toCharArray()) {
-                if (c == '0') {
-                    c0++;
-                } else if (c == '1') {
-                    c1++;
-                }
-            }
-            for (int k = 1; k <= n; k++) {
-                List<String> ps = new ArrayList<>();
-                permutation("", ps, 0, k);
-                
-            }
-
-            for (int i = 0; i < n; i++) {
-                List<String> ps = new ArrayList<>();
-                permutation("", ps, 0, i);
-                for (String p : ps) {
-                    String np = p + t;
-                    int m = np.length();
-
-                    char[] chs = new char[m];
-                    boolean[] vis = new boolean[m];
-                    cnt = 0;
-                    cal(chs, np, t, vis, 0, m);
-                }
-                if (mink <= n / 2 - 1) {
-                    break;
-                }
-            }
-            out.append(String.format("%s\n", minks));
+            String s = solve1(t);
+            out.append(String.format("%s\n", s));
         }
+    }
+
+    void test() throws IOException {
+        Random random = new Random();
+        for (; ; ) {
+            int n = random.nextInt(5);
+            StringBuilder sb = new StringBuilder(in.readLine());
+            for (int i = 0; i < n; i++) {
+                sb.append(random.nextInt(2) + '0');
+            }
+            String t = sb.toString();
+            String a1 = solve1(t);
+            String a2 = solve2(t);
+            assert period(a1) == period(a2);
+        }
+    }
+
+    String solve2(String t) {
+        int n = t.length();
+        for (int i = 0; i < n; i++) {
+            mink = Integer.MAX_VALUE;
+            List<String> ps = new ArrayList<>();
+            permutation("", ps, 0, i);
+            for (String p : ps) {
+                String np = p + t;
+                int m = np.length();
+
+                char[] chs = new char[m];
+                boolean[] vis = new boolean[m];
+                cnt = 0;
+                cal(chs, np, t, vis, 0, m);
+            }
+            if (mink <= n / 2 - 1) {
+                break;
+            }
+        }
+        return minks;
     }
 
 }
