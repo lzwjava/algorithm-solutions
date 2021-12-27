9f1018dbe6e1b2e884661c1cd136999167b902c4
lzwjava <lzwjava@gmail.com>
2021-12-27 23:31:00 +0800

    a

--- codeforces/1622C/src/Main.java
+++ codeforces/1622C/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -25,12 +26,27 @@ public class Main {
         m.close();
     }
 
+    int[] parseArray(String s) {
+        StringTokenizer st = new StringTokenizer(s);
+        int n = st.countTokens();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        return a;
+    }
+
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             t--;
-            int n = Integer.parseInt(in.readLine());
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            int[] a = parseArray(in.readLine());
         }
+
     }
 
 }
