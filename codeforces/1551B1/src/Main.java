dfcd201a2df5593bece19e11af8f6944c941040f
lzwjava <lzwjava@gmail.com>
2021-12-28 12:42:02 +0800

    a

--- codeforces/1551B1/src/Main.java
+++ codeforces/1551B1/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -19,21 +21,43 @@ public class Main {
         out.close();
     }
 
+    String options = "rbw";
+    String color = "rb";
+    String s;
+
     void permutation(char[] cs, int cur, int n) {
         if (cur == n) {
+            check(cs, n);
             return;
         }
-        String color = "rbw";
         for (int i = 0; i < 3; i++) {
-            cs[cur] = color.charAt(i);
+            cs[cur] = options.charAt(i);
             permutation(cs, cur + 1, n);
+        }
+    }
+
+    boolean check(char[] cs, int n) {
+        List<Character>[] as = new ArrayList[2];
+        for (int i = 0; i < 2; i++) {
+            as[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < n; i++) {
+            char c = cs[i];
+            int idx = color.indexOf(c);
+            if (idx >= 0) {
+                as[idx].add(s.charAt(i));
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            
         }
     }
 
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
-            String s = in.readLine();
+            s = in.readLine();
             int n = s.length();
             char[] chs = new char[n];
             permutation(chs, 0, n);
