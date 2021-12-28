81af147022905d285cfefc8f907fb586ac841c7e
lzwjava <lzwjava@gmail.com>
2021-12-28 18:06:37 +0800

    a

--- codeforces/479C/src/Main.java
+++ codeforces/479C/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,14 +21,38 @@ public class Main {
         out.close();
     }
 
+    class Exam implements Comparable<Exam> {
+        int a, b;
+
+        Exam(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public int compareTo(Exam o) {
+            if (a != o.a) {
+                return Integer.compare(a, o.a);
+            } else {
+                return Integer.compare(b, o.b);
+            }
+        }
+    }
+
     void solve() throws IOException {
         int n = Integer.parseInt(in.readLine());
-        int[] a = new int[n];
-        int[] b = new int[n];
+        Exam[] exams = new Exam[n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(in.readLine());
-            a[i] = Integer.parseInt(st.nextToken());
-            b[i] = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            exams[i] = new Exam(a, b);
+        }
+        Arrays.sort(exams);
+        int last = 0;
+        for (int i = 0; i < n; i++) {
+            Exam e = exams[i];
+            int min = Math.min(e.a, e.b);
         }
     }
 
