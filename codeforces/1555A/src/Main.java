05d9faf12169f1278382a2c84bc70995122a3391
lzwjava <lzwjava@gmail.com>
2021-12-29 02:16:27 +0800

    a

--- codeforces/1555A/src/Main.java
+++ codeforces/1555A/src/Main.java
@@ -1,22 +1,47 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    Main() {
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         new Main().solve();
     }
 
-    void solve() throws IOException {
+    void solve() {
         Scanner in = new Scanner(System.in);
-        int t = ;
+        int t = in.nextInt();
         while (t > 0) {
             t--;
-            long n = Long.parseLong(in.readLine());
-
+            long n = in.nextLong();
+            long time = 0;
+            if (n >= 20) {
+                long d = n - 20;
+                long a;
+                if (d % 10 == 0) {
+                    a = d / 10;
+                } else {
+                    a = d / 10 + 1;
+                }
+                time += 25 * a;
+                n -= a * 10;
+            }
+            if (n > 18) {
+                time += 25 * 2;
+            } else if (n > 16) {
+                time += 25 + 20;
+            } else if (n > 14) {
+                time += 20 + 20;
+            } else if (n > 12) {
+                time += 20 + 15;
+            } else if (n > 10) {
+                time += 15 + 15;
+            } else if (n > 8) {
+                time += 25;
+            } else if (n > 6) {
+                time += 20;
+            } else {
+                time += 15;
+            }
+            System.out.println(String.format("%d", time));
         }
     }
 
