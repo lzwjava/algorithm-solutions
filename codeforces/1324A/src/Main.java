aae6ff73158628a6e7e870f282c40b221a461ff0
lzwjava <lzwjava@gmail.com>
2021-12-28 16:00:18 +0800

    a

--- codeforces/1324A/src/Main.java
+++ codeforces/1324A/src/Main.java
@@ -27,8 +27,25 @@ public class Main {
             int n = Integer.parseInt(in.readLine());
             int[] a = new int[n];
             StringTokenizer st = new StringTokenizer(in.readLine());
+            int min = Integer.MAX_VALUE;
             for (int i = 0; i < n; i++) {
                 a[i] = Integer.parseInt(st.nextToken());
+                min = Integer.min(min, a[i]);
+            }
+            for (int i = 0; i < n; i++) {
+                a[i] -= min;
+            }
+            boolean ok = true;
+            for (int i = 1; i < n; i++) {
+                if (a[i] % 2 != a[0] % 2) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                out.append("YES\n");
+            } else {
+                out.append("NO\n");
             }
         }
     }
