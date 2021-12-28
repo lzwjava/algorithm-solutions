5e59aedbccfe065c5425920074927816408bd284
lzwjava <lzwjava@gmail.com>
2021-12-29 02:00:06 +0800

    a

--- codeforces/1555A/src/Main.java
+++ codeforces/1555A/src/Main.java
@@ -1,36 +1,22 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Main {
 
-    BufferedReader in;
-    PrintWriter out;
-
     Main() {
-        in = new BufferedReader(new InputStreamReader(System.in));
-        out = new PrintWriter(System.out);
-    }
-
-    void close() throws IOException {
-        in.close();
-        out.flush();
-        out.close();
     }
 
     public static void main(String[] args) throws IOException {
-        Main m = new Main();
-        m.solve();
-        m.close();
+        new Main().solve();
     }
 
     void solve() throws IOException {
-        int t = Integer.parseInt(in.readLine());
+        Scanner in = new Scanner(System.in);
+        int t = ;
         while (t > 0) {
             t--;
             long n = Long.parseLong(in.readLine());
-            
+
         }
     }
 
--- codeforces/java_sample_c/src/Main.java
+++ codeforces/java_sample_c/src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        new Main().solve();
+    }
+
+    void solve() {
+        Scanner in = new Scanner(System.in);
     }
 
 }
