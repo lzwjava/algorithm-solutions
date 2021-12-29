06fa8d2e908e9b75e3752045433bcd0bb2c6c790
lzwjava <lzwjava@gmail.com>
2021-12-30 01:13:51 +0800

    a

--- codeforces/1616B/src/Main.java
+++ codeforces/1616B/src/Main.java
@@ -18,8 +18,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main m = new Main();
-//        m.solve();
-        m.test();
+        m.solve();
+//        m.test();
         m.close();
     }
 
@@ -54,10 +54,17 @@ public class Main {
     class Seg {
         char c;
         int p;
+        int end;
 
-        Seg(char c, int p) {
+        Seg(char c, int p, int end) {
             this.c = c;
             this.p = p;
+            this.end = end;
+        }
+
+        int start() {
+            // end- start+1=p
+            return end + 1 - p;
         }
     }
 
@@ -66,51 +73,32 @@ public class Main {
         char lastC = ' ';
         int lastP = 0;
         List<Seg> list = new ArrayList<>();
-        List<Integer> ps = new ArrayList<>();
         String ns = String.format("%s.", s);
-        char minC = ' ';
         for (int i = 0; i < ns.length(); i++) {
             char c = ns.charAt(i);
             if (c != lastC) {
                 if (lastC != ' ') {
-                    ps.add(i);
-                    list.add(new Seg(lastC, lastP));
+                    list.add(new Seg(lastC, lastP, i - 1));
                 }
                 lastC = c;
                 lastP = 1;
             } else {
                 lastP++;
             }
-            if (i < n) {
-                if (minC == ' ') {
-                    minC = c;
-                } else {
-                    if (Character.compare(c, minC) < 0) {
-                        minC = c;
-                    }
-                }
-            }
         }
         int ln = list.size();
         String ans = "";
         for (int i = 0; i < ln; i++) {
             Seg seg = list.get(i);
-            if (i > 0 && seg.c != minC) {
-                continue;
-            }
             int pos;
             if (i == 0) {
                 pos = 0;
             } else {
                 Seg leftSeg = list.get(i - 1);
-                if (Integer.compare(leftSeg.c, seg.c) < 0) {
-                    pos = ps.get(i - 1);
+                if (Character.compare(leftSeg.c, seg.c) < 0) {
+                    pos = seg.start();
                 } else {
-                    if (i == ln - 1) {
-                        pos = n - 1;
-                    } else {
-                        pos = ps.get(i) - 1;
-                    }
+                    pos = seg.end;
                 }
             }
             String s1 = s.substring(0, pos + 1);
@@ -136,7 +124,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         Random random = new Random();
         for (int i = 0; i < len; i++) {
-            char c = (char) (random.nextInt(5) + 'a');
+            char c = (char) (random.nextInt(10) + 'a');
             sb.append(c);
         }
         return sb.toString();
@@ -145,7 +133,6 @@ public class Main {
     void test() {
         while (true) {
             String s = randomString(5);
-//            s = "eadcedcdca";
             String ans = cal(s);
             String ans1 = cal1(s);
             assert (ans.equals(ans1));
