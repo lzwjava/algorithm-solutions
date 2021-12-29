2456532454b056ec4aac7cfd9b3f52479ae31026
lzwjava <lzwjava@gmail.com>
2021-12-29 14:24:26 +0800

    a

--- codeforces/1585C/src/Main.java
+++ codeforces/1585C/src/Main.java
@@ -9,6 +9,16 @@ public class Main {
     void solve() {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
+        while (t > 0) {
+            t--;
+            int n, k;
+            n = in.nextInt();
+            k = in.nextInt();
+            int[] x = new int[n];
+            for (int i = 0; i < n; i++) {
+                x[i] = in.nextInt();
+            }
+        }
     }
 
 }
