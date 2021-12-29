1f258f72a6b227f18298c152dfa9d298431d4093
lzwjava <lzwjava@gmail.com>
2021-12-29 15:54:36 +0800

    a

--- codeforces/1585C/src/Main.java
+++ codeforces/1585C/src/Main.java
@@ -16,12 +16,13 @@ public class Main {
             k = in.nextInt();
             int[] x = new int[n];
             List<Integer> right = new ArrayList<>();
+            List<Integer> left = new ArrayList<>();
             for (int i = 0; i < n; i++) {
                 x[i] = in.nextInt();
                 if (x[i] > 0) {
                     right.add(x[i]);
                 } else {
-                    right.add(-x[i]);
+                    left.add(-x[i]);
                 }
             }
             Collections.sort(right, new Comparator<Integer>() {
@@ -30,36 +31,60 @@ public class Main {
                     return Integer.compare(o2, o1);
                 }
             });
-
-            // visited depot count
-            int vis = 0;
-            int rn = right.size();
-            long dist = 0;
-            int pos = 0;
-            if (rn > 0) {
-                pos = right.get(0);
-                while (vis < rn) {
-                    if (pos != right.get(vis)) {
-                        pos = right.get(vis);
-                        dist += pos;
-                    }
-                    // rn: 3
-                    if (vis + k <= rn) {
-                        int rp = pos;
-                        dist += rp;
-                        pos = 0;
-                        vis += k;
-                    } else {
-                        int nk = rn - vis;
-                        int rp = pos;
-                        pos = 0;
-                        dist += rp;
-                        vis += nk;
-                    }
+            Collections.sort(left, new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return Integer.compare(o2, o1);
                 }
+            });
+
+            int rn = right.size();
+            int ln = left.size();
+            long dist = 0;
+            if (rn > 0 && ln > 0) {
+                if (right.get(0) >= left.get(0)) {
+                    dist += calDist(right, k) + left.get(0) + calDist(left, k);
+                } else {
+                    dist += calDist(left, k) + right.get(0) + calDist(right, k);
+                }
+            } else if (rn > 0) {
+                dist = calDist(right, k);
+            } else if (ln > 0) {
+                dist = calDist(left, k);
             }
             System.out.println(dist);
         }
+    }
+
+    long calDist(List<Integer> depots, int k) {
+        // visited depot count
+        int vis = 0;
+        int rn = depots.size();
+        long dist = 0;
+        int pos = 0;
+        if (rn > 0) {
+            pos = depots.get(0);
+            while (vis < rn) {
+                if (pos != depots.get(vis)) {
+                    pos = depots.get(vis);
+                    dist += pos;
+                }
+                // rn: 3
+                if (vis + k <= rn) {
+                    int rp = pos;
+                    dist += rp;
+                    pos = 0;
+                    vis += k;
+                } else {
+                    int nk = rn - vis;
+                    int rp = pos;
+                    pos = 0;
+                    dist += rp;
+                    vis += nk;
+                }
+            }
+        }
+        return dist;
     }
 
 }
