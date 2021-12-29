f940b823cc42f6be031b21ac52466859aa2b3d13
lzwjava <lzwjava@gmail.com>
2021-12-29 16:40:06 +0800

    a

--- codeforces/1614B/src/Main.java
+++ codeforces/1614B/src/Main.java
@@ -1,9 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         new Main().solve();
+    }
+
+    class Item implements Comparable<Item> {
+        int v, i;
+
+        Item(int v, int i) {
+            this.v = v;
+            this.i = i;
+        }
+
+        @Override
+        public int compareTo(Item o) {
+            return Integer.compare(o.v, v);
+        }
     }
 
     void solve() {
@@ -12,10 +26,41 @@ public class Main {
         while (t > 0) {
             t--;
             int n = in.nextInt();
-            int[] a = new int[n];
+            List<Integer> list = new ArrayList<>();
+            List<Item> sorted = new ArrayList<>();
             for (int i = 0; i < n; i++) {
-                a[i] = in.nextInt();
+                int v = in.nextInt();
+                list.add(v);
+                sorted.add(new Item(v, i));
             }
+            Collections.sort(sorted);
+
+            int m = n + 1;
+            int[] x = new int[m];
+            x[0] = 0;
+            int p = 1;
+            int c = 0;
+            for (int i = 1; i <= n; i++) {
+                x[i] = p;
+                c++;
+                p *= -1;
+                if (c == 2) {
+                    p++;
+                    c = 0;
+                }
+            }
+            int time = 0;
+            for (int i = 1; i <= n; i++) {
+                int d = Math.abs(x[i] - x[0]);
+                d *= sorted.get(i - 1).v;
+                time += d;
+            }
+            List<Item> xs = new ArrayList<>();
+            for (int i = 1; i <= n; i++) {
+                xs.add(new Item(x[i], sorted.get(i - 1).i));
+            }
+            Collections.sort(xs, Comparator.comparingInt(o -> o.i));
+            
         }
     }
 
