6abcb5e93ccf9fa3c49963fe6f1870c454d40617
lzwjava <lzwjava@gmail.com>
2021-12-29 22:11:51 +0800

    a

--- codeforces/1610B/src/Main.java
+++ codeforces/1610B/src/Main.java
@@ -16,7 +16,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main m = new Main();
-        m.solve();
+//        m.solve();
+        m.test();
         m.close();
     }
 
@@ -37,24 +38,20 @@ public class Main {
         return true;
     }
 
-    void solve() throws IOException {
-        int t = Integer.parseInt(in.readLine());
-        while (t > 0) {
-            t--;
-            int n = Integer.parseInt(in.readLine());
-            List<Integer> list = new ArrayList<>();
+    boolean kalindrome(int n, List<Integer> list) {
+        boolean ok;
+        if (palindrome(list)) {
+            ok = true;
+        } else {
+            ok = false;
             Set<Integer> set = new HashSet<>();
-            StringTokenizer st = new StringTokenizer(in.readLine());
-            for (int i = 0; i < n; i++) {
-                int v = Integer.parseInt(st.nextToken());
-                list.add(v);
-                set.add(v);
-            }
-            boolean ok = false;
-            if (!list.get(0).equals(list.get(n - 1))) {
-                set = new HashSet<>();
-                set.add(list.get(0));
-                set.add(list.get(n - 1));
+            for (int i = 0; i < n / 2; i++) {
+                int i1 = i;
+                int i2 = n - 1 - i;
+                if (!list.get(i1).equals(list.get(i2))) {
+                    set.add(list.get(i1));
+                    set.add(list.get(i2));
+                }
             }
             for (int x : set) {
                 List<Integer> tl = new ArrayList<>(list);
@@ -64,6 +61,50 @@ public class Main {
                     break;
                 }
             }
+        }
+        return ok;
+    }
+
+    void test() {
+        Random random = new Random();
+//        while (true) {
+        List<Integer> list = new ArrayList<>();
+        int m = 200000;
+        for (int i = 0; i < m; i++) {
+            list.add(random.nextInt(10));
+        }
+        int n = list.size();
+        List<Integer> nlist = new ArrayList<>(list);
+        for (int i = 0; i < nlist.size() / 2; i++) {
+            int t = nlist.get(i);
+            nlist.set(i, nlist.get(n - 1 - i));
+            nlist.set(n - 1 - i, t);
+        }
+        list.addAll(nlist);
+        int k = random.nextInt(100);
+        int v = random.nextInt(10);
+        for (int i = 0; i < k; i++) {
+            int idx = random.nextInt(list.size());
+            list.add(idx, v);
+        }
+//        list.add(random.nextInt(list.size()), random.nextInt(m));
+        List<Integer> alist = new ArrayList<>(list);
+        assert kalindrome(alist.size(), alist);
+//        }
+    }
+
+    void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            t--;
+            int n = Integer.parseInt(in.readLine());
+            List<Integer> list = new ArrayList<>();
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            for (int i = 0; i < n; i++) {
+                int v = Integer.parseInt(st.nextToken());
+                list.add(v);
+            }
+            boolean ok = kalindrome(n, list);
             if (ok) {
                 out.append("YES\n");
             } else {
