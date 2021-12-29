9c862a071cbc2622e204845b6b47c4dfaddcdfaa
lzwjava <lzwjava@gmail.com>
2021-12-29 21:09:52 +0800

    a

--- codeforces/1610B/src/Main.java
+++ codeforces/1610B/src/Main.java
@@ -1,5 +1,5 @@
 import java.io.PrintWriter;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -22,13 +22,46 @@ public class Main {
         out.close();
     }
 
+    boolean palindrome(List<Integer> list) {
+        int m = list.size();
+        for (int i = 0; i < m; i++) {
+            int a = list.get(i);
+            int b = list.get(m + 1 - i);
+            if (a != b) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     void solve() {
         int t = in.nextInt();
         while (t > 0) {
             int n = in.nextInt();
-            int[] a = new int[n];
+            List<Integer> list = new ArrayList<>();
+            Set<Integer> set = new HashSet<>();
             for (int i = 0; i < n; i++) {
-                a[i] = in.nextInt();
+                int v = in.nextInt();
+                list.add(v);
+                set.add(v);
+            }
+            boolean ok = false;
+            for (int x : set) {
+                List<Integer> tl = new ArrayList<>();
+                for (int v : list) {
+                    if (v != x) {
+                        tl.add(v);
+                    }
+                }
+                if (palindrome(tl)) {
+                    ok = true;
+                    break;
+                }
+            }
+            if (ok) {
+                out.append("YES\n");
+            } else {
+                out.append("NO\n");
             }
         }
     }
