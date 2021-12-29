3eb4fdc3d180021077c1b169886da6834d2ec213
lzwjava <lzwjava@gmail.com>
2021-12-29 21:29:29 +0800

    a

--- codeforces/1610B/src/Main.java
+++ codeforces/1610B/src/Main.java
@@ -1,17 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.*;
 
 public class Main {
 
-    Scanner in;
+    BufferedReader in;
     PrintWriter out;
 
     Main() {
-        in = new Scanner(System.in);
+        in = new BufferedReader(new InputStreamReader(System.in));
         out = new PrintWriter(System.out);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Main m = new Main();
         m.solve();
         m.close();
@@ -34,15 +37,16 @@ public class Main {
         return true;
     }
 
-    void solve() {
-        int t = in.nextInt();
+    void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             t--;
-            int n = in.nextInt();
+            int n = Integer.parseInt(in.readLine());
             List<Integer> list = new ArrayList<>();
             Set<Integer> set = new HashSet<>();
+            StringTokenizer st = new StringTokenizer(in.readLine());
             for (int i = 0; i < n; i++) {
-                int v = in.nextInt();
+                int v = Integer.parseInt(st.nextToken());
                 list.add(v);
                 set.add(v);
             }
