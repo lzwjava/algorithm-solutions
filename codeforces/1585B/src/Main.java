709d48fb667722ef03f13ac2cb7273b815eb0fcf
lzwjava <lzwjava@gmail.com>
2021-12-29 14:08:03 +0800

    a

--- codeforces/1585B/src/Main.java
+++ codeforces/1585B/src/Main.java
@@ -9,6 +9,23 @@ public class Main {
     void solve() {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
+        while (t > 0) {
+            t--;
+            int n = in.nextInt();
+            int[] a = new int[n];
+            int mx = 0;
+            for (int i = 0; i < n; i++) {
+                a[i] = in.nextInt();
+                mx = Integer.max(mx, a[i]);
+            }
+            int i;
+            for (i = n - 1; i >= 0; i--) {
+                if (a[i] == mx) {
+                    break;
+                }
+            }
+            System.out.println(n - 1 - i);
+        }
     }
 
 }
