e9c9396339eb75fc071b4f9e7666cf8af2a05f78
lzwjava <lzwjava@gmail.com>
2021-12-31 01:35:34 +0800

    a

--- codeforces/1355A/src/Main.java
+++ codeforces/1355A/src/Main.java
@@ -16,13 +16,40 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main m = new Main();
-        m.solve();
+//        m.solve();
+        m.test();
         m.close();
     }
 
     void close() {
         out.flush();
         out.close();
+    }
+
+    long cal(long a, long k) {
+        for (int i = 0; i < k - 1; i++) {
+            String s = String.format("%d", a);
+            int min = 10;
+            int max = -1;
+            for (char c : s.toCharArray()) {
+                int digit = Character.digit(c, 10);
+                if (digit < min) {
+                    min = digit;
+                }
+                if (digit > max) {
+                    max = digit;
+                }
+            }
+            a = a + min * max;
+        }
+        return a;
+    }
+
+    void test() {
+        for (int i = 1; i <= 100; i++) {
+            long ans = cal(487, i);
+            out.append(String.format("%d\n", ans));
+        }
     }
 
     void solve() throws IOException {
@@ -32,22 +59,8 @@ public class Main {
             StringTokenizer st = new StringTokenizer(in.readLine());
             long a = Long.parseLong(st.nextToken());
             long k = Long.parseLong(st.nextToken());
-            for (int i = 0; i < k - 1; i++) {
-                String s = String.format("%d", a);
-                int min = 10;
-                int max = -1;
-                for (char c : s.toCharArray()) {
-                    int digit = Character.digit(c, 10);
-                    if (digit < min) {
-                        min = digit;
-                    }
-                    if (digit > max) {
-                        max = digit;
-                    }
-                }
-                a = a + min * max;
-            }
-            out.append(String.format("%d\n", a));
+            long ans = cal(a, k);
+            out.append(String.format("%d\n", ans));
         }
     }
 }
