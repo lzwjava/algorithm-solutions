3a5c080b1de26ca5e8681fe7ff68e67a075842db
lzwjava <lzwjava@gmail.com>
2022-01-01 10:55:31 +0800

    a

--- codeforces/330A/src/Main.java
+++ codeforces/330A/src/Main.java
@@ -29,5 +29,12 @@ public class Main {
         StringTokenizer st = new StringTokenizer(in.readLine());
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
+        char[][] grid = new char[r][c];
+        for (int i = 0; i < r; i++) {
+            String s = in.readLine();
+            for (int j = 0; j < c; j++) {
+                grid[i][j] = s.charAt(j);
+            }
+        }
     }
 }
