eefb46c8ca4d223ed8c190e0fe41753f1524ac47
lzwjava <lzwjava@gmail.com>
2022-01-01 23:57:11 +0800

    a

--- codeforces/1183A/src/Main.java
+++ codeforces/1183A/src/Main.java
@@ -24,7 +24,24 @@ public class Main {
         out.close();
     }
 
+    int sumOfDigits(int x) {
+        int s = 0;
+        while (x != 0) {
+            s += x % 10;
+            x /= 10;
+        }
+        return s;
+    }
+
     void solve() throws IOException {
         int a = Integer.parseInt(in.readLine());
+        while (true) {
+            int s = sumOfDigits(a);
+            if (s % 4 == 0) {
+                break;
+            }
+            a++;
+        }
+        out.append(String.format("%d\n", a));
     }
 }
