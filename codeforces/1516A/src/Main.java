b7d8eb299f6707c07b26c68429d2083d8adf1c05
lzwjava <lzwjava@gmail.com>
2022-01-02 19:40:55 +0800

    a

--- codeforces/1516A/src/Main.java
+++ codeforces/1516A/src/Main.java
@@ -25,6 +25,16 @@ public class Main {
         out.close();
     }
 
+    int[] parseArray(String s) {
+        StringTokenizer st = new StringTokenizer(s);
+        int n = st.countTokens();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        return a;
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
@@ -32,6 +42,7 @@ public class Main {
             StringTokenizer st = new StringTokenizer(in.readLine());
             int n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
+            int[] a = parseArray(in.readLine());
         }
     }
 }
