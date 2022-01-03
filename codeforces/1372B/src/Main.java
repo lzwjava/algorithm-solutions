a4bda6db6ea6d1b04e976a43976374149a5526d1
lzwjava <lzwjava@gmail.com>
2022-01-03 22:19:22 +0800

    a

--- codeforces/1372B/src/Main.java
+++ codeforces/1372B/src/Main.java
@@ -24,11 +24,35 @@ public class Main {
         out.close();
     }
 
+    int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+    int lcm(int a, int b) {
+        return a / gcd(a, b) / b;
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             t--;
             int n = Integer.parseInt(in.readLine());
+            int minLcm = Integer.MAX_VALUE;
+            int ma = 0, mb = 0;
+            for (int a = 1; a <= n; a++) {
+                int b = n - a;
+                int lcm = lcm(a, b);
+                if (lcm < minLcm) {
+                    minLcm = lcm;
+                    ma = a;
+                    mb = b;
+                }
+            }
+            out.append(String.format("%d %d\n", ma, mb));
         }
     }
 }
