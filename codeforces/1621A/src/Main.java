04689baebd32ce95f86342e6876f81ae6be90fa7
lzwjava <lzwjava@gmail.com>
2022-01-03 22:49:01 +0800

    a

--- codeforces/1621A/src/Main.java
+++ codeforces/1621A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 public class Main {
 
@@ -29,6 +30,32 @@ public class Main {
         while (t > 0) {
             t--;
             int n = Integer.parseInt(in.readLine());
+            int k = Integer.parseInt(in.readLine());
+            char[][] grid = new char[n][n];
+            for (int i = 0; i < n; i++) {
+                Arrays.fill(grid[i], '.');
+            }
+            int max;
+            if (n % 2 == 0) {
+                max = n / 2;
+            } else {
+                max = n / 2 + 1;
+            }
+            if (k <= max) {
+                for (int i = 0; i < max; i++) {
+                    int x = 2 * i;
+                    int y = 2 * i;
+                    grid[x][y] = 'R';
+                }
+                for (int x = 0; x < n; x++) {
+                    for (int y = 0; y < n; y++) {
+                        out.append(grid[x][y]);
+                    }
+                    out.append('\n');
+                }
+            } else {
+                out.append("-1\n");
+            }
         }
     }
 }
