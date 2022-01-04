9ab07dcd264c68970445f33043eec1946f58f1b4
lzwjava <lzwjava@gmail.com>
2022-01-04 16:07:00 +0800

    a

---
+++ codeforces/1092B/src/Main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    BufferedReader in;
+    PrintWriter out;
+
+    Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
+    }
+
+    void close() {
+        out.flush();
+        out.close();
+    }
+
+    int[] parseArray(String s) {
+        StringTokenizer st = new StringTokenizer(s);
+        int n = st.countTokens();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        return a;
+    }
+
+    void solve() throws IOException {
+        int n = Integer.parseInt(in.readLine());
+        int[] a = parseArray(in.readLine());
+    }
+}
