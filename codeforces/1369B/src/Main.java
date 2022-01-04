836bf27d2c45ab891511c56a029cf43751239feb
lzwjava <lzwjava@gmail.com>
2022-01-04 17:51:01 +0800

    a

--- codeforces/1369B/src/Main.java
+++ codeforces/1369B/src/Main.java
@@ -24,11 +24,55 @@ public class Main {
         out.close();
     }
 
+    boolean better(String a, String b) {
+        int an = a.length();
+        int bn = b.length();
+        if (an < bn) {
+            return true;
+        } else if (an > bn) {
+            return false;
+        } else {
+            for (int i = 0; i < an; i++) {
+                char ca = a.charAt(i);
+                char cb = b.charAt(i);
+                if (ca != cb) {
+                    return ca < cb;
+                }
+            }
+            return false;
+        }
+    }
+
+    String ans;
+
+    void dp(String s) {
+        if (better(s, ans)) {
+            ans = s;
+        }
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char si = s.charAt(i);
+            char sj = s.charAt(i + 1);
+            if (si == '1' && sj == '0') {
+                // remove 0
+                String ns1 = s.substring(0, i + 1) + s.substring(i + 2, n);
+                dp(ns1);
+                // remove 1
+                String ns2 = s.substring(0, i) + s.substring(i + 1, n);
+                dp(ns2);
+            }
+        }
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             t--;
             int n = Integer.parseInt(in.readLine());
+            String s = in.readLine();
+            ans = s;
+            dp(s);
+            out.append(String.format("%s\n", ans));
         }
     }
 }
