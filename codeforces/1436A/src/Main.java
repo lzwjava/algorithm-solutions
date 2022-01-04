e8e2192a8048f016eef4a8b5ab08ff53e02e860c
lzwjava <lzwjava@gmail.com>
2022-01-04 15:15:21 +0800

    a

--- codeforces/1436A/src/Main.java
+++ codeforces/1436A/src/Main.java
@@ -25,13 +25,67 @@ public class Main {
         out.close();
     }
 
+    int[] parseArray(String s) {
+        StringTokenizer st = new StringTokenizer(s);
+        int n = st.countTokens();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        return a;
+    }
+
+    void permutation(int[] nums, boolean[] vis, int cur) {
+        found = true;
+        if (found) {
+            return;
+        }
+        if (cur == n) {
+            double s = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = i; j < n; j++) {
+                    s += a[j] * 1.0 / j;
+                }
+            }
+            if (Math.abs(s - Math.round(s)) < 1e-10) {
+                int v = (int) Math.round(s);
+                if (v == m) {
+
+                }
+            }
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                vis[i] = true;
+                nums[cur] = a[i];
+                permutation(nums, vis, cur + 1);
+                vis[i] = false;
+            }
+        }
+    }
+
+    int n, m;
+    int[] a;
+    boolean found;
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             t--;
             StringTokenizer st = new StringTokenizer(in.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            a = parseArray(in.readLine());
+            int[] nums = new int[n];
+            boolean[] vis = new boolean[n];
+            found = false;
+            permutation(nums, vis, 0);
+            if (found) {
+                out.append("YES\n");
+            } else {
+                out.append("NO\n");
+            }
         }
     }
 }
