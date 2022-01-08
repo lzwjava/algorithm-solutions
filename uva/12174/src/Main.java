088dfa7992e7fb2a3a35989d64ea4d0ae0449c88
lzwjava <lzwjava@gmail.com>
2022-01-08 22:19:24 +0800

    a

--- uva/12174/src/Main.java
+++ uva/12174/src/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -36,6 +36,12 @@ public class Main {
         return a;
     }
 
+    boolean checkGroup(List<Integer> items) {
+        int size = items.size();
+        Set<Integer> set = new HashSet<>(items);
+        return set.size() == size;
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
@@ -45,9 +51,40 @@ public class Main {
             int n = Integer.parseInt(st.nextToken());
             int[] x = parseArray(in.readLine());
             int m = Integer.min(s, n);
+            int total = 0;
             for (int i = 1; i <= m; i++) {
-                
+                int group = (n - i + s - 1) / s + 1;
+                int[] groupSize = new int[group];
+                groupSize[0] = i;
+                int tn = n - i;
+                for (int j = 1; j < group; j++) {
+                    if (tn >= s) {
+                        groupSize[j] = s;
+                        tn -= s;
+                    } else {
+                        groupSize[j] = tn;
+                    }
+                }
+                List<Integer>[] groups = new ArrayList[group];
+                int p = 0;
+                for (int j = 0; j < group; j++) {
+                    groups[j] = new ArrayList<>();
+                    for (int k = 0; k < groupSize[j]; k++) {
+                        groups[j].add(x[p++]);
+                    }
+                }
+                boolean ok = true;
+                for (List<Integer> groupItem : groups) {
+                    if (!checkGroup(groupItem)) {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok) {
+                    total++;
+                }
             }
+            out.append(String.format("%d\n", total));
         }
     }
 }
