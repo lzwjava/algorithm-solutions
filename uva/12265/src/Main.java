f02432c65d322cd279ba9051d670174e9e22a462
lzwjava <lzwjava@gmail.com>
2022-01-09 21:53:20 +0800

    a

--- uva/12265/src/Main.java
+++ uva/12265/src/Main.java
@@ -33,6 +33,13 @@ public class Main {
             StringTokenizer st = new StringTokenizer(in.readLine());
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
+            char[][] grid = new char[n][m];
+            for (int i = 0; i < n; i++) {
+                String s = in.readLine();
+                for (int j = 0; j < s.length(); j++) {
+                    grid[i][j] = s.charAt(j);
+                }
+            }
         }
     }
 }
