7d45bff0f27e603ecdd320f111ce1bb2e25acb84
lzwjava <lzwjava@gmail.com>
2022-01-10 19:51:28 +0800

    a

--- uva/1607/src/Main.java
+++ uva/1607/src/Main.java
@@ -54,58 +54,20 @@ public class Main {
         }
     }
 
-    void permutation(int[] inputs, int x, int cur, int n) {
-        if (cur == n) {
-            if (x == 0 || x == n) {
-                return;
-            }
-            boolean ok = true;
-            for (int y = 0; y <= 1; y++) {
-                Map<Integer, Integer> map = new HashMap<>();
-                for (int i = 0; i < n; i++) {
-                    int rv;
-                    if (inputs[i] == -1) {
-                        rv = y;
-                    } else {
-                        rv = inputs[i];
-                    }
-                    map.put(-(i + 1), rv);
-                }
-                int fv = cal(map);
-                if (fv != y) {
-                    ok = false;
-                    break;
-                }
-            }
-            if (ok) {
-                if (x < minx) {
-                    minx = x;
-                    StringBuilder sb = new StringBuilder();
-                    for (int i = 0; i < n; i++) {
-                        if (inputs[i] == -1) {
-                            sb.append('x');
-                        } else {
-                            sb.append((char) (inputs[i] + '0'));
-                        }
-                    }
-                    mins = sb.toString();
-                }
-            }
-            return;
+    int calAll(int y) {
+        int[] inputs = new int[n];
+        for (int i = 0; i < n; i++) {
+            inputs[i] = y;
         }
-        for (int i = -1; i <= 1; i++) {
-            inputs[cur] = i;
-            int nx = x;
-            if (i == -1) {
-                nx++;
-            }
-            permutation(inputs, nx, cur + 1, n);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(-(i + 1), inputs[i]);
         }
+        return cal(map);
     }
 
     int n, m;
     int[] a;
-    int minx;
     String mins;
 
     int cal(Map<Integer, Integer> map) {
@@ -129,9 +91,8 @@ public class Main {
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
             a = parseArray(in.readLine());
-            int[] inputs = new int[n];
-            minx = Integer.MAX_VALUE;
-            permutation(inputs, 0, 0, n);
+            int all0 = calAll(0);
+            int all1 = calAll(1);
             out.append(String.format("%s\n", mins));
         }
     }
