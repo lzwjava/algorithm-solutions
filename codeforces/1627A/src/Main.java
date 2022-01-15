8f5413db8d62e39f1662f2e102fcfdad64c4d28f
lzwjava <lzwjava@gmail.com>
2022-01-15 22:57:48 +0800

    a

--- codeforces/1627A/src/Main.java
+++ codeforces/1627A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -28,7 +29,52 @@ public class Main {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             t--;
-            int n = Integer.parseInt(in.readLine());
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken()) - 1;
+            char[][] grid = new char[n][m];
+            boolean allWhite = true;
+            for (int i = 0; i < n; i++) {
+                String s = in.readLine();
+                for (int j = 0; j < m; j++) {
+                    char ch = s.charAt(j);
+                    grid[i][j] = ch;
+                    if (ch == 'B') {
+                        allWhite = false;
+                    }
+                }
+            }
+            int ans;
+            if (allWhite) {
+                ans = -1;
+            } else {
+                if (grid[r][c] == 'B') {
+                    ans = 0;
+                } else {
+                    boolean rowBlack = false;
+                    for (int i = 0; i < n; i++) {
+                        if (grid[i][c] == 'B') {
+                            rowBlack = true;
+                            break;
+                        }
+                    }
+                    boolean columnBlack = false;
+                    for (int j = 0; j < m; j++) {
+                        if (grid[r][j] == 'B') {
+                            columnBlack = true;
+                            break;
+                        }
+                    }
+                    if (rowBlack || columnBlack) {
+                        ans = 1;
+                    } else {
+                        ans = 2;
+                    }
+                }
+            }
+            out.append(String.format("%d\n", ans));
         }
     }
 }
