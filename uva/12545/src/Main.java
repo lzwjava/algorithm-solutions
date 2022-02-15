1a32f0c8824a468d310767fce41e34c743453c73
lzwjava <lzwjava@gmail.com>
2022-02-15 23:24:39 +0800

    a

--- uva/12545/src/Main.java
+++ uva/12545/src/Main.java
@@ -25,12 +25,34 @@ public class Main {
         m.close();
     }
 
+    int count(String s, char c) {
+        int n = s.length();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == c) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
     void solve() throws IOException {
         int t = Integer.parseInt(in.readLine());
         while (t > 0) {
             t--;
             String a = in.readLine();
             String b = in.readLine();
+            int a1 = count(a, '1');
+            int a0 = count(a, '0');
+            int au = count(a, '?');
+
+            int b1 = count(b, '1');
+            int b0 = count(b, '0');
+
+            int a00 = a0 + au;
+            if (a00 >= b0) {
+
+            }
         }
     }
 }
