e024c3e6a1938a19fbd1ca0a00bdcffa9db3027e
lzwjava <lzwjava@gmail.com>
2022-06-14 21:39:34 +0800

    a

--- codeforces/1624A/src/Main.java
+++ codeforces/1624A/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -29,6 +30,18 @@ public class Main {
         while (t > 0) {
             t--;
             int n = Integer.parseInt(in.readLine());
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int min = 0, max = 0;
+            for (int i = 0; i < n; i++) {
+                int x = Integer.parseInt(st.nextToken());
+                if (i == 0) {
+                    min = max = x;
+                } else {
+                    min = Integer.min(min, x);
+                    max = Integer.max(max, x);
+                }
+            }
+            System.out.println(max - min);
         }
     }
 }
