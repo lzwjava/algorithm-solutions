f92e17fe019c678e2b3ce88e87172fa63f6ec969
lzwjava <lzwjava@gmail.com>
2023-11-16 18:37:25 +0800

    add

--- leetcode/container_with_most_water.py
+++ leetcode/container_with_most_water.py
@@ -7,43 +7,19 @@ class Solution:
         return min(height[i], height[j]) * (j - i)
 
     def maxArea(self, height: List[int]) -> int:
-        return self.maxArea2(height)
-
-    def maxArea2(self, height: List[int]) -> int:
         n = len(height)
-        i = 0
-        j = n - 1
-        mi = 0
-        mj = n - 1
-        max_area = self.area(height, mi, mj)
-        while True:
-            i += 1
-            if i == mj:
-                break
-            a1 = self.area(height, i, mj)
-            if a1 > max_area:
-                mi = i
-                max_area = a1
-
-        while True:
-            j -= 1
-            if j == mi:
-                break
-            a2 = self.area(height, mi, j)
-            if a2 > max_area:
-                mj = j
-                max_area = a2
-
-        return max_area
-
-    def maxArea1(self, height: List[int]) -> int:
-        n = len(height)
+        left = 0
+        right = n - 1
         max_area = 0
-        for i in range(0, n - 1):
-            for j in range(i + 1, n):
-                min_h = min(height[i], height[j])
-                area = min_h * (j - i)
-                max_area = max(max_area, area)
+        while left < right:
+            max_area = max(max_area, self.area(height, left, right))
+            if height[left] < height[right]:
+                left += 1
+            elif height[left] > height[right]:
+                right -= 1
+            else:
+                left += 1
+                right -= 1
 
         return max_area
 
