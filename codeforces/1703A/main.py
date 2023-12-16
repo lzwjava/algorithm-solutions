9344122ecc4f90a98fea7da768e6ad2257999c28
lzwjava <lzwjava@gmail.com>
2023-12-16 11:40:28 +0800

    add

---
+++ codeforces/1703A/main.py
@@ -0,0 +1,16 @@
+import sys
+from os import path
+
+if path.exists('in.txt'):
+    sys.stdin = open('in.txt', 'r')
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    if s.upper() == 'YES':
+        print('Yes')
+    else:
+        print('No')
