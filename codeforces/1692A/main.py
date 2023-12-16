017a17f7b3154d32afbd304aad392827729a2274
lzwjava <lzwjava@gmail.com>
2023-12-16 11:54:52 +0800

    add

--- codeforces/1692A/main.py
+++ codeforces/1692A/main.py
@@ -7,3 +7,13 @@ if path.exists('in.txt'):
 input = sys.stdin.readline
 
 t = int(input())
+
+for _ in range(t):
+    ds = list(map(int, input().split()))
+    assert len(ds) == 4
+    count = 0
+    for i in range(1, 4):
+        if ds[i] > ds[0]:
+            count += 1
+
+    print(count)
