5d14262bc40bffd9c96a6203c14e1fec2d2e1063
Zhiwei Li <lzwjava@gmail.com>
2023-12-16 21:24:23 +0800

    add

--- codeforces/1902C/main.py
+++ codeforces/1902C/main.py
@@ -1,3 +1,4 @@
+import math
 import sys
 from os import path
 
@@ -8,7 +9,31 @@ input = sys.stdin.readline
 
 t = int(input())
 
+pm = int(math.sqrt(1e9))
+
+is_primes = [True] * (pm + 1)
+
+pms = []
+
+for i in range(2, pm):
+    if is_primes[i]:
+        pms.append(i)
+        for j in range(i * i, pm + 1, i):
+            is_primes[j] = False
+
 for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
-    
+
+    m = max(a)
+
+    print(m)
+
+    b = []
+    for i in range(n):
+        if a[i] != m:
+            b.append(m - a[i])
+
+    j = 0
+    while True:
+        
