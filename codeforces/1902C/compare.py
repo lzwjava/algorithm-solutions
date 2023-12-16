fa3e0936d03f015300b8a5ca3850e09316b3b833
Zhiwei Li <lzwjava@gmail.com>
2023-12-16 23:46:57 +0800

    add

--- codeforces/1902C/compare.py
+++ codeforces/1902C/compare.py
@@ -1,6 +1,8 @@
 import subprocess
 
-subprocess.run(["python", "gen.py"], check=True)
+
+def gen():
+    subprocess.run(["python", "gen.py"], check=True)
 
 
 def run(args, output_file):
@@ -18,5 +20,39 @@ def run(args, output_file):
         print(f"Output saved to {output_file}")
 
 
-run(["python", "main.py"], "out/1.txt")
-run(["./a.out"], "out/2.txt")
+def diff() -> bool:
+    file1 = "out/1.txt"
+    file2 = "out/2.txt"
+
+    command = ["diff", file1, file2]
+
+    try:
+
+        result = subprocess.run(command, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+
+        stdout = result.stdout
+        stderr = result.stderr
+
+        if stdout:
+            print("Differences found:")
+            print(stdout)
+            return True
+        else:
+            print("No differences found.")
+            return False
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        return True
+
+
+while True:
+    gen()
+
+    run(["python", "main.py"], "out/1.txt")
+    run(["./a.out"], "out/2.txt")
+
+    has_diff = diff()
+    if has_diff:
+        break
+
+print('end')
