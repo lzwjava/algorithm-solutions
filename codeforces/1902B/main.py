89928c0cc5fe7718fa19dca519db5cb05a3382be
lzwjava <lzwjava@gmail.com>
2023-12-16 17:19:21 +0800

    add

--- codeforces/1902B/main.py
+++ codeforces/1902B/main.py
@@ -1,3 +1,4 @@
+import math
 import sys
 from os import path
 
@@ -9,5 +10,21 @@ input = sys.stdin.readline
 t = int(input())
 
 for _ in range(t):
-    m = map(int, input().split())
-    print(m)
+    a = list(map(int, input().split()))
+    n = a[0]
+    P = a[1]
+    l = a[2]
+    t = a[3]
+
+    weeks = math.ceil(n / 7)
+
+    work_days = 0
+
+    if weeks > 0:
+        if (l + 2 * t) * weeks >= P:
+            day = math.ceil(P / (l + 2 * t))
+            work_days += day
+            P = 0
+        else:
+            P -= (l + 2 * t) * weeks
+            work_days += weeks
