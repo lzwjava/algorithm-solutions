416b53076f485ed94de999ea27b956f2691da521
lzwjava <lzwjava@gmail.com>
2023-12-16 19:57:14 +0800

    add

---
+++ codeforces/1902C/main.py
@@ -0,0 +1,14 @@
+import sys
+from os import path
+
+if path.exists('in.txt'):
+    sys.stdin = open('in.txt', 'r')
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
