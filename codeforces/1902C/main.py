eb00fc6c2403a0bc86bf9c4d2cf049d566d21dc7
Zhiwei Li <lzwjava@gmail.com>
2023-12-16 21:34:56 +0800

    add

--- codeforces/1902C/main.py
+++ codeforces/1902C/main.py
@@ -21,6 +21,8 @@ for i in range(2, pm):
         for j in range(i * i, pm + 1, i):
             is_primes[j] = False
 
+pmn = len(pms)
+
 for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
@@ -34,6 +36,32 @@ for _ in range(t):
         if a[i] != m:
             b.append(m - a[i])
 
-    j = 0
-    while True:
-        
+    bn = len(b)
+
+    x = 1
+
+    for j in range(pmn):
+        p = pms[j]
+        while True:
+            ok = True
+            for i in range(bn):
+                if b[i] % p != 0:
+                    ok = False
+                    break
+
+            if ok:
+                x *= p
+                for i in range(bn):
+                    b[i] /= p
+            else:
+                break
+
+        greater = True
+        for i in range(bn):
+            if p <= b[i]:
+                greater = False
+                break
+        if greater:
+            break
+
+    
