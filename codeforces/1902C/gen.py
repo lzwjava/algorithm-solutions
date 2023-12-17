ad02e4c0f6ebd8705ff7448682cbcc8bdf876702
Zhiwei Li <lzwjava@gmail.com>
2023-12-17 09:44:42 +0800

    add

--- codeforces/1902C/gen.py
+++ codeforces/1902C/gen.py
@@ -5,9 +5,20 @@ sys.stdout = open('in.txt', 'w')
 
 print(1)
 
-n = random.randint(5, 10)
+n = random.randint(3, 5)
 
 print(n)
 
-for _ in range(n):
-    print(random.randint(-10, 10), end=' ')
+start_range = -10
+end_range = 10
+batch_size = n
+
+if batch_size <= end_range - start_range + 1:
+    random_numbers = random.sample(range(start_range, end_range + 1), batch_size)
+    for i in range(batch_size):
+        if i != 0:
+            print(' ', end='')
+        print(random_numbers[i], end='')
+    print('')
+else:
+    print("Batch size is larger than the range of distinct numbers.")
