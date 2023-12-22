71578a4149c71d151350e7a0052720484ed7d6e0
lzwjava <lzwjava@gmail.com>
2023-12-22 12:02:58 +0800

    add

--- codeforces/1902E/main.py
+++ codeforces/1902E/main.py
@@ -2,6 +2,17 @@ import sys
 from os import path
 
 
+def collapse(a, b):
+    if a == '':
+        return b
+    elif b == '':
+        return a
+    elif a[-1] == b[0]:
+        return collapse(a[0:], b[1:])
+    else:
+        return a + b
+
+
 def main():
     if path.exists('in.txt'):
         sys.stdin = open('in.txt', 'r')
@@ -15,5 +26,12 @@ def main():
         str = input()
         strs.append(str)
 
-    
+    sum = 0
+    for i in range(n):
+        for j in range(n):
+            res = collapse(strs[i], strs[j])
+            sum += len(res)
+    print(sum)
+
+
 main()
