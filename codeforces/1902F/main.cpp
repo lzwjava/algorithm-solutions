1e4ffcd4d8fce60b69a13e1a106b3d8b51c2e1d7
lzwjava <lzwjava@gmail.com>
2023-12-27 22:04:28 +0800

    add

--- codeforces/1902F/main.cpp
+++ codeforces/1902F/main.cpp
@@ -11,104 +11,101 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 
-const int LIMIT = 2e5 + 7, LOG_SIZE = 20;
+const int MAX_N = 2e5 + 7, MAX_LG = 20;
 
-vector<int> graph[LIMIT];
-pair<int, int> base[LIMIT][LOG_SIZE];
-int nextNode[LIMIT][LOG_SIZE], preOrder[LIMIT], postOrder[LIMIT], preOrderCount, postOrderCount;
-int distanceFromRoot[LIMIT], nodeValue[LIMIT], linearBasis[LOG_SIZE];
+vector<int> adjList[MAX_N];
+pair<int, int> base[MAX_N][MAX_LG];
+int nextNode[MAX_N][MAX_LG], pre[MAX_N], post[MAX_N], preCnt, postCnt;
+int dist[MAX_N], val[MAX_N], basis[MAX_LG];
 
-void addLinearBasis(int x) {
-    for (int i = LOG_SIZE - 1; i >= 0; --i) {
+void addBasis(int x) {
+    for (int i = MAX_LG - 1; i >= 0; --i) {
         if (x & (1 << i)) {
-            if (!linearBasis[i]) {
-                linearBasis[i] = x;
+            if (!basis[i]) {
+                basis[i] = x;
                 break;
             } else
-                x ^= linearBasis[i];
+                x ^= basis[i];
         }
     }
 }
 
-void depthFirstSearch(int currentNode, int parent) {
-    pair<int, int> currentBase = {nodeValue[currentNode], distanceFromRoot[currentNode]};
-    for (int i = LOG_SIZE - 1; i >= 0; --i) {
-        if (currentBase.first & (1 << i)) {
-            if (currentBase.second > base[currentNode][i].second)
-                swap(currentBase, base[currentNode][i]);
-            if (currentBase.second == -1)
+void DFS(int node, int parent) {
+    pair<int, int> curr = {val[node], dist[node]};
+    for (int i = MAX_LG - 1; i >= 0; --i) {
+        if (curr.first & (1 << i)) {
+            if (curr.second > base[node][i].second)
+                swap(curr, base[node][i]);
+            if (curr.second == -1)
                 break;
-            currentBase.first ^= base[currentNode][i].first;
+            curr.first ^= base[node][i].first;
         }
     }
-    preOrder[currentNode] = ++preOrderCount;
-    for (auto neighbor : graph[currentNode])
-        if (neighbor != parent) {
-            distanceFromRoot[neighbor] = distanceFromRoot[currentNode] + 1;
-            nextNode[neighbor][0] = currentNode;
-            for (int j = 1; j < LOG_SIZE; ++j)
-                nextNode[neighbor][j] = nextNode[nextNode[neighbor][j - 1]][j - 1];
-            for (int j = 0; j < LOG_SIZE; ++j)
-                base[neighbor][j] = base[currentNode][j];
-            depthFirstSearch(neighbor, currentNode);
+    pre[node] = ++preCnt;
+    for (auto n : adjList[node])
+        if (n != parent) {
+            dist[n] = dist[node] + 1;
+            nextNode[n][0] = node;
+            for (int j = 1; j < MAX_LG; ++j)
+                nextNode[n][j] = nextNode[nextNode[n][j - 1]][j - 1];
+            for (int j = 0; j < MAX_LG; ++j)
+                base[n][j] = base[node][j];
+            DFS(n, node);
         }
-    postOrder[currentNode] = ++postOrderCount;
+    post[node] = ++postCnt;
 }
 
-bool isAncestor(int a, int b) {
-    return preOrder[a] <= preOrder[b] && postOrder[a] >= postOrder[b];
+bool isAnc(int a, int b) {
+    return pre[a] <= pre[b] && post[a] >= post[b];
 }
 
-int lowestCommonAncestor(int a, int b) {
-    if (isAncestor(a, b))
+int LCA(int a, int b) {
+    if (isAnc(a, b))
         return a;
-    for (int i = LOG_SIZE - 1; i >= 0; --i)
-        if (!isAncestor(nextNode[a][i], b))
+    for (int i = MAX_LG - 1; i >= 0; --i)
+        if (!isAnc(nextNode[a][i], b))
             a = nextNode[a][i];
     return nextNode[a][0];
 }
 
 int main() {
-    // Redirecting stdin to read from a file if 'in.txt' exists
+    // Redirect stdin if 'in.txt' exists
     if (ifstream("in.txt")) {
         freopen("in.txt", "r", stdin);
     }
 
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int numNodes;
-    cin >> numNodes;
-    for (int i = 0; i < numNodes; ++i) {
-        cin >> nodeValue[i];
-        for (int j = 0; j < LOG_SIZE; ++j)
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> val[i];
+        for (int j = 0; j < MAX_LG; ++j)
             base[i][j].second = -1;
     }
-    for (int i = 0; i < numNodes - 1; ++i) {
-        int nodeA, nodeB;
-        cin >> nodeA >> nodeB;
-        --nodeA;
-        --nodeB;
-        graph[nodeA].push_back(nodeB);
-        graph[nodeB].push_back(nodeA);
+    for (int i = 0; i < n - 1; ++i) {
+        int a, b;
+        cin >> a >> b;
+        --a; --b;
+        adjList[a].push_back(b);
+        adjList[b].push_back(a);
     }
-    depthFirstSearch(0, 0);
-    int numQueries;
-    cin >> numQueries;
-    while (numQueries--) {
-        int nodeA, nodeB, xorValue;
-        cin >> nodeA >> nodeB >> xorValue;
-        --nodeA;
-        --nodeB;
-        int commonAncestor = lowestCommonAncestor(nodeA, nodeB);
-        for (int i = 0; i < LOG_SIZE; ++i)
-            linearBasis[i] = 0;
-        for (int i = 0; i < LOG_SIZE; ++i)
-            if (base[nodeA][i].second >= distanceFromRoot[commonAncestor]) addLinearBasis(base[nodeA][i].first);
-        for (int i = 0; i < LOG_SIZE; ++i)
-            if (base[nodeB][i].second >= distanceFromRoot[commonAncestor]) addLinearBasis(base[nodeB][i].first);
-        for (int i = LOG_SIZE - 1; i >= 0; --i)
-            if (xorValue & (1 << i))
-                xorValue ^= linearBasis[i];
-        cout << (xorValue ? "NO" : "YES") << '\n';
+    DFS(0, 0);
+    int q;
+    cin >> q;
+    while (q--) {
+        int a, b, x;
+        cin >> a >> b >> x;
+        --a; --b;
+        int ancestor = LCA(a, b);
+        for (int i = 0; i < MAX_LG; ++i) basis[i] = 0;
+        for (int i = 0; i < MAX_LG; ++i)
+            if (base[a][i].second >= dist[ancestor]) addBasis(base[a][i].first);
+        for (int i = 0; i < MAX_LG; ++i)
+            if (base[b][i].second >= dist[ancestor]) addBasis(base[b][i].first);
+        for (int i = MAX_LG - 1; i >= 0; --i)
+            if (x & (1 << i))
+                x ^= basis[i];
+        cout << (x ? "NO" : "YES") << '\n';
     }
 }
