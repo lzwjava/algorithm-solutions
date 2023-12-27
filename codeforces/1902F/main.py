9f58bca32d1b94b177677e51531f8f235d1b1122
lzwjava <lzwjava@gmail.com>
2023-12-27 14:01:52 +0800

    add

--- codeforces/1902F/main.py
+++ codeforces/1902F/main.py
@@ -11,9 +11,12 @@ def main():
     n = int(input())
     a = list(map(int, input().split()))
     print(a)
+
+    graph = [list()] * n
     for i in range(n - 1):
         u, v = map(int, input().split())
         print(u, v)
+
     q = int(input())
     for i in range(q):
         x, y, k = map(int, input().split())
