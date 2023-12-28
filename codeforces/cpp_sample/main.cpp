bbc8b669023eda61d6c81674d027307c54fd8e27
lzwjava <lzwjava@gmail.com>
2023-12-28 19:57:35 +0800

    add

--- codeforces/cpp_sample/main.cpp
+++ codeforces/cpp_sample/main.cpp
@@ -1,13 +1,16 @@
-#include<iostream>
-#include<algorithm>
-#include<string>
-#include<cassert>
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-#ifndef ONLINE_JUDGE    
-    freopen("1.in", "r", stdin);
-#endif
+int main()
+{
+    if (ifstream("in.txt")) {
+        freopen("in.txt", "r", stdin);
+    }
+
+    
     return 0;
 }
