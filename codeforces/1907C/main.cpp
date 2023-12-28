e01fd0188a1ea5082e5528ad198ceab3bb87b6f0
lzwjava <lzwjava@gmail.com>
2023-12-28 22:30:33 +0800

    add

--- codeforces/1907C/main.cpp
+++ codeforces/1907C/main.cpp
@@ -14,11 +14,22 @@ int main()
         freopen("in.txt", "r", stdin);
     }
 
+    int t;
     cin >> t;
 
     while (t--) {
+        int n;
         cin >> n;
+        string s;
         cin >> s;
+
+        list<char> chs;
+
+        for (int i = 0; i < s.size(); i++) {
+            chs.push_back(s[i]);
+        }
+
+        cout << chs.size() << endl;
     }
 
     return 0;
