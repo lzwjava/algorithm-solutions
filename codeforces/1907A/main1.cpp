49341ca8b0ef79b3e6ec88d08f745d3d5f0475ab
lzwjava <lzwjava@gmail.com>
2023-12-28 19:49:42 +0800

    add

--- codeforces/1907A/main1.cpp
+++ codeforces/1907A/main1.cpp
@@ -13,9 +13,12 @@ int main()
         freopen("in.txt", "r", stdin);
     }
 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
     int t;
     cin >> t;
-    for (int i = 0; i < t; i++) {
+    while (t--) {
         string s;
         cin >> s;
 
