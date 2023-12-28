a8e2642e6082832f0714bcdd8a72eb0549bbc346
lzwjava <lzwjava@gmail.com>
2023-12-28 22:41:42 +0800

    add

--- codeforces/1915A/main.cpp
+++ codeforces/1915A/main.cpp
@@ -14,5 +14,21 @@ int main()
         freopen("in.txt", "r", stdin);
     }
 
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a == c) {
+            cout << b << endl;
+        } else if (a == b) {
+            cout << c << endl;
+        } else {
+            cout << a << endl;
+        }
+    }
+
     return 0;
 }
