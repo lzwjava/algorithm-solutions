ee947bc21f1270a35ea6bc570d9b3e4aa38e5d42
lzwjava <lzwjava@gmail.com>
2023-12-28 19:20:35 +0800

    add

--- codeforces/1907A/main.cpp
+++ codeforces/1907A/main.cpp
@@ -1,9 +1,9 @@
 #include <algorithm>
 #include <cassert>
-#include <iostream>
-#include <string>
 #include <fstream>
-
+#include <iostream>
+#include <set>
+#include <string>
 
 using namespace std;
 
@@ -13,7 +13,25 @@ int main()
         freopen("in.txt", "r", stdin);
     }
 
-    
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        string s;
+        cin >> s;
+
+        char c1 = s[0];
+        char c2 = s[1];
+
+        string sa = "abcdefgh";
+        string sb = "12345678";
+
+        set<string> ans;
+
+        for (char c : sa) {
+            string cs = string(1, c) + string(1, c2);
+            ans.insert(cs)
+        }
+    }
 
     return 0;
 }
