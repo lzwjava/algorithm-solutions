2015fd9a78d71e9725be8e602c0d0a70314f27f6
lzwjava <lzwjava@gmail.com>
2023-12-29 00:37:35 +0800

    add

--- codeforces/1915E/main.cpp
+++ codeforces/1915E/main.cpp
@@ -14,5 +14,22 @@ int main()
         freopen("in.txt", "r", stdin);
     }
 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        long long s[n];
+        s[0] = a[i];
+        for (int i = 1; i < n; i++) {
+            s[i] = s[i - 1] + a[i];
+        }
+        
+    }
+
     return 0;
 }
