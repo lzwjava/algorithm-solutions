1b094c885db4a99c220868e9e34e6774007e2a82
lzwjava <lzwjava@gmail.com>
2023-12-30 23:09:42 +0800

    add

--- codeforces/1916A/main.cpp
+++ codeforces/1916A/main.cpp
@@ -10,6 +10,32 @@ using namespace std;
 
 void solve()
 {
+    int n, k;
+    cin >> n >> k;
+    int b[n];
+    int p = 1;
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+        p *= b[i];
+    }
+    if (2023 % p == 0) {
+        cout << "YES" << endl;
+        int a = 2023 / p;
+        int rs[k];
+        rs[0] = a;
+        for (int i = 1; i < k; i++) {
+            rs[i] = 1;
+        }
+        for (int i = 0; i < k; i++) {
+            if (i != 0) {
+                cout << " ";
+            }
+            cout << rs[i];
+        }
+        cout << endl;
+    } else {
+        cout << "NO" << endl;
+    }
 }
 
 int main()
