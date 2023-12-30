d848e464c48d8a2d8d5af948b616916658d8dffa
lzwjava <lzwjava@gmail.com>
2023-12-30 23:44:26 +0800

    add

--- codeforces/1916B/main.cpp
+++ codeforces/1916B/main.cpp
@@ -8,8 +8,45 @@
 
 using namespace std;
 
+int gcd(int a, int b)
+{
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+long cal(int a, int b)
+{
+    int c = gcd(a, b);
+    if (c == 1) {
+        long ans;
+        if (b % a == 0) {
+            if (a == 1) {
+                ans = (long)b * b;
+            } else {
+                int d = b / a;
+                if (d < a) {
+                    ans = (long)b * d;
+                } else {
+                    ans = (long)b * a;
+                }
+            }
+        } else {
+            ans = (long long)b * a;
+        }
+        return ans;
+    } else {
+        return cal(a / c, b / c) * c;
+    }
+}
+
 void solve()
 {
+    int a, b;
+    cin >> a >> b;
+    long ans = cal(a, b);
+    cout << ans << endl;
 }
 
 int main()
