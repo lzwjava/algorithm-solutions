1fd70c7fe1c8688e07e466247f34cbebf04e3b0f
lzwjava <lzwjava@gmail.com>
2023-12-30 22:30:27 +0800

    add

---
+++ codeforces/1916B/main.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+
+void solve()
+{
+}
+
+int main()
+{
+    if (ifstream("in.txt")) {
+        freopen("in.txt", "r", stdin);
+    }
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
