6c63738639e5a9958c1ef09b124e0e1c1ba80928
Zhiwei Li <lzwjava@gmail.com>
2023-12-30 17:51:27 +0800

    add

--- codeforces/1917D/main.cpp
+++ codeforces/1917D/main.cpp
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+long MOD = 998244353;
+
 struct A {
     int p, q;
 
@@ -27,14 +29,40 @@ struct A {
 
 int inver(A a[], int i, int j)
 {
+    if (i == j) {
+        return 0;
+    }
+
     int m = (i + j) / 2;
     int left = inver(a, i, m);
     int right = inver(a, m + 1, j);
     int il = i, ir = m + 1;
+    int bn = j - i + 1;
+    A b[bn];
+    int bi = 0;
+    int ln = m - i + 1;
+    int rn = j - (m + 1) + 1;
+    int in = 0;
     while (il <= m || ir <= j) {
         if (il <= m && ir <= j) {
+            if (a[il] < a[ir]) {
+                b[bi++] = a[il++];
+                in += ir;
+            } else {
+                in += (ln - il);
+                b[bi++] = a[ir++];
+            }
+        } else if (il <= m) {
+            in += ir;
+            b[bi++] = a[il++];
+        } else {
+            b[bi++] = a[ir++];
         }
     }
+    for (int h = 0; h < bn; h++) {
+        a[i + h] = b[h];
+    }
+    return in;
 }
 
 void solve()
@@ -57,7 +85,8 @@ void solve()
         a[h].p = p[i];
         a[h].q = q[j];
     }
-    inver(a, 0, nk - 1);
+    int iv = inver(a, 0, nk - 1);
+    cout << iv % MOD << endl;
 }
 
 int main()
