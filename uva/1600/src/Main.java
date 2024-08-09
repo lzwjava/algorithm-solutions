723d5c56e7fa2deaa1053051af13a0a3cea149ab
lzwjava <lzwjava@gmail.com>
2024-08-09 08:20:26 +0800

    add

--- uva/1600/src/Main.java
+++ uva/1600/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -20,10 +22,31 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main m = new Main();
-        m.solve();
+        try {
+            m.solve();
+        } finally {
+            m.close();
+        }
     }
 
-    private void solve() {
+    private void solveCase() throws IOException {
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(in.readLine());
+        int[][] grid = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(in.readLine());
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
 
+    private void solve() throws IOException {
+        int c = Integer.parseInt(in.readLine());
+        for (int i = 0; i < c; i++) {
+            solveCase();
+        }
     }
 }
