4d8b26132aaf833b55f5c960b35b13481c6c5a5e
lzwjava <lzwjava@gmail.com>
2024-08-09 10:37:12 +0800

    add

--- uva/1600/src/Main.java
+++ uva/1600/src/Main.java
@@ -38,18 +38,20 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(in.readLine());
         int[][] grid = new int[m][n];
-        int[][] dists = new int[m][n];
+        int[][][] dists = new int[m][n][k + 1];
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(in.readLine());
             for (int j = 0; j < n; j++) {
                 grid[i][j] = Integer.parseInt(st.nextToken());
-                dists[i][j] = Integer.MAX_VALUE;
+                for (int v = 0; v < k + 1; v++) {
+                    dists[i][j][v] = Integer.MAX_VALUE;
+                }
             }
         }
 
 
         Pos p1 = new Pos(0, 0, 0, k);
-        dists[0][0] = 0;
+        dists[0][0][k] = k;
 
         PriorityQueue<Pos> pq = new PriorityQueue<>();
         pq.add(p1);
@@ -63,16 +65,22 @@ public class Main {
                 for (int d = 0; d < 4; d++) {
                     int ndx = p.x + dx[d];
                     int ndy = p.y + dy[d];
-                    if (ndx >= 0 && ndx < m && ndy >= 0 && ndy < n && dists[ndx][ndy] > p.dist + 1) {
-                        if (grid[ndx][ndy] == 0) {
-                            dists[ndx][ndy] = p.dist + 1;
-                            Pos np = new Pos(ndx, ndy, p.dist + 1, k);
-                            pq.add(np);
-                        } else if (p.k > 0) {
-                            dists[ndx][ndy] = p.dist + 1;
-                            Pos np = new Pos(ndx, ndy, p.dist + 1, p.k - 1);
-                            pq.add(np);
-                        }
+                    if (ndx >= 0 && ndx < m && ndy >= 0 && ndy < n) {
+                        if (dists[ndx][ndy].dist > p.dist + 1 || dists[ndx][ndy].k)
+
+                            if (grid[ndx][ndy] == 0) {
+                                if (dists[ndx][ndy].dist > p.dist + 1 || dists[ndx][ndy].k < k) {
+
+                                }
+
+                                dists[ndx][ndy] = p.dist + 1;
+                                Pos np = new Pos(ndx, ndy, p.dist + 1, k);
+                                pq.add(np);
+                            } else if (p.k > 0) {
+                                dists[ndx][ndy] = p.dist + 1;
+                                Pos np = new Pos(ndx, ndy, p.dist + 1, p.k - 1);
+                                pq.add(np);
+                            }
                     }
                 }
             }
@@ -86,6 +94,16 @@ public class Main {
         for (int i = 0; i < c; i++) {
             solveCase();
         }
+    }
+}
+
+class State {
+    int dist;
+    int k;
+
+    public State(int dist, int k) {
+        this.dist = dist;
+        this.k = k;
     }
 }
 
