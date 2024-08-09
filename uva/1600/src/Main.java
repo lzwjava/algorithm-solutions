b56bbb37b5abbc697fe979c70d6ae85945b8dac6
lzwjava <lzwjava@gmail.com>
2024-08-09 09:03:31 +0800

    add

--- uva/1600/src/Main.java
+++ uva/1600/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 import java.util.stream.IntStream;
 
@@ -9,6 +10,8 @@ public class Main {
 
     private final BufferedReader in;
     private final PrintWriter out;
+    private final int[] dx = new int[]{1, 0, -1, 0};
+    private final int[] dy = new int[]{0, 1, 0, -1};
 
     public Main() {
         in = new BufferedReader(new InputStreamReader(System.in));
@@ -35,12 +38,51 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(in.readLine());
         int[][] grid = new int[m][n];
+        int[][] dists = new int[m][n];
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(in.readLine());
             for (int j = 0; j < n; j++) {
                 grid[i][j] = Integer.parseInt(st.nextToken());
+                dists[i][j] = Integer.MAX_VALUE;
             }
         }
+
+
+        Pos p1 = new Pos(0, 0, 0);
+        dists[0][0] = 0;
+
+        PriorityQueue<Pos> pq = new PriorityQueue<>();
+        pq.add(p1);
+        int ans = -1;
+        while (!pq.isEmpty()) {
+            Pos p = pq.poll();
+            if (p.x == m - 1 && p.y == n - 1) {
+                ans = p.dist;
+            } else {
+                for (int d = 0; d < 4; d++) {
+                    int ndx = p.x + dx[d];
+                    int ndy = p.y + dy[d];
+                    if (ndx >= 0 && ndx < m && ndy >= 0 && ndy < n && dists[ndx][ndy] >= p.dist + 1) {
+                        if (grid[ndx][ndy] == 0) {
+                            dists[ndx][ndy] = p.dist + 1;
+                            Pos np = new Pos(ndx, ndy, p.dist + 1);
+                            pq.add(np);
+                        } else {
+                            for (int v = 1; v <= k; v++) {
+                                int vdx = p.x + dx[d] * v;
+                                int vdy = p.y + dy[d] * v;
+                                if (vdx >= 0 && vdx < m && vdy >= 0 && vdy < n && dists[vdx][vdy] >= p.dist + v) {
+                                    dists[vdx][vdy] = p.dist + v;
+                                    Pos vp = new Pos(vdx, vdy, p.dist + v);
+                                    pq.add(vp);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        out.println(ans);
     }
 
     private void solve() throws IOException {
@@ -48,5 +90,22 @@ public class Main {
         for (int i = 0; i < c; i++) {
             solveCase();
         }
+    }
+}
+
+class Pos implements Comparable<Pos> {
+    int x;
+    int y;
+    int dist;
+
+    public Pos(int x, int y, int dist) {
+        this.x = x;
+        this.y = y;
+        this.dist = dist;
+    }
+
+    @Override
+    public int compareTo(Pos pos) {
+        return Integer.compare(dist, pos.dist);
     }
 }
