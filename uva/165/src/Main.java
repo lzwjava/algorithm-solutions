444418455a7569761c5239249e8174ccad20e3ab
Zhiwei Li <lzwjava@gmail.com>
2024-08-18 18:28:44 +0800

    add

---
+++ uva/165/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Main {
+
+    private final BufferedReader in;
+    private final PrintWriter out;
+
+    public Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    private void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+    }
+
+    private void solve() {
+    }
+}
