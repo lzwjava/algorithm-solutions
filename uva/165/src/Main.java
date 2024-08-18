6bb518943f3729259d589c30a9d703b9ae21e9bd
Zhiwei Li <lzwjava@gmail.com>
2024-08-18 21:27:50 +0800

    add

--- uva/165/src/Main.java
+++ uva/165/src/Main.java
@@ -21,6 +21,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main m = new Main();
         m.solve();
+        m.close();
     }
 
     private void solve() {
