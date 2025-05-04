811309180a457a8db1b7c65280facb761d8ca0a4
Zhiwei Li <lzwjava@gmail.com>
2025-05-04 20:08:56 +0800

    fix(codeforces/2108F): Correct Main.java issues

--- codeforces/2108F/src/Main.java
+++ codeforces/2108F/src/Main.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 
 public class Main {
@@ -13,22 +15,37 @@ public class Main {
         out = new PrintWriter(System.out);
     }
 
-    public static void main(String[] args) throws IOException {
-        Main m = new Main();
-        m.solve();
-        m.close();
-    }
-
-    void close() {
-        out.flush();
-        out.close();
-    }
-
     void solve() throws IOException {
-        int t = Integer.parseInt(in.readLine());
-        while (t > 0) {
-            t--;
-            int n = Integer.parseInt(in.readLine());
+    }
+
+    void close() throws IOException {
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.flush();
+            out.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inStream = null;
+        PrintStream outStream = null;
+        boolean isLocal = System.getProperty("os.name").equals("Mac OS X");
+        if (isLocal) {
+            inStream = new FileInputStream("1.in");
+            // outStream = new PrintStream("1.out");
+            System.setIn(inStream);
+            // System.setOut(outStream);
+        }
+
+        Main main = new Main();
+        main.solve();
+        main.close();
+
+        if (isLocal) {
+            inStream.close();
+            // outStream.close();
         }
     }
 }
