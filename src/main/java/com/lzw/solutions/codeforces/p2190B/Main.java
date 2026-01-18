288583f2c1defd5e022328f55fca90aa44bb883e
Zhiwei Li <lzwjava@gmail.com>
2026-01-18 23:19:36 +0800

    feat(codeforces): add solution for problem p2190B

---
+++ src/main/java/com/lzw/solutions/codeforces/p2190B/Main.java
@@ -0,0 +1,36 @@
+package com.lzw.solutions.codeforces.p2190B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Main {
+
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public Main() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        m.solve();
+        m.close();
+    }
+
+    private void close() {
+        out.flush();
+        out.close();
+    }
+
+    private void solve() throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            t--;
+            int n = Integer.parseInt(in.readLine());
+        }
+    }
+}
