a1f696f78e1e84b5a970371246aae000e1a5abdc
Zhiwei Li <lzwjava@gmail.com>
2026-01-23 23:23:43 +0800

    feat(codeforces): add solution template for p2189C2

---
+++ src/main/java/com/lzw/solutions/codeforces/p2189C2/Main.java
@@ -0,0 +1,20 @@
+package com.lzw.solutions.codeforces.p2189C2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Main {
+    private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    private static final PrintWriter out = new PrintWriter(System.out, true);
+
+    private static void solve() throws IOException {
+        // your code here
+    }
+
+    public static void main(String[] args) throws IOException {
+        solve();
+        out.close();
+    }
+}
